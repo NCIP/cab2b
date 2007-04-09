193f387f4ec2188fa0ad726feb676943e74414de
srinath_k <srinath_k@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-04-09 05:14:12 +0000

    modified tagged value for originAssociation

--- source/server/main/edu/wustl/cab2b/server/util/InheritanceUtil.java
+++ source/server/main/edu/wustl/cab2b/server/util/InheritanceUtil.java
@@ -1,6 +1,10 @@
 package edu.wustl.cab2b.server.util;
 
 import static edu.wustl.cab2b.common.util.Constants.CONNECTOR;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 import edu.common.dynamicextensions.domaininterface.AbstractAttributeInterface;
 import edu.common.dynamicextensions.domaininterface.AssociationInterface;
 import edu.common.dynamicextensions.domaininterface.AttributeInterface;
@@ -16,10 +20,12 @@ import edu.wustl.cab2b.server.cache.EntityCache;
 public class InheritanceUtil {
     /**
      * Checks whether passed attribute/association is inheriated.
-     * @param abstractAttribute Attribute/Association to check.
+     * @param abstractAttribute
+     *            Attribute/Association to check.
      * @return TRUE if it is inherited else returns FALSE
      */
-    public static boolean isInherited(AbstractAttributeInterface abstractAttribute) {
+    public static boolean isInherited(
+                                      AbstractAttributeInterface abstractAttribute) {
         for (TaggedValueInterface tag : abstractAttribute.getTaggedValueCollection()) {
             if (tag.getKey().equals(Constants.TYPE_DERIVED)) {
                 return true;
@@ -29,11 +35,14 @@ public class InheritanceUtil {
     }
 
     /**
-     * Returns actual attribute if passed attribute is a derieved one. Else returns the passed attribute
-     * @param attribute Attribute for which actual attribute is expected.
+     * Returns actual attribute if passed attribute is a derieved one. Else
+     * returns the passed attribute
+     * @param attribute
+     *            Attribute for which actual attribute is expected.
      * @return The actual attribute
      */
-    public static AttributeInterface getActualAttribute(AttributeInterface attribute) {
+    public static AttributeInterface getActualAttribute(
+                                                        AttributeInterface attribute) {
         if (!isInherited(attribute)) {
             return attribute;
         }
@@ -54,35 +63,61 @@ public class InheritanceUtil {
     }
 
     /**
-     * Returns actual association if passed association is a derieved one. Else returns the passed association
-     * @param association Attribute for which actual association is expected.
+     * Returns actual association if passed association is a derieved one. Else
+     * returns the passed association
+     * @param association
+     *            Attribute for which actual association is expected.
      * @return The actual association
      */
-    public static AssociationInterface getActualAassociation(AssociationInterface association) {
+    public static AssociationInterface getActualAassociation(
+                                                             AssociationInterface association) {
         if (!isInherited(association)) {
             return association;
         }
-        String originalAssociationPointer=""; 
+        String originalAssociationPointer = "";
         for (TaggedValueInterface tag : association.getTaggedValueCollection()) {
             if (tag.getKey().equals(Constants.ORIGINAL_ASSOCIATION_POINTER)) {
                 originalAssociationPointer = tag.getValue();
                 break;
             }
         }
-        if(originalAssociationPointer.equals("")) {
-            throw new RuntimeException("Inconsistent database state !!! Found one inherited association without, tagged value having key as Constants.ORIGINAL_ASSOCIATION_POINTER");
+        if (originalAssociationPointer.equals("")) {
+            throw new RuntimeException(
+                    "Inconsistent database state !!! Found one inherited association without, tagged value having key as Constants.ORIGINAL_ASSOCIATION_POINTER");
         }
-        AssociationInterface actualAssociation = EntityCache.getInstance().getAssociationByUniqueStringIdentifier(originalAssociationPointer);
+
+        if (association.isSystemGenerated()) {
+            originalAssociationPointer = reverse(originalAssociationPointer);
+        }
+        AssociationInterface actualAssociation = EntityCache.getInstance().getAssociationByUniqueStringIdentifier(
+                                                                                                                  originalAssociationPointer);
         return actualAssociation;
     }
 
+    private static String reverse(String originalAssociationPointer) {
+        String[] tokens = originalAssociationPointer.split(CONNECTOR);
+        return concatStrings(tokens[3], tokens[2], tokens[1], tokens[0]);
+    }
+
     public static String generateUniqueId(AssociationInterface association) {
+        return concatStrings(association.getEntity().getName(),
+                             association.getSourceRole().getName(),
+                             association.getTargetRole().getName(),
+                             association.getTargetEntity().getName());
+    }
+
+    private static String concatStrings(String srcEntityName,
+                                        String srcRoleName, String tgtRoleName,
+                                        String tgtEntityName) {
         StringBuilder builder = new StringBuilder();
-        builder.append(association.getEntity().getName());
-        builder.append(CONNECTOR); 
-        builder.append(association.getTargetRole().getName());
-        builder.append(CONNECTOR); 
-        builder.append(association.getTargetEntity().getName());
+        builder.append(srcEntityName);
+        builder.append(CONNECTOR);
+        builder.append(srcRoleName);
+        builder.append(CONNECTOR);
+        builder.append(tgtEntityName);
+        builder.append(CONNECTOR);
+        builder.append(tgtRoleName);
         return builder.toString();
+
     }
 }
