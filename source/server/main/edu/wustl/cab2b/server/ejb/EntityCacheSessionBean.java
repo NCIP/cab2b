74a8668360bcc908029413df49f852a255325d19
Rahul_Ner <Rahul_Ner@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-04-11 12:31:49 +0000

    refactored entity cache as per bug 4011

--- source/server/main/edu/wustl/cab2b/server/ejb/EntityCacheSessionBean.java
+++ source/server/main/edu/wustl/cab2b/server/ejb/EntityCacheSessionBean.java
@@ -1,5 +1,5 @@
 /**
- * <p>Description:	This class is used to cache the Entity and its Attribute objects.</p>
+ * <p>Description:  This class is used to cache the Entity and its Attribute objects.</p>
  * Copyright:    Copyright (c) year
  * Company: Washington University, School of Medicine, St. Louis.
  * @author Gautam Shetty
@@ -17,63 +17,56 @@ import javax.ejb.SessionContext;
 import edu.wustl.cab2b.common.beans.MatchedClass;
 import edu.wustl.cab2b.common.entityCache.EntityCacheBusinessInterface;
 import edu.wustl.cab2b.common.entityCache.IEntityCache;
-import edu.wustl.cab2b.common.exception.RuntimeException;
 import edu.wustl.cab2b.server.cache.EntityCache;
-import edu.wustl.common.util.global.ApplicationProperties;
 
 /**
  * This class is used to cache the Entity and its Attribute objects.
  * @author gautam_shetty
  */
-public class EntityCacheSessionBean extends AbstractStatelessSessionBean implements EntityCacheBusinessInterface, Serializable
-{
+public class EntityCacheSessionBean extends AbstractStatelessSessionBean implements EntityCacheBusinessInterface,
+        Serializable {
     private static final long serialVersionUID = 1234567890L;
-    
-    private SessionContext sessionContext;
-    
+
     /**
      * List of all Entity objects.
      */
     private static EntityCache entityCache;
-    
+
     /**
      * Is set to true if the entity cache is loaded else is set to false.
      */
     static boolean cacheLoaded = false;
+
     /**
      * Returns the EntityCache instance.
      * @return Returns the entityCache.
      */
-    public IEntityCache getEntityCache()
-    {
+    public IEntityCache getEntityCache() {
         return entityCache;
     }
-    
+
     /**
      * Sets the EntityCache instance.
      * @param entityCache The entityCache to set.
      */
-    public static void setEntityCache(EntityCache entityCache)
-    {
+    public static void setEntityCache(EntityCache entityCache) {
         EntityCacheSessionBean.entityCache = entityCache;
     }
-    
+
     /**
      * @return Returns the cacheLoaded.
      */
-    public static boolean isCacheLoaded()
-    {
+    public static boolean isCacheLoaded() {
         return cacheLoaded;
     }
-    
+
     /**
      * @param cacheLoaded The cacheLoaded to set.
      */
-    public static void setCacheLoaded(boolean cacheLoaded)
-    {
+    public static void setCacheLoaded(boolean cacheLoaded) {
         EntityCacheSessionBean.cacheLoaded = cacheLoaded;
     }
-    
+
     /**
      * Returns the Entity objects whose fields match with the respective not null 
      * fields in the passed entity object.
@@ -81,11 +74,10 @@ public class EntityCacheSessionBean extends AbstractStatelessSessionBean impleme
      * @return the Entity objects whose fields match with the respective not null 
      * fields in the passed entity object.
      */
-    public MatchedClass getEntityOnEntityParameters(Collection entityCollection)
-    {
+    public MatchedClass getEntityOnEntityParameters(Collection entityCollection) {
         return entityCache.getEntityOnEntityParameters(entityCollection);
     }
-    
+
     /**
      * Returns the Entity objects whose Attribute fields match with the respective not null 
      * fields in the passed Attribute object.
@@ -93,35 +85,20 @@ public class EntityCacheSessionBean extends AbstractStatelessSessionBean impleme
      * @return the Entity objects whose Attribute fields match with the respective not null 
      * fields in the passed Attribute object.
      */
-    public MatchedClass getEntityOnAttributeParameters(Collection attributeCollection)
-    {
+    public MatchedClass getEntityOnAttributeParameters(Collection attributeCollection) {
         return entityCache.getEntityOnAttributeParameters(attributeCollection);
     }
-    
+
     /**
      * (non-Javadoc)
      * @see javax.ejb.SessionBean#setSessionContext(javax.ejb.SessionContext)
      */
-    public void setSessionContext(SessionContext sessionContext) throws EJBException,
-            RemoteException
-    {
-        this.sessionContext = sessionContext;
+    public void setSessionContext(SessionContext sessionContext) throws EJBException, RemoteException {
         
+        super.setSessionContext(sessionContext);
+
         // Get instance of the EntityCache class.
         entityCache = EntityCache.getInstance();
-        
-//        try
-//        {
-//            // If cache is not loaded, load the cache.
-//            if (!isCacheLoaded())
-//            {
-//                entityCache.init();
-//                cacheLoaded = true;
-//            }
-//        }
-//        catch (RuntimeException runExp)
-//        {
-//            throw new RemoteException(runExp.getMessage(), runExp);
-//        }
+
     }
 }
