4e06ce4f3aa8828fc10d04bad2a093ded95e1f7f
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-04-11 16:16:37 +0000

    added new jCalender

--- source/client/main/edu/wustl/cab2b/client/ui/controls/Cab2bDatePicker.java
+++ source/client/main/edu/wustl/cab2b/client/ui/controls/Cab2bDatePicker.java
@@ -1,19 +1,18 @@
 package edu.wustl.cab2b.client.ui.controls;
 
 import java.awt.Dimension;
+import java.sql.Date;
 
 import org.jdesktop.swingx.JXDatePicker;
 
-public class Cab2bDatePicker extends JXDatePicker{
+import com.toedter.calendar.JDateChooser;
+
+public class Cab2bDatePicker extends JDateChooser{
 
 	final Dimension dim = new Dimension(100,20);
 
 	public Cab2bDatePicker() {
-		super();
-		setPreferredSize(dim);
-	}
-	public Cab2bDatePicker(long time) {
-		super(time);
-		setPreferredSize(dim);
+		super(null, "yyyy/MM/dd");
+		setPreferredSize(dim);			
 	}
 }
