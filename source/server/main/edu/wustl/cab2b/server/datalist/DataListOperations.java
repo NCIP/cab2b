f99c5b50d6795b78c2398150129d57666d932dee
chetan_bh <chetan_bh@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-04-17 09:25:26 +0000

    Code Review commnets incorporated (#4100), like
    1) Instance variable mapOfNewEntities and mapOfNewAssociations
    moved to instance methods.
    
    2) All methods wraps other exception like DAOException, DE Exceptions into a RuntimeException and throws it.

--- source/server/main/edu/wustl/cab2b/server/datalist/DataListOperations.java
+++ source/server/main/edu/wustl/cab2b/server/datalist/DataListOperations.java
@@ -27,6 +27,8 @@ import edu.wustl.cab2b.common.datalist.DataList;
 import edu.wustl.cab2b.common.datalist.DataListBusinessInterface;
 import edu.wustl.cab2b.common.datalist.IDataRow;
 import edu.wustl.cab2b.common.domain.DataListMetadata;
+import edu.wustl.cab2b.common.errorcodes.ErrorCodeConstants;
+import edu.wustl.cab2b.common.exception.RuntimeException;
 import edu.wustl.cab2b.server.util.DynamicExtensionUtility;
 import edu.wustl.common.bizlogic.DefaultBizLogic;
 import edu.wustl.common.exception.BizLogicException;
@@ -36,8 +38,9 @@ import edu.wustl.common.util.global.Constants;
 import edu.wustl.common.util.logger.Logger;
 
 /**
- * Class which performs several <i>Data List</i> operations like 
- * save, retreive etc.
+ * Class which performs several Data List operations like 
+ * save, retreive data list etc.
+ * 
  * @author chetan_bh
  */
 public class DataListOperations extends DefaultBizLogic
@@ -47,11 +50,6 @@ public class DataListOperations extends DefaultBizLogic
 	 * Hibernate DAO Type to use.
 	 */
 	private static final int DAO_TYPE = Constants.HIBERNATE_DAO;
-	
-	/**
-	 * DE's Entity manager instance.
-	 */
-	EntityManagerInterface entityManager = EntityManager.getInstance();
 
 	/**
 	 * DE's DomainObjectFactory instance.
@@ -65,57 +63,62 @@ public class DataListOperations extends DefaultBizLogic
 			.getEntityGroupByName(edu.wustl.cab2b.common.util.Constants.DATALIST_ENTITY_GROUP_NAME);
 
 	/**
-	 * Map to maintain list of new entities created so that 
-	 * we can reuse the existing entiy later.
-	 * 
-	 * New entities will contain new attributes, corresponding to 
-	 * old entitiy's old attributes. 
+	 * Returns a list of all available data list metadata. 
+	 * @return list of data list metadata.
+	 * @throws DAOException
 	 */
-	Map<String, EntityInterface> mapOfNewEntities = new HashMap<String, EntityInterface>();
-
-	/**
-	 * Map to maintain list of new Associations created between
-	 * new entities to reuse later. 
-	 */
-	Map<String, AssociationInterface> mapOfNewAssociations = new HashMap<String, AssociationInterface>();
-
-	/**
-	 * This is a Map of attribute->value and association->list_of_containments.
-	 * This map complies to the requiremnt of 2nd parameter in 
-	 * {@link EntityManager#insertData(EntityInterface, Map)}.
-	 */
-	Map<AbstractAttributeInterface, Object> dataListAttributesMap;
-
-	/**
-	 * Entity instance for data list as an entity.
-	 */
-	EntityInterface dataListEntity;
-
-	public List<DataListMetadata> retrieveAllDataListMetadata() throws DAOException
+	public List<DataListMetadata> retrieveAllDataListMetadata() throws RuntimeException
 	{
 		List<DataListMetadata> allDataList = null;
 
-		allDataList = (List<DataListMetadata>) retrieve("DataListMetadata");
-		
-		Logger.out.info("allDataListMetadata.size() ########### " + allDataList.size());
+		try
+		{
+			allDataList = (List<DataListMetadata>) retrieve(DataListMetadata.class.getName());
+		}
+		catch (DAOException e)
+		{
+			throw (new RuntimeException(e.getMessage(), e,
+					ErrorCodeConstants.DATALIST_RETRIEVE_ERROR));
+		}
 		return allDataList;
 	}
 
+	/**
+	 * Returns a data list which contains saved data.
+	 * @param dataListId
+	 * @return a data list.
+	 */
 	public DataList retrieveDataList(Long dataListId)
 	{
 		DataList dataList = new DataList();
 		//TODO Yet to implement this functionality.
 		return dataList;
 	}
-	
-	public DataListMetadata retrieveDataListMetadata(Long id) throws DAOException
+
+	/**
+	 * Returns a data list metadata.
+	 * @param id
+	 * @return
+	 * @throws DAOException
+	 */
+	public DataListMetadata retrieveDataListMetadata(Long id) throws RuntimeException
 	{
-		List results = (new DataListOperations()).retrieve("DataListMetadata", "id", id);
-		if(results != null && results.size() > 0)
-			return (DataListMetadata)results.get(0);
+		List<DataListMetadata> results = null;
+		try
+		{
+			results = (new DataListOperations()).retrieve(DataListMetadata.class.getName(), "id",
+					id);
+		}
+		catch (DAOException e)
+		{
+			throw (new RuntimeException(e.getMessage(), e,
+					ErrorCodeConstants.DATALIST_RETRIEVE_ERROR));
+		}
+		if (results != null && results.size() > 0)
+			return results.get(0);
 		return null;
 	}
-	
+
 	/**
 	 * Constructs the recursive map {@link DataListOperations#dataListAttributesMap}} recursively 
 	 * and persists it to the database using Dynamic Extension APIs.
@@ -128,15 +131,41 @@ public class DataListOperations extends DefaultBizLogic
 	 * @throws BizLogicException 
 	 * @throws DAOException 
 	 */
-	public Long save(DataList dataListToSave) throws DynamicExtensionsApplicationException,
-			DynamicExtensionsSystemException, BizLogicException, UserNotAuthorizedException
+	public Long save(DataList dataListToSave) throws RuntimeException
 	{
 		List<IDataRow> dataRows = dataListToSave.getDataList();
+
+		/**
+		 * DE's Entity manager instance.
+		 */
+		EntityManagerInterface entityManager = EntityManager.getInstance();
 
 		/* There will always be one root node is the datalist tree. */
 		IDataRow rootDataRow = dataRows.get(0);
 
-		createMapForDataListEntity(dataListToSave.getDataListAnnotation());
+		Map<AbstractAttributeInterface, Object> dataListAttributesMap = new HashMap<AbstractAttributeInterface, Object>();
+
+		EntityInterface dataListEntity = domainObjectFactory.createEntity();
+		dataListEntity.setName("DataList_" + System.currentTimeMillis());
+		dataListEntity.addEntityGroupInterface(dataListEntityGroup);
+		dataListEntityGroup.addEntity(dataListEntity);
+
+		/**
+		 * Map to maintain list of new entities created so that 
+		 * we can reuse the existing entiy later.
+		 * 
+		 * New entities will contain new attributes, corresponding to 
+		 * old entitiy's old attributes. 
+		 */
+		Map<String, EntityInterface> mapOfNewEntities = new HashMap<String, EntityInterface>();
+
+		/**
+		 * Map to maintain list of new Associations created between
+		 * new entities to reuse later. 
+		 */
+		Map<String, AssociationInterface> mapOfNewAssociations = new HashMap<String, AssociationInterface>();
+
+		mapOfNewEntities.put(dataListEntity.getName() + "_1", dataListEntity); // Tree level one.
 
 		for (IDataRow firstLevelTypeDataRow : rootDataRow.getChildren())
 		{
@@ -145,73 +174,88 @@ public class DataListOperations extends DefaultBizLogic
 				List<IDataRow> valueDataRowsForThisType = firstLevelTypeDataRow.getChildren();
 				for (IDataRow firstLevelDataRow : valueDataRowsForThisType)
 				{
-					createNewEntityAndAssociation(dataListAttributesMap, firstLevelDataRow, dataListEntity, 2);
+					createNewEntityAndAssociation(dataListAttributesMap, firstLevelDataRow,
+							dataListEntity, 2, mapOfNewEntities, mapOfNewAssociations);
 				}
 			}
 			else
 			{
-				createNewEntityAndAssociation(dataListAttributesMap, firstLevelTypeDataRow, dataListEntity, 2);
+				createNewEntityAndAssociation(dataListAttributesMap, firstLevelTypeDataRow,
+						dataListEntity, 2, mapOfNewEntities, mapOfNewAssociations);
 			}
 		}
 
-		Long entityId = entityManager.persistEntity(dataListEntity).getId();
+		Long dataListId = null;
+		try
+		{
+			Long entityId = entityManager.persistEntity(dataListEntity).getId();
 
-		Long recordId = entityManager.insertData(dataListEntity, dataListAttributesMap);
-		
-		Logger.out.info("DataList saved successfully !!!! (entityId, recordId) :: " + entityId
-				+ ", " + recordId);
+			Long recordId = entityManager.insertData(dataListEntity, dataListAttributesMap);
 
-		DataListMetadata dataListMetadata = dataListToSave.getDataListAnnotation();
-		dataListMetadata.setEntityId(entityId);
+			Logger.out.info("DataList saved successfully !!!! (entityId, recordId) :: " + entityId
+					+ ", " + recordId);
 
-		Long dataListId = saveDataListMetadata(dataListMetadata);
+			DataListMetadata dataListMetadata = dataListToSave.getDataListAnnotation();
+			dataListMetadata.setEntityId(entityId);
 
+			dataListId = saveDataListMetadata(dataListMetadata);
+
+		}
+		catch (DynamicExtensionsSystemException e)
+		{
+			throw (new RuntimeException(e.getMessage(), e, ErrorCodeConstants.DATALIST_SAVE_ERROR));
+		}
+		catch (DynamicExtensionsApplicationException e)
+		{
+			throw (new RuntimeException(e.getMessage(), e, ErrorCodeConstants.DATALIST_SAVE_ERROR));
+		}
 		return dataListId;
 	}
-	
+
 	/**
 	 * Creates new entities and associations if they are not already created.
-	 * @param mapToConstruct
-	 * @param dataRow
-	 * @param entity
-	 * @param treeLevel
-	 * @throws DynamicExtensionsApplicationException
-	 * @throws DynamicExtensionsSystemException
+	 * @param mapToConstruct map to construct.
+	 * @param dataRow data row which contains values and attributes collection used to construct map.
+	 * @param entity parent entity.
+	 * @param treeLevel tree level is only to distinguish same entities in different levels in the datalist.
 	 */
-	private void createNewEntityAndAssociation(Map<AbstractAttributeInterface, Object> mapToConstruct, IDataRow dataRow,
-			EntityInterface entity, int treeLevel) throws DynamicExtensionsApplicationException,
-			DynamicExtensionsSystemException
+	private void createNewEntityAndAssociation(
+			Map<AbstractAttributeInterface, Object> mapToConstruct, IDataRow dataRow,
+			EntityInterface entity, int treeLevel, Map<String, EntityInterface> mapOfNewEntities,
+			Map<String, AssociationInterface> mapOfNewAssociations)
 	{
-		EntityInterface newEntity = mapOfNewEntities.get(dataRow.getEntityInterface()
-				.getName()
-				+ "_" + treeLevel);
+
+		String keyForNewEntitiesMap = dataRow.getEntityInterface().getName() + "_" + treeLevel;
+
+		EntityInterface newEntity = mapOfNewEntities.get(keyForNewEntitiesMap);
 		if (newEntity == null)
 		{
 			newEntity = getNewEntity(dataRow.getEntityInterface());
-			mapOfNewEntities
-					.put(dataRow.getEntityInterface().getName() + "_" + treeLevel, newEntity);
+			mapOfNewEntities.put(keyForNewEntitiesMap, newEntity);
 		}
 
-		AssociationInterface newAssociation = mapOfNewAssociations.get(entity.getName()
-				+ "_" + dataRow.getEntityInterface().getName());
+		String keyForNewAssociationsMap = entity.getName() + "_"
+				+ dataRow.getEntityInterface().getName();
+
+		AssociationInterface newAssociation = mapOfNewAssociations.get(keyForNewAssociationsMap);
 		if (newAssociation == null)
 		{
 			newAssociation = createNewOneToManyAsso(entity, newEntity);
-			mapOfNewAssociations.put(entity.getName() + "_"
-					+ dataRow.getEntityInterface().getName(), newAssociation);
+			mapOfNewAssociations.put(keyForNewAssociationsMap, newAssociation);
 			mapToConstruct.put(newAssociation, new ArrayList());
 			entity.addAssociation(newAssociation);
 		}
 		List<Map> listOfSubMaps = (List<Map>) mapToConstruct.get(newAssociation);
-		
+
 		if (listOfSubMaps == null)
 		{
 			listOfSubMaps = new ArrayList<Map>();
 			mapToConstruct.put(newAssociation, listOfSubMaps);
 		}
-		
+
 		Map<AbstractAttributeInterface, Object> mapToAdd = new HashMap<AbstractAttributeInterface, Object>();
-		constructDataRowMap(dataRow, mapToAdd, newEntity, treeLevel);
+		constructDataRowMap(dataRow, mapToAdd, newEntity, treeLevel, mapOfNewEntities,
+				mapOfNewAssociations);
 
 		listOfSubMaps.add(mapToAdd);
 	}
@@ -225,9 +269,7 @@ public class DataListOperations extends DefaultBizLogic
 	private AssociationInterface createNewOneToManyAsso(EntityInterface srcEntity,
 			EntityInterface tarEntity)
 	{
-		AssociationInterface returner = null;
-
-		returner = domainObjectFactory.createAssociation();
+		AssociationInterface returner = domainObjectFactory.createAssociation();
 		String associationName = "AssociationName_" + srcEntity.getAssociationCollection().size()
 				+ 1;
 		returner.setName(associationName);
@@ -262,47 +304,6 @@ public class DataListOperations extends DefaultBizLogic
 	}
 
 	/**
-	 * Constructs the top level data map.
-	 * @param dataListMetadata
-	 */
-	private void createMapForDataListEntity(DataListMetadata dataListMetadata)
-	{
-		dataListAttributesMap = new HashMap<AbstractAttributeInterface, Object>();
-
-		dataListEntity = domainObjectFactory.createEntity();
-
-		dataListEntity.setName("DataList_" + System.currentTimeMillis());
-
-		dataListEntity.addEntityGroupInterface(dataListEntityGroup);
-
-		dataListEntityGroup.addEntity(dataListEntity);
-
-		AttributeInterface dataListName = domainObjectFactory.createStringAttribute();
-		dataListName.setName("name");
-
-		AttributeInterface dataListDesc = domainObjectFactory.createStringAttribute();
-		dataListDesc.setName("description");
-
-		AttributeInterface dataListDateCreated = domainObjectFactory.createDateAttribute();
-		dataListDateCreated.setName("dateCreatedOn");
-
-		AttributeInterface dataListDateLastModified = domainObjectFactory.createDateAttribute();
-		dataListDateLastModified.setName("dataLastModifiedOn");
-
-		dataListEntity.addAttribute(dataListName);
-		dataListEntity.addAttribute(dataListDesc);
-		dataListEntity.addAttribute(dataListDateCreated);
-		dataListEntity.addAttribute(dataListDateLastModified);
-
-		dataListAttributesMap.put(dataListName, dataListMetadata.getName());
-		dataListAttributesMap.put(dataListDesc, dataListMetadata.getDescription());
-		dataListAttributesMap.put(dataListDateCreated, dataListMetadata.getCreatedOn());
-		dataListAttributesMap.put(dataListDateLastModified, dataListMetadata.getLastUpdatedOn());
-
-		mapOfNewEntities.put(dataListEntity.getName() + "_1", dataListEntity); // Tree level one.		
-	}
-
-	/**
 	 * A recursive function to construct the recursive map of attribute->value 
 	 * and association->list(map).
 	 * 
@@ -310,13 +311,11 @@ public class DataListOperations extends DefaultBizLogic
 	 * @param mapToConstruct
 	 * @param newEntity
 	 * @param treeLevel
-	 * @throws DynamicExtensionsApplicationException
-	 * @throws DynamicExtensionsSystemException
 	 */
 	private void constructDataRowMap(IDataRow dataRowToSave,
 			Map<AbstractAttributeInterface, Object> mapToConstruct, EntityInterface newEntity,
-			int treeLevel) throws DynamicExtensionsApplicationException,
-			DynamicExtensionsSystemException
+			int treeLevel, Map<String, EntityInterface> mapOfNewEntities,
+			Map<String, AssociationInterface> mapOfNewAssociations)
 	{
 		Object[] values = dataRowToSave.getRow();
 
@@ -343,12 +342,14 @@ public class DataListOperations extends DefaultBizLogic
 				{
 					for (IDataRow subDR : dataRow.getChildren())
 					{
-						createNewEntityAndAssociation(mapToConstruct, subDR, newEntity, treeLevel);
+						createNewEntityAndAssociation(mapToConstruct, subDR, newEntity, treeLevel,
+								mapOfNewEntities, mapOfNewAssociations);
 					}
 				}
 				else
 				{
-					createNewEntityAndAssociation(mapToConstruct, dataRow, newEntity, treeLevel);
+					createNewEntityAndAssociation(mapToConstruct, dataRow, newEntity, treeLevel,
+							mapOfNewEntities, mapOfNewAssociations);
 				}
 			}
 		}
@@ -399,11 +400,10 @@ public class DataListOperations extends DefaultBizLogic
 	/**
 	 * Creates and returns a new attribute given old attribute.
 	 * @param oldAttribute
-	 * @return
+	 * @return new attriute with same old attribute name.
 	 */
 	private AttributeInterface getNewAttribute(AttributeInterface oldAttribute)
 	{
-		//Logger.out.info("DataListSessionBean :: getNewAttribute");
 		AttributeInterface newAttribute = null;
 		String oldAttributeType = oldAttribute.getDataType();
 		if (oldAttributeType.equals(EntityManagerConstantsInterface.STRING_ATTRIBUTE_TYPE))
@@ -445,12 +445,12 @@ public class DataListOperations extends DefaultBizLogic
 		dataListEntityGroup.addEntity(newEntity);
 
 		newEntity.setName(oldEntity.getName());
-		
+
 		TaggedValueInterface taggedValue = domainObjectFactory.createTaggedValue();
-		taggedValue.setKey("original_entity_id");
+		taggedValue.setKey(edu.wustl.cab2b.common.util.Constants.OLD_ENTITY_ID_TAG_NAME);
 		taggedValue.setValue(oldEntity.getId().toString());
 		newEntity.addTaggedValue(taggedValue);
-		
+
 		Collection<AttributeInterface> oldAttribs = oldEntity.getAttributeCollection();
 		for (AttributeInterface oldAttrib : oldAttribs)
 		{
@@ -463,15 +463,23 @@ public class DataListOperations extends DefaultBizLogic
 	}
 
 	/**
-	 * @throws UserNotAuthorizedException 
-	 * @throws BizLogicException 
+	 * Saves data list metadata.
 	 * @see DataListBusinessInterface#saveDataListMetadata(DataListMetadata)
 	 */
-	public Long saveDataListMetadata(DataListMetadata datalistMetadata) throws BizLogicException,
-			UserNotAuthorizedException
+	public Long saveDataListMetadata(DataListMetadata datalistMetadata) throws RuntimeException
 	{
-		insert(datalistMetadata, DAO_TYPE);
-		Logger.out.info("########### saved matadata successfully ");
+		try
+		{
+			insert(datalistMetadata, DAO_TYPE);
+		}
+		catch (BizLogicException e)
+		{
+			throw (new RuntimeException(e.getMessage(), e, ErrorCodeConstants.DATALIST_SAVE_ERROR));
+		}
+		catch (UserNotAuthorizedException e)
+		{
+			throw (new RuntimeException(e.getMessage(), e, ErrorCodeConstants.DATALIST_SAVE_ERROR));
+		}
 		return datalistMetadata.getId();
 	}
 
