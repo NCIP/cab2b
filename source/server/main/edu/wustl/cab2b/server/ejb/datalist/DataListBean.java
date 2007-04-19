6aab4efacba001c95c5db3b06c951c01b0667070
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-04-19 05:51:45 +0000

    added getEntityRecord() method

--- source/server/main/edu/wustl/cab2b/server/ejb/datalist/DataListBean.java
+++ source/server/main/edu/wustl/cab2b/server/ejb/datalist/DataListBean.java
@@ -4,6 +4,13 @@ package edu.wustl.cab2b.server.ejb.datalist;
 import java.rmi.RemoteException;
 import java.util.List;
 
+<<<<<<< DataListBean.java
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.common.dynamicextensions.entitymanager.EntityRecord;
+import edu.common.dynamicextensions.exception.DynamicExtensionsApplicationException;
+import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
+=======
+>>>>>>> 1.5
 import edu.wustl.cab2b.common.datalist.DataList;
 import edu.wustl.cab2b.common.datalist.DataListBusinessInterface;
 import edu.wustl.cab2b.common.domain.DataListMetadata;
@@ -52,4 +59,12 @@ public class DataListBean extends AbstractStatelessSessionBean
 	{
 		return (new DataListOperations()).save(dataList);
 	}
+
+	public List<EntityRecord> getEntityRecord(Long entityId) throws RemoteException {
+		return (new DataListOperations()).getEntityRecord(entityId);
+	}
+
+	
+
+	
 }
