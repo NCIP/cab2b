e399605f7c5377cf27d583ddacd9e2766a4363ff
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-04-19 05:21:49 +0000

    Added getExperiment(), getDataListEntityNames() method declaration

--- source/common/main/edu/wustl/cab2b/common/experiment/ExperimentBusinessInterface.java
+++ source/common/main/edu/wustl/cab2b/common/experiment/ExperimentBusinessInterface.java
@@ -1,9 +1,14 @@
 package edu.wustl.cab2b.common.experiment;
 
 import java.rmi.RemoteException;
+import java.util.Set;
 import java.util.Vector;
 
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.common.dynamicextensions.exception.DynamicExtensionsApplicationException;
+import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
 import edu.wustl.cab2b.common.BusinessInterface;
+import edu.wustl.cab2b.common.domain.Experiment;
 import edu.wustl.common.exception.BizLogicException;
 import edu.wustl.common.security.exceptions.UserNotAuthorizedException;
 import edu.wustl.common.util.dbManager.DAOException;
@@ -18,5 +23,7 @@ public interface ExperimentBusinessInterface extends BusinessInterface {
 	
 	public Vector getExperimentHierarchy() throws ClassNotFoundException, DAOException, RemoteException;
 	
+	public Experiment getExperiment(Long id) throws RemoteException, DAOException;
 	
+	public Set<EntityInterface> getDataListEntityNames(Experiment exp) throws RemoteException;
 }
