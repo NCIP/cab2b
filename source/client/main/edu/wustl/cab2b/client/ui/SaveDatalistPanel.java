b64b7062a6e8870b7d411a6d62b169fab9edf333
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-04-26 09:15:34 +0000

    bug 4170

--- source/client/main/edu/wustl/cab2b/client/ui/SaveDatalistPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/SaveDatalistPanel.java
@@ -19,12 +19,9 @@ import java.util.Date;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
-import edu.common.dynamicextensions.exception.DynamicExtensionsApplicationException;
-import edu.common.dynamicextensions.exception.DynamicExtensionsSystemException;
 import edu.wustl.cab2b.client.ui.controls.Cab2bButton;
 import edu.wustl.cab2b.client.ui.controls.Cab2bLabel;
 import edu.wustl.cab2b.client.ui.controls.Cab2bPanel;
@@ -37,9 +34,6 @@ import edu.wustl.cab2b.common.datalist.DataListBusinessInterface;
 import edu.wustl.cab2b.common.datalist.DataListHome;
 import edu.wustl.cab2b.common.domain.DataListMetadata;
 import edu.wustl.cab2b.common.ejb.EjbNamesConstants;
-import edu.wustl.common.exception.BizLogicException;
-import edu.wustl.common.security.exceptions.UserNotAuthorizedException;
-import edu.wustl.common.util.dbManager.DAOException;
 import edu.wustl.common.util.logger.Logger;
 
 /**
@@ -47,194 +41,191 @@ import edu.wustl.common.util.logger.Logger;
  *
  */
 
-public class SaveDatalistPanel extends Cab2bPanel
-{
+public class SaveDatalistPanel extends Cab2bPanel {
 
-	private Cab2bLabel titleLabel;
-	private Cab2bLabel descLabel;
-	private Cab2bTextField txtTitle;
-	private JTextArea txtDesc;
-	private Cab2bButton cancelButton;
-	private Cab2bButton saveButton;
-	private Cab2bPanel centerPanel;
-	private Cab2bPanel bottomPanel;
-	private JScrollPane m_srollPane;
+    private Cab2bLabel titleLabel;
 
-	JDialog dialog;
-	MainSearchPanel mainSearchPanel;
-	
-	//flag used to indicate status of data list  
-	public static boolean isDataListSaved = false;
+    private Cab2bLabel descLabel;
 
-	SaveDatalistPanel(MainSearchPanel mainSearchpanel)
-	{
-		this.mainSearchPanel = mainSearchpanel;
-		initGUIWithGBL();
-	}
+    private Cab2bTextField txtTitle;
 
-	private void initGUIWithGBL()
-	{
-		titleLabel = new Cab2bLabel("Title");
-		descLabel = new Cab2bLabel("Description");
-		txtTitle = new Cab2bTextField();
+    private JTextArea txtDesc;
 
-		txtDesc = new JTextArea();
-		m_srollPane = new JScrollPane(txtDesc);
+    private Cab2bButton cancelButton;
 
-		cancelButton = new Cab2bButton("Cancel");
-		cancelButton.addActionListener(new ActionListener()
-		{
+    private Cab2bButton saveButton;
 
-			public void actionPerformed(ActionEvent arg0)
-			{
-				Logger.out.info("disposing save data list dialog... ");
-				dialog.dispose();
-			}
-		});
+    private Cab2bPanel centerPanel;
 
-		saveButton = new Cab2bButton("Save");		
-		saveButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent arg0)
-			{
-				isDataListSaved = true;
+    private Cab2bPanel bottomPanel;
 
-				String dataListName = txtTitle.getText();
-				if (dataListName == null || dataListName.equals(""))
-				{
-					dataListName = "" + new Date();
-				}
-				String dataListDesc = txtDesc.getText();
+    private JScrollPane m_srollPane;
 
-				DataListMetadata dataListAnnotation = new DataListMetadata();
-				dataListAnnotation.setName(dataListName);
-				dataListAnnotation.setDescription(dataListDesc);
-				dataListAnnotation.setCreatedOn(new Date());
-				dataListAnnotation.setLastUpdatedOn(new Date());
+    JDialog dialog;
 
-				MainSearchPanel.getDataList().setDataListAnnotation(dataListAnnotation);
-				
-				CustomSwingWorker sw = new CustomSwingWorker(SaveDatalistPanel.this)
-				{
-					Long id;
-					
-					@Override
-					protected void doNonUILogic() throws RuntimeException
-					{
-						DataListBusinessInterface dataListBI = (DataListBusinessInterface) CommonUtils
-						.getBusinessInterface(EjbNamesConstants.DATALIST_BEAN, DataListHome.class,
-								SaveDatalistPanel.this);
-						
-						try
-						{
-							id = dataListBI.saveDataList(MainSearchPanel.getDataList());
-							
-							MainSearchPanel.savedDataListMetadata = dataListBI.retrieveDataListMetadata(id);
-							
-							Logger.out.info("data list saved successfully (in entity with id) : "+id);
-							
-						}
-						catch (RemoteException e)
-						{
-							e.printStackTrace();
-						}
-						finally
-						{
-							dialog.dispose();
-						}
-					}
+    MainSearchPanel mainSearchPanel;
 
-					@Override
-					protected void doUIUpdateLogic() throws RuntimeException
-					{
-						dialog.dispose();
-						if(id > 0)
-							JOptionPane.showMessageDialog(mainSearchPanel, "Data List " + MainSearchPanel.getDataList().getDataListAnnotation().getName() + " saved successfully !");
-						else
-							Logger.out.debug("data list not saved ! "+id);
-						Logger.out.info("datalist id : "+MainSearchPanel.getDataList().getDataListAnnotation().getId());
-						Logger.out.info("entity id for dl : "+MainSearchPanel.getDataList().getDataListAnnotation().getEntityId());
-					}
-					
-				};
-				sw.start();			
-			}
+    //flag used to indicate status of data list  
+    public static boolean isDataListSaved = false;
 
-		});
+    SaveDatalistPanel(MainSearchPanel mainSearchpanel) {
+        this.mainSearchPanel = mainSearchpanel;
+        initGUIWithGBL();
+    }
 
-		GridBagLayout gbl = new GridBagLayout();
-		centerPanel = new Cab2bPanel();
-		centerPanel.setLayout(gbl);
+    private void initGUIWithGBL() {
+        titleLabel = new Cab2bLabel("Title");
+        descLabel = new Cab2bLabel("Description");
+        txtTitle = new Cab2bTextField();
 
-		this.setLayout(new BorderLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
+        txtDesc = new JTextArea();
+        m_srollPane = new JScrollPane(txtDesc);
 
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.gridwidth = 1;
-		gbc.gridheight = 1;
-		gbc.insets = new Insets(5, 5, 5, 5);
-		centerPanel.add(titleLabel, gbc);
+        cancelButton = new Cab2bButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
 
-		gbc.gridx = 1;
-		gbc.gridy = 0;
-		gbc.gridwidth = 4;
-		gbc.gridheight = 1;
-		gbc.insets = new Insets(5, 5, 5, 5);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		centerPanel.add(txtTitle, gbc);
+            public void actionPerformed(ActionEvent arg0) {
+                Logger.out.info("disposing save data list dialog... ");
+                dialog.dispose();
+            }
+        });
 
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		gbc.gridwidth = 1;
-		gbc.gridheight = 1;
+        saveButton = new Cab2bButton("Save");
+        saveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                isDataListSaved = true;
 
-		gbc.insets = new Insets(5, 5, 5, 5);
-		gbc.fill = GridBagConstraints.NONE;
-		centerPanel.add(descLabel, gbc);
+                String dataListName = txtTitle.getText();
+                if (dataListName == null || dataListName.equals("")) {
+                    dataListName = "" + new Date();
+                }
+                String dataListDesc = txtDesc.getText();
 
-		gbc.gridx = 1;
-		gbc.gridy = 1;
-		gbc.gridwidth = 4;
-		gbc.gridheight = 3;
-		gbc.insets = new Insets(5, 5, 5, 5);
-		gbc.weightx = 1.0;
-		gbc.weighty = 1.0;
-		gbc.fill = GridBagConstraints.BOTH;
-		centerPanel.add(m_srollPane, gbc);
+                DataListMetadata dataListAnnotation = new DataListMetadata();
+                dataListAnnotation.setName(dataListName);
+                dataListAnnotation.setDescription(dataListDesc);
+                dataListAnnotation.setCreatedOn(new Date());
+                dataListAnnotation.setLastUpdatedOn(new Date());
 
-		bottomPanel = new Cab2bPanel();
-		FlowLayout flowLayout = new FlowLayout();
-		flowLayout.setAlignment(FlowLayout.RIGHT);
-		flowLayout.setHgap(10);
-		bottomPanel.setLayout(flowLayout);
-		bottomPanel.add(cancelButton);
-		bottomPanel.add(saveButton);
+                MainSearchPanel.getDataList().setDataListAnnotation(dataListAnnotation);
 
-		this.add(centerPanel, BorderLayout.CENTER);
-		this.add(bottomPanel, BorderLayout.SOUTH);
-	}
+                CustomSwingWorker sw = new CustomSwingWorker(SaveDatalistPanel.this) {
+                    Long id;
 
-	public JDialog showInDialog()
-	{
-		Dimension dimension = MainFrame.mainframeScreenDimesion;
-		dialog = WindowUtilities.setInDialog(NewWelcomePanel.mainFrame, this, "Save Data List",
-				new Dimension((int) (dimension.width * 0.35), (int) (dimension.height * 0.30)),
-				true, false);
+                    @Override
+                    protected void doNonUILogic() throws RuntimeException {
+                        DataListBusinessInterface dataListBI = (DataListBusinessInterface) CommonUtils.getBusinessInterface(
+                                                                                                                            EjbNamesConstants.DATALIST_BEAN,
+                                                                                                                            DataListHome.class,
+                                                                                                                            SaveDatalistPanel.this);
 
-		Logger.out.info("dialog initialized ########## " + dialog);
-		dialog.setVisible(true);
-		return dialog;
-	}
+                        try {
+                            id = dataListBI.saveDataList(MainSearchPanel.getDataList());
 
-	public static void main(String[] args)
-	{
-		SaveDatalistPanel obj = new SaveDatalistPanel(null);
+                            MainSearchPanel.savedDataListMetadata = dataListBI.retrieveDataListMetadata(id);
 
-		JFrame frame = new JFrame("Experiment");
-		frame.setSize(550, 400);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(obj);
-		frame.setVisible(true);
-	}
+                            Logger.out.info("data list saved successfully (in entity with id) : " + id);
+
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        } finally {
+                            dialog.dispose();
+                        }
+                    }
+
+                    @Override
+                    protected void doUIUpdateLogic() throws RuntimeException {
+                        dialog.dispose();
+                        if (id > 0) {
+                            /*JOptionPane.showMessageDialog(mainSearchPanel, "Data List " + MainSearchPanel.getDataList().getDataListAnnotation().getName() + " saved successfully !");*/
+                            SearchNavigationPanel.messageLabel.setText("* Datalist '"
+                                    + MainSearchPanel.getDataList().getDataListAnnotation().getName()
+                                    + "' saved successfully .");
+                            updateUI();
+                        } else
+                            Logger.out.debug("data list not saved ! " + id);
+                        Logger.out.info("datalist id : "
+                                + MainSearchPanel.getDataList().getDataListAnnotation().getId());
+                        Logger.out.info("entity id for dl : "
+                                + MainSearchPanel.getDataList().getDataListAnnotation().getEntityId());
+                    }
+
+                };
+                sw.start();
+            }
+
+        });
+
+        GridBagLayout gbl = new GridBagLayout();
+        centerPanel = new Cab2bPanel();
+        centerPanel.setLayout(gbl);
+
+        this.setLayout(new BorderLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        centerPanel.add(titleLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridwidth = 4;
+        gbc.gridheight = 1;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        centerPanel.add(txtTitle, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.NONE;
+        centerPanel.add(descLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = 4;
+        gbc.gridheight = 3;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.BOTH;
+        centerPanel.add(m_srollPane, gbc);
+
+        bottomPanel = new Cab2bPanel();
+        FlowLayout flowLayout = new FlowLayout();
+        flowLayout.setAlignment(FlowLayout.RIGHT);
+        flowLayout.setHgap(10);
+        bottomPanel.setLayout(flowLayout);
+        bottomPanel.add(cancelButton);
+        bottomPanel.add(saveButton);
+
+        this.add(centerPanel, BorderLayout.CENTER);
+        this.add(bottomPanel, BorderLayout.SOUTH);
+    }
+
+    public JDialog showInDialog() {
+        Dimension dimension = MainFrame.mainframeScreenDimesion;
+        dialog = WindowUtilities.setInDialog(NewWelcomePanel.mainFrame, this, "Save Data List", new Dimension(
+                (int) (dimension.width * 0.35), (int) (dimension.height * 0.30)), true, false);
+
+        Logger.out.info("dialog initialized ########## " + dialog);
+        dialog.setVisible(true);
+        return dialog;
+    }
+
+    public static void main(String[] args) {
+        SaveDatalistPanel obj = new SaveDatalistPanel(null);
+
+        JFrame frame = new JFrame("Experiment");
+        frame.setSize(550, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(obj);
+        frame.setVisible(true);
+    }
 }
