a0fd9679aa85b6d13869158b989341bba895f6d9
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-04-27 08:10:11 +0000

    Formatted the code. Javadoc added

--- source/client/main/edu/wustl/cab2b/client/ui/query/Utility.java
+++ source/client/main/edu/wustl/cab2b/client/ui/query/Utility.java
@@ -1,16 +1,4 @@
-/**
- * <p>Title: Utility Class>
- * <p>Description:  This class contains the utility methods required for
- * building the Query object from the UI.</p>
- * Copyright:    Copyright (c) year
- * Company: Washington University, School of Medicine, St. Louis.
- * @author Gautam Shetty
- * @version 1.00
- */
-
 package edu.wustl.cab2b.client.ui.query;
-
-import java.util.List;
 
 import edu.wustl.common.querysuite.metadata.associations.IAssociation;
 import edu.wustl.common.querysuite.metadata.associations.IInterModelAssociation;
@@ -20,75 +8,73 @@ import edu.wustl.common.querysuite.queryobject.LogicalOperator;
 import edu.wustl.common.querysuite.queryobject.RelationalOperator;
 
 /**
- * This class contains the utility methods required for
- * building the Query object from the UI.
- * @author gautam_shetty
+ * This class contains the utility methods required for building the UI.
+ * @author Chandrakant Talele
+ * @author Gautam Shetty
  */
-public class Utility
-{
-    
+public class Utility {
+
     /**
      * Returns the RelationalOperator, given the relational operator string. 
      * @param operator The relational operator string.
      * @return the RelationalOperator, given the relational operator string.
      */
-    public static RelationalOperator getRelationalOperator(String operator)
-    {
-       return RelationalOperator.getOperatorForStringRepresentation(operator);
+    public static RelationalOperator getRelationalOperator(String operator) {
+        return RelationalOperator.getOperatorForStringRepresentation(operator);
     }
-    
-    public static String displayStringForRelationalOperator(RelationalOperator relationalOperator)
-    {
-    	return relationalOperator.getStringRepresentation();
+
+    /**
+     * @param relationalOperator Input Relational Operator
+     * @return Display string for given operator 
+     */
+    public static String displayStringForRelationalOperator(RelationalOperator relationalOperator) {
+        return relationalOperator.getStringRepresentation();
     }
-    
+
     /**
      * Returns the LogicalOperator, given the logical operator string. 
      * @param operator The logical operator string.
      * @return the LogicalOperator, given the logical operator string.
      */
-    public static LogicalOperator getLogicalOperator(String operator)
-    {
+    public static LogicalOperator getLogicalOperator(String operator) {
         LogicalOperator logicalOperator = LogicalOperator.And;
-        if (operator.equals("OR"))
-        {
+        if (operator.equals("OR")) {
             logicalOperator = LogicalOperator.Or;
         }
-        
+
         return logicalOperator;
     }
-    
-    public static String getPathDisplayString(IPath path)
-	{
-		StringBuffer sb = new StringBuffer();
-		sb.append("<HTML><B>Path</B>:");
-		List<IAssociation> list = path.getIntermediateAssociations();
-		sb.append(path.getSourceEntity().getName());
-		for(int i=0; i<list.size(); i++)
-		{
-			sb.append("<B>----></B>");
-			sb.append(list.get(i).getTargetEntity().getName());
-		}
-		sb.append("<HTML>");
-		return sb.toString();
-	}
+
     /**
-     * Method to get roleName for given association
-     * @param association
-     * @return
+     * @param path Input path
+     * @return string representation of given path
      */
-    public static String getRoleName(IAssociation association)
-	{
-		String roleName;
-		if(association instanceof IIntraModelAssociation)
-	    {
-			roleName = ((IIntraModelAssociation)association).getDynamicExtensionsAssociation().getTargetRole().getName();
-	    }
-		else
-		{
-			 roleName = ((IInterModelAssociation)association).getSourceAttribute().getName() + " = " + 
-			 ((IInterModelAssociation)association).getTargetAttribute().getName();
-		}
-		return roleName;
-	}
+    public static String getPathDisplayString(IPath path) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<HTML><B>Path</B>:");
+        sb.append(path.getSourceEntity().getName());
+        for (IAssociation association : path.getIntermediateAssociations()) {
+            sb.append("<B>----></B>");
+            sb.append(association.getTargetEntity().getName());
+        }
+        sb.append("<HTML>");
+        return sb.toString();
+    }
+
+    /**
+     * @param association Input association object
+     * @return The roleName for given association
+     */
+    public static String getRoleName(IAssociation association) {
+        String roleName = "";
+        if (association instanceof IIntraModelAssociation) {
+            IIntraModelAssociation intraModel = (IIntraModelAssociation) association;
+            roleName = intraModel.getDynamicExtensionsAssociation().getTargetRole().getName();
+        } else {
+            IInterModelAssociation interModel = (IInterModelAssociation) association;
+            roleName = interModel.getSourceAttribute().getName() + " = "
+                    + interModel.getTargetAttribute().getName();
+        }
+        return roleName;
+    }
 }
