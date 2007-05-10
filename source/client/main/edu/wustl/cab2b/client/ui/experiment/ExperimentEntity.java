bdb09d39c612d652b1cf87a6b5056a0d08b5afe6
hrishikesh_rajpathak <hrishikesh_rajpathak@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-05-10 12:32:04 +0000

    Experiment name "abc" removed. Experiment name getting displayed properly

--- source/client/main/edu/wustl/cab2b/client/ui/experiment/ExperimentEntity.java
+++ source/client/main/edu/wustl/cab2b/client/ui/experiment/ExperimentEntity.java
@@ -1,24 +1,23 @@
 package edu.wustl.cab2b.client.ui.experiment;
 
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
-import edu.wustl.common.util.logger.Logger;
 
-public class ExperimentEntity 
-{
+public class ExperimentEntity {
 	private EntityInterface m_entity;
-	public void setEntityInterface(EntityInterface entity)
-	{
+
+	public void setEntityInterface(EntityInterface entity) {
 		m_entity = entity;
 	}
-	public EntityInterface getEntityInterface()
-	{
+
+	public EntityInterface getEntityInterface() {
 		return m_entity;
 	}
-	public String toString()
-	{
-	//String displyaName = edu.wustl.cab2b.common.util.Utility.getTaggedValue(m_entity.getTaggedValueCollection(),edu.wustl.cab2b.common.util.Constants.ENTITY_DISPLAY_NAME).getValue();			 
-		String displyaName ="abc";
+
+	public String toString() {
+		String displyaName = edu.wustl.cab2b.common.util.Utility.getTaggedValue(
+				m_entity.getTaggedValueCollection(),
+				edu.wustl.cab2b.common.util.Constants.ENTITY_DISPLAY_NAME).getValue();
 		return displyaName;
-		
+
 	}
 }
