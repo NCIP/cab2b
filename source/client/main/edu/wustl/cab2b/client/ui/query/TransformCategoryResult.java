1004df9107f85bc5956f61efde0f6a49752de05a
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-05-11 08:21:16 +0000

    Updated code related to displayName

--- source/client/main/edu/wustl/cab2b/client/ui/query/TransformCategoryResult.java
+++ source/client/main/edu/wustl/cab2b/client/ui/query/TransformCategoryResult.java
@@ -60,26 +60,28 @@ public class TransformCategoryResult {
 
         while (recordIter.hasNext()) {
             ICategorialClassRecord iCategorialClassRecord = (ICategorialClassRecord) recordIter.next();
-            Logger.out.info("Set DisplayName :" + iCategorialClassRecord.getCategorialClass().toString());
+            //Logger.out.info("Set DisplayName :" + edu.wustl.cab2b.common.util.Utility.getDisplayName( iCategorialClassRecord.getCategorialClass().getCategorialClassEntity()));           
+                      
+            String  displayName = edu.wustl.cab2b.common.util.Utility.getDisplayName( iCategorialClassRecord.getCategorialClass().getCategorialClassEntity()) + "_"+iCategorialClassRecord.getCategorialClass().getCategorialClassEntity().getId();
+            
 
             //record node
             B2BTreeNode iCategorialClassRecordNode = new B2BTreeNode();
-            iCategorialClassRecordNode.setDisplayName(iCategorialClassRecord.getCategorialClass().toString());
+            iCategorialClassRecordNode.setDisplayName(displayName);
             iCategorialClassRecordNode.setValue(null);
 
             //geting attribute list for the iCategorialClassRecord
             Map<CategorialAttribute, String> categorialAttributeMap = iCategorialClassRecord.getAttributesValues();
 
             if (categorialAttributeMap != null) {
-                setAttributeToB2BTreeNode(iCategorialClassRecordNode, categorialAttributeMap);
-                /*     parentTreeNode.addChild(iCategorialClassRecordNode);
-                 return;*/
+                setAttributeToB2BTreeNode(iCategorialClassRecordNode, categorialAttributeMap);            
             }
 
             //getting categorial class node 
             Map<CategorialClass, List<ICategorialClassRecord>> categorialClassRecordMap = iCategorialClassRecord.getChildrenCategorialClassRecords();
             Set categorialClassRecordMapKeys = categorialClassRecordMap.keySet();
             Iterator iter = categorialClassRecordMapKeys.iterator();
+          
             while (iter.hasNext()) {
                 //getting the ICategoryRecordList
                 List<ICategorialClassRecord> newICategorialClassRecordList = categorialClassRecordMap.get((CategorialClass) iter.next());
@@ -93,8 +95,12 @@ public class TransformCategoryResult {
                 //adding this child to parent 
                 iCategorialClassRecordNode.addChild(child);
 
-                Logger.out.info("PARENT :" + iCategorialClassRecordNode + "   CHILD :" + child);
+                //Logger.out.info("PARENT :" + iCategorialClassRecordNode + "   CHILD :" + child);
             }
+            
+            //setting display name for group node by removing _ String   
+            parentTreeNode.setDisplayName(iCategorialClassRecordNode.toString().substring(0,iCategorialClassRecordNode.toString().lastIndexOf("_")));
+            
             //adding iCategorialClassRecordNode to parent
             parentTreeNode.addChild(iCategorialClassRecordNode);
         }
@@ -115,16 +121,18 @@ public class TransformCategoryResult {
                 CategorialAttribute categorialAttribute = (CategorialAttribute) categorialAttributeMapKeysIter.next();
                 B2BTreeNode attributeNode = new B2BTreeNode();
                 attributeNode.setDisplayName(categorialAttribute.getCategoryAttribute().getName());
-                Logger.out.info("Categorial Attribute Name :"
-                        + categorialAttribute.getCategoryAttribute().getName());
-                Logger.out.info("Categorial Attribute Value :" + categorialAttributeMap.get(categorialAttribute));
+                /*Logger.out.info("Categorial Attribute Name :"
+                        + categorialAttribute.getCategoryAttribute().getName());*/
+/*                Logger.out.info("Categorial Attribute Value :" + categorialAttributeMap.get(categorialAttribute));*/
 
                 attributeNode.setChildren(null);
                 attributeNode.setValue(categorialAttributeMap.get(categorialAttribute));
 
                 //adding attribute as child to the main node
-                Logger.out.info(" PARENT :" + parentTreeNode + " Attribute  CHILD Node:" + attributeNode);
                 parentTreeNode.addChild(attributeNode);
+                
+                /*Logger.out.info(" PARENT :" + parentTreeNode + " Attribute  CHILD Node:" + attributeNode);*/
+                
             }
         }
         return parentTreeNode;
