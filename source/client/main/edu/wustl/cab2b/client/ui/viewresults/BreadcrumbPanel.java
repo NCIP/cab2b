1683c2e634941772ff99f819d1de95c08f784bb8
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-05-15 08:46:59 +0000

    Label "paths" changed to "path"

--- source/client/main/edu/wustl/cab2b/client/ui/viewresults/BreadcrumbPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/viewresults/BreadcrumbPanel.java
@@ -1,4 +1,5 @@
 package edu.wustl.cab2b.client.ui.viewresults;
+
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
@@ -10,56 +11,44 @@ import edu.wustl.cab2b.client.ui.controls.Cab2bHyperlink;
 import edu.wustl.cab2b.client.ui.controls.Cab2bPanel;
 import edu.wustl.common.util.logger.Logger;
 
+public class BreadcrumbPanel extends Cab2bPanel {
 
+    private ActionListener m_breadCrumbListener = null;
 
+    private Vector m_breadCrumbs = null;
 
-public class BreadcrumbPanel extends Cab2bPanel
-{
-	
-	
-	private ActionListener m_breadCrumbListener= null;
-	
-	private Vector m_breadCrumbs = null;
-
-	
-	/**
+    /**
      * Constructor
      */
-	BreadcrumbPanel(ActionListener breadCrumbListener,Vector breadCrumbs)
-	{
-		this.m_breadCrumbListener = breadCrumbListener; 
-		this.m_breadCrumbs = breadCrumbs;
-		initGUI();
-	}
-	
-	/**
+    BreadcrumbPanel(ActionListener breadCrumbListener, Vector breadCrumbs) {
+        this.m_breadCrumbListener = breadCrumbListener;
+        this.m_breadCrumbs = breadCrumbs;
+        initGUI();
+    }
+
+    /**
      * Creates the breadcrumb based on the elements in the vector.
      */
-	private void initGUI()
-	{			
-		this.setLayout(new FlowLayout(5));
-		JLabel label  = new JLabel("Paths: ");
-		label.setPreferredSize(new Dimension(100,20));
-		
-		this.add(new JLabel("Paths: "));  // Adding "Paths" JLabel.
-		int size = this.m_breadCrumbs.size();
-		Logger.out.info("SIZE : "+size);
-		for(int i = 0; i < size; i++)
-		{
-			/* Get the text from the vector.*/
-			String strWholeText = (String)this.m_breadCrumbs.get(i);			
-			Cab2bHyperlink breadCrumbHyperlink = new Cab2bHyperlink();			
-			/*Set the whole string as the user object for this link.*/
-			breadCrumbHyperlink.setUserObject(strWholeText);
-					
-			String strJustText = strWholeText.substring(strWholeText.indexOf("#")+1);
-			/*Set the display name appropriatly*/
-			breadCrumbHyperlink.setText(strJustText);		
-			breadCrumbHyperlink.addActionListener(this.m_breadCrumbListener);
-			this.add(breadCrumbHyperlink);
-			if(i < size-1)
-				this.add(new JLabel(" -> "));
-		}
-	}
-    
+    private void initGUI() {
+        this.setLayout(new FlowLayout(5));
+        this.add(new JLabel("Path : ")); // Adding "Paths" JLabel.
+        int size = this.m_breadCrumbs.size();
+        Logger.out.info("SIZE : " + size);
+        for (int i = 0; i < size; i++) {
+            /* Get the text from the vector.*/
+            String strWholeText = (String) this.m_breadCrumbs.get(i);
+            Cab2bHyperlink breadCrumbHyperlink = new Cab2bHyperlink();
+            /*Set the whole string as the user object for this link.*/
+            breadCrumbHyperlink.setUserObject(strWholeText);
+
+            String strJustText = strWholeText.substring(strWholeText.indexOf("#") + 1);
+            /*Set the display name appropriatly*/
+            breadCrumbHyperlink.setText(strJustText);
+            breadCrumbHyperlink.addActionListener(this.m_breadCrumbListener);
+            this.add(breadCrumbHyperlink);
+            if (i < size - 1)
+                this.add(new JLabel(" -> "));
+        }
+    }
+
 }
