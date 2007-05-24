8f68700c9428f0108800a437d147ef33e5bb4d4d
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-05-24 10:08:30 +0000

    marked selected element as non-hyperlink

--- source/client/main/edu/wustl/cab2b/client/ui/viewresults/BreadcrumbPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/viewresults/BreadcrumbPanel.java
@@ -31,26 +31,37 @@ public class BreadcrumbPanel extends Cab2bPanel {
      */
     private void initGUI() {
         this.setLayout(new RiverLayout());
-        JLabel label = new JLabel("Path: ");
-        label.setPreferredSize(new Dimension(100, 20));
-
-        this.add(new JLabel("Paths: ")); // Adding "Paths" JLabel.
+        //JLabel label = ;
+        //label.setPreferredSize(new Dimension(100, 20));
+        this.add(new JLabel("Path: ")); // Adding "Paths" JLabel.
         int size = this.m_breadCrumbs.size();
         Logger.out.info("SIZE : " + size);
         for (int i = 0; i < size; i++) {
-            /* Get the text from the vector.*/
+            //Get the text from the vector
             String strWholeText = (String) this.m_breadCrumbs.get(i);
+            String strJustText = strWholeText.substring(strWholeText.indexOf("#") + 1);
+            if(i==size-1) {
+                //if last element, add lable instead of hyperlink
+                JLabel selectedObject = new JLabel(strJustText);
+                selectedObject.setForeground(Cab2bHyperlink.getUnclickedHyperlinkColor());
+                this.add(selectedObject);
+                break;
+            }
             Cab2bHyperlink breadCrumbHyperlink = new Cab2bHyperlink();
-            /*Set the whole string as the user object for this link.*/
+            //Set the whole string as the user object for this link
             breadCrumbHyperlink.setUserObject(strWholeText);
 
-            String strJustText = strWholeText.substring(strWholeText.indexOf("#") + 1);
-            /*Set the display name appropriatly*/
+            
+            //Set the display name appropriatly
             breadCrumbHyperlink.setText(strJustText);
             breadCrumbHyperlink.addActionListener(this.m_breadCrumbListener);
             this.add(breadCrumbHyperlink);
-            if (i < size - 1)
-                this.add(new JLabel(" >> "));
+            if (i < size - 1) {
+                JLabel arrow = new JLabel(" >> ");
+                //arrow will match with other links due to this
+                arrow.setForeground(Cab2bHyperlink.getUnclickedHyperlinkColor());
+                this.add(arrow);
+            }
         }
 
     }
