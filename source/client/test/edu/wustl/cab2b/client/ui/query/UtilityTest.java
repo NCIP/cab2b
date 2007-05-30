5c7664d0248c064bc6844bc2b73e9f84835d7db3
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-05-30 15:14:17 +0000

    added test cases

---
+++ source/client/test/edu/wustl/cab2b/client/ui/query/UtilityTest.java
@@ -0,0 +1,128 @@
+package edu.wustl.cab2b.client.ui.query;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import junit.framework.TestCase;
+import edu.common.dynamicextensions.domain.DomainObjectFactory;
+import edu.common.dynamicextensions.domaininterface.AssociationInterface;
+import edu.common.dynamicextensions.domaininterface.AttributeInterface;
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.common.dynamicextensions.domaininterface.RoleInterface;
+import edu.wustl.cab2b.client.ui.util.ClientConstants;
+import edu.wustl.cab2b.common.queryengine.result.IQueryResult;
+import edu.wustl.cab2b.common.queryengine.result.IRecord;
+import edu.wustl.cab2b.common.queryengine.result.QueryResultFactory;
+import edu.wustl.common.querysuite.metadata.associations.IAssociation;
+import edu.wustl.common.querysuite.metadata.associations.impl.InterModelAssociation;
+import edu.wustl.common.querysuite.metadata.associations.impl.IntraModelAssociation;
+import edu.wustl.common.querysuite.metadata.path.Path;
+import edu.wustl.common.querysuite.queryobject.LogicalOperator;
+
+/**
+ * @author Chandrakant Talele
+ */
+public class UtilityTest extends TestCase {
+    DomainObjectFactory fact = DomainObjectFactory.getInstance();
+
+    public void testGetRoleNameForIntraModel() {
+        String name = "someName";
+
+        RoleInterface role = fact.createRole();
+        role.setName(name);
+        AssociationInterface association = fact.createAssociation();
+        association.setTargetRole(role);
+        IntraModelAssociation intraModelAssociation = new IntraModelAssociation(association);
+        assertEquals(name, Utility.getRoleName(intraModelAssociation));
+    }
+
+    public void testGetRoleNameForInterModel() {
+        String srcName = "srcName";
+        String tgtName = "tgtName";
+
+        AttributeInterface src = fact.createStringAttribute();
+        src.setName(srcName);
+
+        AttributeInterface tgt = fact.createStringAttribute();
+        tgt.setName(tgtName);
+
+        InterModelAssociation a = new InterModelAssociation(src, tgt);
+        assertEquals(srcName + " = " + tgtName, Utility.getRoleName(a));
+    }
+
+    public void testGetRecordNumNoRecords() {
+        IQueryResult queryResult = QueryResultFactory.createResult(fact.createEntity());
+        assertEquals(0, Utility.getRecordNum(queryResult));
+    }
+
+    public void testGetRecordNum() {
+        int size = 4;
+        int urls = 3;
+        IQueryResult queryResult = QueryResultFactory.createResult(fact.createEntity());
+        List<IRecord> list = new ArrayList<IRecord>(size);
+        for (int i = 0; i < size; i++) {
+            list.add(QueryResultFactory.createRecord(new HashSet<AttributeInterface>(), null));
+        }
+        for (int i = 0; i < urls; i++) {
+            queryResult.addRecords("URL" + i, list);
+        }
+        assertEquals(size * urls, Utility.getRecordNum(queryResult));
+    }
+
+    public void testGetPathDisplayString() {
+        EntityInterface source = fact.createEntity();
+        source.setName("sourceEntity");
+
+        EntityInterface target = fact.createEntity();
+        target.setName("targetEntity");
+
+        AssociationInterface association = fact.createAssociation();
+        association.setTargetEntity(target);
+
+        IntraModelAssociation intraModelAssociation = new IntraModelAssociation(association);
+
+        List<IAssociation> list = new ArrayList<IAssociation>();
+        list.add(intraModelAssociation);
+
+        Path path = new Path(source, target, list);
+        String str = Utility.getPathDisplayString(path);
+        assertEquals("<HTML><B>Path</B>:sourceEntity<B>----></B>targetEntity<HTML>", str);
+    }
+
+    public void testGetPathDisplayStringTwoAssociations() {
+        EntityInterface source = fact.createEntity();
+        source.setName("sourceEntity");
+
+        EntityInterface target = fact.createEntity();
+        target.setName("targetEntity");
+
+        EntityInterface inter = fact.createEntity();
+        inter.setName("inter");
+
+        AssociationInterface association1 = fact.createAssociation();
+        association1.setTargetEntity(inter);
+
+        AssociationInterface association2 = fact.createAssociation();
+        association2.setTargetEntity(target);
+
+        IntraModelAssociation intraModelAssociation1 = new IntraModelAssociation(association1);
+        IntraModelAssociation intraModelAssociation2 = new IntraModelAssociation(association2);
+
+        List<IAssociation> list = new ArrayList<IAssociation>();
+        list.add(intraModelAssociation1);
+        list.add(intraModelAssociation2);
+
+        Path path = new Path(source, target, list);
+        String str = Utility.getPathDisplayString(path);
+        assertEquals("<HTML><B>Path</B>:sourceEntity<B>----></B>inter<B>----></B>targetEntity<HTML>", str);
+    }
+
+    public void testGetLogicalOperatorOR() {
+        assertEquals(LogicalOperator.Or, Utility.getLogicalOperator(ClientConstants.OPERATOR_OR));
+    }
+
+    public void testGetLogicalOperatorAND() {
+        assertEquals(LogicalOperator.And, Utility.getLogicalOperator(ClientConstants.OPERATOR_AND));
+    }
+}
