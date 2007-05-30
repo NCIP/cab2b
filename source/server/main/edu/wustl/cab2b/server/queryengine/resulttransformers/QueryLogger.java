dd108334a2233ff0316d3b61a97a61b7d1f52ee1
srinath_k <srinath_k@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-05-30 08:29:45 +0000

    *** empty log message ***

--- source/server/main/edu/wustl/cab2b/server/queryengine/resulttransformers/QueryLogger.java
+++ source/server/main/edu/wustl/cab2b/server/queryengine/resulttransformers/QueryLogger.java
@@ -2,6 +2,7 @@ package edu.wustl.cab2b.server.queryengine.resulttransformers;
 
 import edu.wustl.common.util.logger.Logger;
 import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.cqlquery.CQLQuery;
 import gov.nih.nci.cagrid.dcql.DCQLQuery;
 
 import java.io.File;
@@ -10,14 +11,12 @@ import java.util.Date;
 
 import javax.xml.namespace.QName;
 
-public class DcqlLogger {
-    private static final String LOG_BASE_DIR = System.getProperty("user.home")
-            + "/dcqlLog";
+public class QueryLogger {
+    private static final String LOG_BASE_DIR = System.getProperty("user.home") + "/cab2bQueries";
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
-            "d MMM yyyy hh_mm_ss a");
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("d MMM yyyy hh_mm_ss a");
 
-    private static final String LOG_FILE_NAME_PREFIX = "dcql_";
+    private static final String LOG_FILE_NAME_PREFIX = "query_";
 
     private final String logFolderName;
 
@@ -27,7 +26,7 @@ public class DcqlLogger {
         new File(LOG_BASE_DIR).mkdir();
     }
 
-    public DcqlLogger() {
+    public QueryLogger() {
         logFolderName = LOG_BASE_DIR + "/" + getCurrentTime();
         new File(logFolderName).mkdir();
     }
@@ -47,10 +46,22 @@ public class DcqlLogger {
         }
         try {
             Utils.serializeDocument(getLogFilePath(), dcqlQuery, new QName(
-                    "http://caGrid.caBIG/1.0/gov.nih.nci.cagrid.dcql",
-                    "DCQLQuery"));
+                    "http://caGrid.caBIG/1.0/gov.nih.nci.cagrid.dcql", "DCQLQuery"));
         } catch (Exception e) {
             Logger.out.info("Could not log dcql.");
+        }
+
+    }
+
+    public void log(CQLQuery cqlQuery) {
+        if (!isLogEnabled()) {
+            return;
+        }
+        try {
+            Utils.serializeDocument(getLogFilePath(), cqlQuery, new QName(
+                    "http://caGrid.caBIG/1.0/gov.nih.nci.cagrid.cqlquery", "CQLQuery"));
+        } catch (Exception e) {
+            Logger.out.info("Could not log cql.");
         }
 
     }
