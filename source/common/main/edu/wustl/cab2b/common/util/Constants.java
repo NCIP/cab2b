04b3d0e6db30285d0123fb8707cbc9304b5ea6f7
chetan_patil <chetan_patil@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-06-11 05:51:55 +0000

    Removed unnecessary constants

--- source/common/main/edu/wustl/cab2b/common/util/Constants.java
+++ source/common/main/edu/wustl/cab2b/common/util/Constants.java
@@ -14,95 +14,85 @@ import java.awt.Dimension;
  * Constants class for the server.
  * @author gautam_shetty
  */
-public class Constants
-{
+public class Constants {
     public static final int CLASS = 1;
-    
+
     public static final int ATTRIBUTE = 2;
-    
-    public static final int CLASS_WITH_DESCRIPTION=4;
-    
-    public static final int ATTRIBUTE_WITH_DESCRIPTION=5;
-    
+
+    public static final int CLASS_WITH_DESCRIPTION = 4;
+
+    public static final int ATTRIBUTE_WITH_DESCRIPTION = 5;
+
     public static final int PV = 3;
-    
+
     public static final int BASED_ON_TEXT = 0;
-    
+
     public static final int BASED_ON_CONCEPT_CODE = 1;
-    
+
     public static final int INLUDE_CLASSES_CLASS = 6;
-    
+
     public static final int INLUDE_CLASSES_ATTRIBUTE = 7;
-    
+
     public static final String OLD_ENTITY_ID_TAG_NAME = "original_entity_id";
-    
+
     public static final String ENTITY_DISPLAY_NAME = "original_entity_display_name";
-    
+
     public static final String FILTERED = "filtered";
-    
+
     /**
      * Constant which represents String that will be used as URL KEY in tagged value
      */
     public static final String URL_KEY = "urlKey";
-    
+
     /**
      * Name of Category Entity Group
      */
     public static final String CATEGORY_ENTITY_GROUP_NAME = "CategoryEntityGroup";
-    
+
     /**
      * Name of Data List Entity Group to exclude from Metadata search.
      */
     public static final String DATALIST_ENTITY_GROUP_NAME = "DataListEntityGroup";
-    
+
     /**
      * tagged key constant to identify a category entity.
      */
     public static final String TYPE_CATEGORY = "Category";
+
     /**
      * tagged key constant to identify project version of a entity group
      */
     public static final String PROJECT_VERSION = "ProjectVersion";
+
     /**
      * tagged key constant to identify wthether a attribute OR an association is a derived one
      */
     public static final String TYPE_DERIVED = "derived";
+
     /**
      * tagged key constant to identify wthether a attribute OR an association is a derived one
      */
     public static final String ORIGINAL_ASSOCIATION_POINTER = "actualAssociationPointer";
+
     /**
      * Represents the String used as field connector to connect intermediate path Ids, while generating the file.
      */
     public static final String CONNECTOR = "_";
-    
+
     /**
      * tagged key constant to identify cab2b entity group
      */
-    public static final String CAB2B_ENTITY_GROUP = "caB2BEntityGroup"; 
-    
-    /**
-     * tagged key constant to identify Bar Chart
-     */
-    public static final String BAR_CHART = "Bar Chart";
-    
-    /**
-     * tagged key constant to identify Line Chart
-     */
-    public static final String LINE_CHART = "Line Chart";
-    
-    /**
-     * tagged key constant to identify Scatter Plot
-     */
-    public static final String SCATTER_PLOT = "Scatter Plot";
-    
+    public static final String CAB2B_ENTITY_GROUP = "caB2BEntityGroup";
+
     public static final String SELECT = "-- Select --";
-    
+
     public static final String SELECTED_PATH = "Selected Path";
-    
+
     public static final String CURATED_PATH = "Curated Path";
-    
+
     public static final String GENERAL_PATH = "General Path";
-    
+
     public static final Dimension WIZARD_SIZE2_DIMENSION = new Dimension(752, 580);
+
+    public static final Dimension WIZARD_NAVIGATION_PANEL_DIMENSION = new Dimension(752, 36);
 }
