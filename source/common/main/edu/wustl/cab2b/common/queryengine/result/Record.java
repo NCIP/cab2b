9e02ccc0c8a7cba4e8cca4ed79ad43a78c7cb7bd
srinath_k <srinath_k@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-06-13 14:43:44 +0000

    custom serialization

--- source/common/main/edu/wustl/cab2b/common/queryengine/result/Record.java
+++ source/common/main/edu/wustl/cab2b/common/queryengine/result/Record.java
@@ -1,16 +1,20 @@
 package edu.wustl.cab2b.common.queryengine.result;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 import edu.common.dynamicextensions.domaininterface.AttributeInterface;
+import edu.wustl.cab2b.common.cache.AbstractEntityCache;
 
 public class Record implements IRecord {
     private static final long serialVersionUID = 4657684475538003175L;
 
-    private Map<AttributeInterface, String> attributesValues;
+    private transient Map<AttributeInterface, String> attributesValues;
 
     private RecordId id;
 
@@ -42,5 +46,38 @@ public class Record implements IRecord {
 
     public Set<AttributeInterface> getAttributes() {
         return Collections.unmodifiableSet(attributesValues.keySet());
+    }
+
+    private void writeObject(ObjectOutputStream s) throws IOException {
+        s.defaultWriteObject();
+        s.writeObject(serializeValues());
+    }
+
+    private void readObject(ObjectInputStream s) throws IOException, ClassNotFoundException {
+        s.defaultReadObject();
+        Map<String, String> values = (Map<String, String>) s.readObject();
+        deserializeValues(values);
+    }
+
+    private Map<String, String> serializeValues() {
+        Map<String, String> res = new HashMap<String, String>();
+        for (Map.Entry<AttributeInterface, String> entry : attributesValues.entrySet()) {
+            AttributeInterface attribute = entry.getKey();
+            res.put(attribute.getEntity().getId() + "_" + attribute.getId(), entry.getValue());
+        }
+        return res;
+    }
+
+    private void deserializeValues(Map<String, String> values) {
+        attributesValues = new HashMap<AttributeInterface, String>();
+        AbstractEntityCache cache = AbstractEntityCache.getCache();
+        for (Map.Entry<String, String> entry : values.entrySet()) {
+            String[] key = entry.getKey().split("_");
+            Long entityId = Long.parseLong(key[0]);
+            Long attributeId = Long.parseLong(key[1]);
+            AttributeInterface attribute = cache.getEntityById(entityId).getAttributeByIdentifier(attributeId);
+
+            attributesValues.put(attribute, entry.getValue());
+        }
     }
 }
