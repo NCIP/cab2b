0e301a19aec8c140cbdad29d69feec90bf5c33f1
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-06-14 12:17:46 +0000

    Removed info loggers. formatted code

--- source/client/main/edu/wustl/cab2b/client/ui/experiment/ExperimentHierarchyPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/experiment/ExperimentHierarchyPanel.java
@@ -7,7 +7,6 @@ import java.util.Vector;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.UIManager;
@@ -15,7 +14,6 @@ import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
-import javax.swing.tree.TreePath;
 
 import org.jdesktop.swingx.JXTree;
 import org.jdesktop.swingx.decorator.HighlighterPipeline;
@@ -24,17 +22,15 @@ import edu.wustl.cab2b.client.ui.RiverLayout;
 import edu.wustl.cab2b.client.ui.controls.Cab2bButton;
 import edu.wustl.cab2b.client.ui.controls.Cab2bPanel;
 import edu.wustl.cab2b.client.ui.util.CommonUtils;
-import edu.wustl.cab2b.common.BusinessInterface;
 import edu.wustl.cab2b.common.domain.Experiment;
 import edu.wustl.cab2b.common.domain.ExperimentGroup;
 import edu.wustl.cab2b.common.experiment.ExperimentBusinessInterface;
 import edu.wustl.cab2b.common.experiment.ExperimentHome;
 import edu.wustl.cab2b.common.locator.Locator;
-import edu.wustl.cab2b.common.locator.LocatorException;
 import edu.wustl.common.tree.ExperimentTreeNode;
 import edu.wustl.common.tree.GenerateTree;
 import edu.wustl.common.util.dbManager.DAOException;
-import edu.wustl.common.util.logger.Logger;
+import edu.wustl.common.util.global.Constants;
 
 /**
  * A panel to display experiment and experiment group hierarchies 
@@ -44,11 +40,13 @@ import edu.wustl.common.util.logger.Logger;
  */
 public class ExperimentHierarchyPanel extends Cab2bPanel {
 
-    JXTree expTree;
+    private static final long serialVersionUID = 1L;
 
-    JButton addNewButton;
+    private JXTree expTree;
 
-    ExperimentDetailsPanel expDetailsPanel;
+    private JButton addNewButton;
+
+    private ExperimentDetailsPanel expDetailsPanel;
 
     public ExperimentHierarchyPanel(ExperimentDetailsPanel newExpDetailsPanel) {
         expDetailsPanel = newExpDetailsPanel;
@@ -56,21 +54,12 @@ public class ExperimentHierarchyPanel extends Cab2bPanel {
     }
 
     private void initGUI() {
-        //Logger.out.info("Inside experiment hirarchy model");
         this.setLayout(new RiverLayout());
         Vector dataVector = null;
-
-        // EJB code start
-        BusinessInterface bus = null;
         try {
-            bus = Locator.getInstance().locate(edu.wustl.cab2b.common.ejb.EjbNamesConstants.EXPERIMENT,
-                                               ExperimentHome.class);
-        } catch (LocatorException e1) {
-            CommonUtils.handleException(e1, this, true, true, false, false);
-        }
-
-        ExperimentBusinessInterface expBus = (ExperimentBusinessInterface) bus;
-        try {
+            ExperimentBusinessInterface expBus = (ExperimentBusinessInterface) Locator.getInstance().locate(
+                                                                                                            edu.wustl.cab2b.common.ejb.EjbNamesConstants.EXPERIMENT,
+                                                                                                            ExperimentHome.class);
             dataVector = expBus.getExperimentHierarchy();
         } catch (RemoteException e1) {
             CommonUtils.handleException(e1, this, true, true, false, false);
@@ -80,12 +69,8 @@ public class ExperimentHierarchyPanel extends Cab2bPanel {
             CommonUtils.handleException(e1, this, true, true, false, false);
         }
 
-        // EJB code end		
         GenerateTree treeGenerator = new GenerateTree();
-        expTree = (JXTree) treeGenerator.createTree(dataVector,
-                                                    edu.wustl.common.util.global.Constants.EXPERIMETN_TREE_ID,
-                                                    true);
-
+        expTree = (JXTree) treeGenerator.createTree(dataVector, Constants.EXPERIMETN_TREE_ID, true);
         expTree.setCellRenderer(new MyRenderer());
         expTree.setRolloverEnabled(true);
         expTree.setHighlighters(new HighlighterPipeline());
@@ -106,28 +91,24 @@ public class ExperimentHierarchyPanel extends Cab2bPanel {
                 ExperimentTreeNode selectedExpTreeNode = (ExperimentTreeNode) userObject;
                 if (selectedExpTreeNode != null) {
                     expDetailsPanel.refreshDetails(selectedExpTreeNode);
-                }                
+                }
             }
         }
         expTree.addTreeSelectionListener(new TreeSelectionListener() {
             public void valueChanged(TreeSelectionEvent tse) {
-                TreePath[] treePaths = tse.getPaths();
                 JXTree tree = (JXTree) tse.getSource();
 
                 Object userObject = ((DefaultMutableTreeNode) tree.getLastSelectedPathComponent()).getUserObject();
-                Logger.out.info("userObject ==>> " + userObject.getClass());
+
                 if (userObject instanceof ExperimentTreeNode) {
                     ExperimentTreeNode selectedExpTreeNode = (ExperimentTreeNode) userObject;
                     if (selectedExpTreeNode != null) {
                         expDetailsPanel.refreshDetails(selectedExpTreeNode);
                     }
-                    Logger.out.info("ExperimentTreeNode id :" + selectedExpTreeNode.getIdentifier());
                 } else if (userObject instanceof Experiment) {
-                    Logger.out.info("Experiment clicked, do some action");
                     if (expDetailsPanel != null)
                         expDetailsPanel.refreshDetails((Experiment) userObject);
                 } else if (userObject instanceof ExperimentGroup) {
-                    Logger.out.info("ExperimentGroup clicked, do some action");
                     if (expDetailsPanel != null)
                         expDetailsPanel.refreshDetails((ExperimentGroup) userObject);
                 }
@@ -166,7 +147,6 @@ public class ExperimentHierarchyPanel extends Cab2bPanel {
         Icon expGrpClosedIcon = new ImageIcon(loader.getResource("folder_closed.ico"));
 
         public MyRenderer() {
-
         }
 
         public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded,
@@ -192,23 +172,12 @@ public class ExperimentHierarchyPanel extends Cab2bPanel {
                     } else {
                         setIcon(expIcon);
                     }
-                } else
-                // this condition to take care of newly created experiment group node.
-                {
+                } else {
+                    //this condition to take care of newly created experiment group node.
                     setIcon(UIManager.getIcon("Tree.closedIcon"));
                 }
             }
             return this;
         }
-    }
-
-    public static void main(String[] args) {
-        ExperimentHierarchyPanel expHiePanel = new ExperimentHierarchyPanel(null);
-
-        JFrame frame = new JFrame("Experiment");
-        frame.setSize(600, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(expHiePanel);
-        frame.setVisible(true);
     }
 }
