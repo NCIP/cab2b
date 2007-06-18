d2bf6f8cb362635371d312ca4b32eba18e8dfaa3
chetan_patil <chetan_patil@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-06-18 08:01:45 +0000

    Remove unnecessary imports

--- source/client/main/edu/wustl/cab2b/client/ui/dag/MainDagPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/dag/MainDagPanel.java
@@ -2,7 +2,6 @@ package edu.wustl.cab2b.client.ui.dag;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Image;
 import java.util.ArrayList;
 import java.util.Enumeration;
