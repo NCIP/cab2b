3e014012455607e3dad766ae80e1263203eb613b
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-06-19 10:17:39 +0000

    Added constants

--- source/client/main/edu/wustl/cab2b/client/ui/util/ClientConstants.java
+++ source/client/main/edu/wustl/cab2b/client/ui/util/ClientConstants.java
@@ -16,4 +16,9 @@ public class ClientConstants {
     public static String APPLICATION_RESOURCES_FILE_NAME = "Cab2bApplicationResources";
 
     public static String CAB2B_LOGO_IMAGE = "b2b_logo_image.gif";
+    
+    public static String POPULAR_CATEGORIES_IMAGE = "popularcategories_icon.gif";
+    public static String MY_SEARCH_QUERIES_IMAGE = "mysearchqueries_icon.gif";
+    public static String MY_EXPERIMENT_IMAGE = "arrow_icon_mo.gif";
+    public static String MY_CATEGORIES_IMAGE = "arrow_icon_mo.gif";
 }
