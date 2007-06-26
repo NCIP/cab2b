0e19443c85066a26468c3b5cdd0a316783c1b9ed
hrishikesh_rajpathak <hrishikesh_rajpathak@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-06-26 12:21:01 +0000

    Icons

--- source/client/main/edu/wustl/cab2b/client/ui/util/ClientConstants.java
+++ source/client/main/edu/wustl/cab2b/client/ui/util/ClientConstants.java
@@ -2,27 +2,54 @@ package edu.wustl.cab2b.client.ui.util;
 
 /**
  * All the constants used at UI side
+ * 
  * @author Chandrakant Talele
  */
 public class ClientConstants {
-    public static final String OPERATOR_AND = "AND";
+	public static final String OPERATOR_AND = "AND";
 
-    public static final String OPERATOR_OR = "OR";
+	public static final String OPERATOR_OR = "OR";
 
-    /** Resource bundle name for getting error codes and its description. */
-    public static String ERROR_CODE_FILE_NAME = "errorcodes";
+	/** Resource bundle name for getting error codes and its description. */
+	public static String ERROR_CODE_FILE_NAME = "errorcodes";
 
-    /** Resource bundle name for getting externalized strings. */
-    public static String APPLICATION_RESOURCES_FILE_NAME = "Cab2bApplicationResources";
+	/** Resource bundle name for getting externalized strings. */
+	public static String APPLICATION_RESOURCES_FILE_NAME = "Cab2bApplicationResources";
 
-    public static String CAB2B_LOGO_IMAGE = "b2b_logo_image.gif";
-    
-    public static String POPULAR_CATEGORIES_IMAGE = "popularcategories_icon.gif";
-    public static String MY_SEARCH_QUERIES_IMAGE = "mysearchqueries_icon.gif";
-    public static String MY_EXPERIMENT_IMAGE = "my_experiments.gif";
-    public static String MY_CATEGORIES_IMAGE = "my_categories.gif";
-    public static String DETAILS_COLUMN_IMAGE = "popularcategories_icon.gif";
-    
-   public static String LIMIT_CONNECT_SELECTED ="limit_set.gif";
-   public static String LIMIT_CONNECT_DESELECTED ="limit_set_disabled.gif";
+	public static String CAB2B_LOGO_IMAGE = "b2b_logo_image.gif";
+
+	public static String POPULAR_CATEGORIES_IMAGE = "popularcategories_icon.gif";
+
+	public static String MY_SEARCH_QUERIES_IMAGE = "mysearchqueries_icon.gif";
+
+	public static String MY_EXPERIMENT_IMAGE = "my_experiments.gif";
+
+	public static String MY_CATEGORIES_IMAGE = "my_categories.gif";
+
+	public static String DETAILS_COLUMN_IMAGE = "popularcategories_icon.gif";
+
+	public static String LIMIT_CONNECT_SELECTED = "limit_set.gif";
+
+	public static String LIMIT_CONNECT_DESELECTED = "limit_set_disabled.gif";
+
+	public static String TREE_OPEN_FOLDER_ICON = "open_folder.gif";
+
+	public static String TREE_CLOSE_FOLDER_ICON = "close_folder.gif";
+
+	public static String VISUALIZE_DATA = "visualize_data.gif";
+
+	public static String ANALYZE_DATA = "analyze_data.gif";
+
+	public static String FILTER_DATA = "filter_data.gif";
+
+	public static String CHARTS = "charts.gif";
+
+	public static String BAR_GRAPH = "graph_bar.gif";
+
+	public static String LINE_GRAPH = "graph_line.gif";
+
+	public static String SCATTER_GRAPH = "graph_scatter.gif";
+	
+	public static String GENE_ANNOTATION = "Gene_Annotation.gif";
+
 }
