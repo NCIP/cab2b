1c3cfaa431b04dfc6ae7a0ce5699aa4f1f0dad16
chetan_patil <chetan_patil@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-06-28 12:29:37 +0000

    Formatted and cleaned

--- source/client/main/edu/wustl/cab2b/client/ui/controls/Cab2bRadioButton.java
+++ source/client/main/edu/wustl/cab2b/client/ui/controls/Cab2bRadioButton.java
@@ -1,21 +1,18 @@
 package edu.wustl.cab2b.client.ui.controls;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 
-import javax.swing.*;
+import javax.swing.JRadioButton;
 
-public class Cab2bRadioButton extends JRadioButton
-{
-	
-	static Color defaultBgColor = Color.WHITE;
-	
-	public Cab2bRadioButton(String strLabel)
-	{
-		super(strLabel);		
-		/*Arial font in windows is mapped to the SansSerif logical name.*/
-		this.setFont(new Font("SansSerif",Font.PLAIN,12));
-		this.setBackground(defaultBgColor);
-	}
+public class Cab2bRadioButton extends JRadioButton {
+    private static final long serialVersionUID = 1L;
+
+    public Cab2bRadioButton(String strLabel) {
+        super(strLabel);
+        /*Arial font in windows is mapped to the SansSerif logical name.*/
+        this.setFont(new Font("SansSerif", Font.PLAIN, 12));
+        this.setBackground(Color.WHITE);
+    }
+    
 }
