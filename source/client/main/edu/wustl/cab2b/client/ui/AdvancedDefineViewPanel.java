f9c80405e9071311bd883380e3a6a7076b4a1def
hrishikesh_rajpathak <hrishikesh_rajpathak@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-07-05 12:26:04 +0000

    Select Default View- default view

--- source/client/main/edu/wustl/cab2b/client/ui/AdvancedDefineViewPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/AdvancedDefineViewPanel.java
@@ -45,222 +45,237 @@ import edu.wustl.common.util.logger.Logger;
 
 public class AdvancedDefineViewPanel extends Cab2bPanel {
 
-    /** Reference to the parent container. */
-    private SearchCenterPanel m_searchCenterPanel = null;
+	/** Reference to the parent container. */
+	private SearchCenterPanel m_searchCenterPanel = null;
 
-    /** Pagination component */
-    private JPagination m_resultsPage = null;
+	/** Pagination component */
+	private JPagination m_resultsPage = null;
 
-    /** The functional class corresponding to root. */
-    private EntityInterface m_rootEntity = null;
+	/** The functional class corresponding to root. */
+	private EntityInterface m_rootEntity = null;
 
-    public static boolean isMultipleGraphException = false;
+	public static boolean isMultipleGraphException = false;
 
-    public AdvancedDefineViewPanel(SearchCenterPanel searchCenterPanel) {
-        this.m_searchCenterPanel = searchCenterPanel;
-        isMultipleGraphException = false;
-        initGUI();
-    }
+	public AdvancedDefineViewPanel(SearchCenterPanel searchCenterPanel) {
+		this.m_searchCenterPanel = searchCenterPanel;
+		isMultipleGraphException = false;
+		initGUI();
+	}
 
-    private void initGUI() {
+	private void initGUI() {
 
-        this.setLayout(new BorderLayout());
+		this.setLayout(new BorderLayout());
 
-        /**
-         * Add the hyperlink to show all classes.
-         */
+		/**
+		 * Add the hyperlink to show all classes.
+		 */
 
-        Cab2bHyperlink showAllLink = new Cab2bHyperlink();
-        showAllLink.setText("Show All Search Results Views");
-        showAllLink.setEnabled(false);
-        JXPanel topPanel = new Cab2bPanel(new RiverLayout());
-        topPanel.add(new Cab2bLabel(""));
-        topPanel.add("tab ", showAllLink);
-        /* Add the top panel to the North of the dialog. */
-        this.add(BorderLayout.NORTH, topPanel);
+		Cab2bHyperlink showAllLink = new Cab2bHyperlink();
+		showAllLink.setText("Show All Search Results Views");
+		showAllLink.setEnabled(false);
+		JXPanel topPanel = new Cab2bPanel(new RiverLayout());
+		topPanel.add(new Cab2bLabel(""));
+		topPanel.add("tab ", showAllLink);
+		/* Add the top panel to the North of the dialog. */
+		this.add(BorderLayout.NORTH, topPanel);
 
-        Vector<PageElement> pageElementCollection = new Vector<PageElement>();
-        /*
-         * Get the classes in the where part from the query.
-         */
-        MainSearchPanel mainSearchPanel = (MainSearchPanel) (this.m_searchCenterPanel.getParent());
+		Vector<PageElement> pageElementCollection = new Vector<PageElement>();
+		/*
+		 * Get the classes in the where part from the query.
+		 */
+		MainSearchPanel mainSearchPanel = (MainSearchPanel) (this.m_searchCenterPanel.getParent());
 
-        /* Get the wrapper. */
-        final IClientQueryBuilderInterface queryObject = mainSearchPanel.getQueryObject();
+		/* Get the wrapper. */
+		final IClientQueryBuilderInterface queryObject = mainSearchPanel.getQueryObject();
 
-        /* Get the collection of EntityInterface. */
-        Collection entityCollection = queryObject.getEntities();
+		/* Get the collection of EntityInterface. */
+		Collection entityCollection = queryObject.getEntities();
 
-        Iterator iterator = entityCollection.iterator();
-        while (iterator.hasNext()) {
+		Iterator iterator = entityCollection.iterator();
+		while (iterator.hasNext()) {
 
-            /*
-             * Iterate over the collection and get the individual entity
-             * instamces.
-             */
-            EntityInterface entityInterface = (EntityInterface) iterator.next();
-            /*
-             * Get the name and description and create the IPageElements needed
-             * for pagination
-             */
-            PageElement pageElement = new PageElementImpl();
-            // set the proper display name
-            pageElement.setDisplayName(edu.wustl.cab2b.common.util.Utility.getDisplayName(entityInterface));
-            // pageElement.setDisplayName(entityInterface.getName());
+			/*
+			 * Iterate over the collection and get the individual entity
+			 * instamces.
+			 */
+			EntityInterface entityInterface = (EntityInterface) iterator.next();
+			/*
+			 * Get the name and description and create the IPageElements needed
+			 * for pagination
+			 */
+			PageElement pageElement = new PageElementImpl();
+			// set the proper display name
+			pageElement.setDisplayName(edu.wustl.cab2b.common.util.Utility
+					.getDisplayName(entityInterface));
+			// pageElement.setDisplayName(entityInterface.getName());
 
-            pageElement.setDescription(entityInterface.getDescription());
+			pageElement.setDescription(entityInterface.getDescription());
 
-            /* Add the instance to the collection of PageElement collection. */
-            pageElementCollection.add(pageElement);
+			/* Add the instance to the collection of PageElement collection. */
+			pageElementCollection.add(pageElement);
 
-        }
+		}
 
-        NumericPager numPager = new NumericPager(pageElementCollection);
-        /*
-         * Initalize the pagination component.
-         */
-        this.m_resultsPage = new JPagination(pageElementCollection, numPager, this, true);
-        m_resultsPage.resetAllLabels();
-        this.m_resultsPage.setPageLinksDisabled();
+		NumericPager numPager = new NumericPager(pageElementCollection);
+		/*
+		 * Initalize the pagination component.
+		 */
+		this.m_resultsPage = new JPagination(pageElementCollection, numPager, this, true);
+		m_resultsPage.resetAllLabels();
+		this.m_resultsPage.setPageLinksDisabled();
 
-        /*
-         * Create a Panel for the center panel, and add the pagination component
-         * to that.
-         */
-        JXPanel centerPanel = new Cab2bPanel(new RiverLayout());
-        centerPanel.add(new Cab2bLabel());
-        centerPanel.add("br", new Cab2bLabel());
-        centerPanel.add("br", new Cab2bLabel());
-        centerPanel.add("br", new Cab2bLabel());
-        // centerPanel.add("br",new Cab2bLabel("Search views available. Please
-        // not links are dummies, so please don't try seleting them."));
-        centerPanel.add("br", new Cab2bLabel());
-        centerPanel.add("br", this.m_resultsPage);
-        /* Add the center panel to the main panel */
-        this.add(BorderLayout.CENTER, centerPanel);
+		/*
+		 * Create a Panel for the center panel, and add the pagination component
+		 * to that.
+		 */
+		JXPanel centerPanel = new Cab2bPanel(new RiverLayout());
+		centerPanel.add(new Cab2bLabel());
+		centerPanel.add("br", new Cab2bLabel());
+		centerPanel.add("br", new Cab2bLabel());
+		centerPanel.add("br", new Cab2bLabel());
+		// centerPanel.add("br",new Cab2bLabel("Search views available. Please
+		// not links are dummies, so please don't try seleting them."));
+		centerPanel.add("br", new Cab2bLabel());
+		centerPanel.add("br", this.m_resultsPage);
+		/* Add the center panel to the main panel */
+		this.add(BorderLayout.CENTER, centerPanel);
 
-        /* The bottom panel */
-        JXPanel bottomPanel = new Cab2bPanel(new RiverLayout(0, 0));
-        JLabel asterix = new Cab2bLabel("* ");
-        asterix.setFont(new Font("Arial", Font.BOLD, 16));
-        asterix.setForeground(Color.RED);
-        bottomPanel.add(asterix);
-        bottomPanel.add(new Cab2bLabel("Select Default View   "));
+		/* The bottom panel */
+		JXPanel bottomPanel = new Cab2bPanel(new RiverLayout(0, 0));
+		JLabel asterix = new Cab2bLabel("* ");
+		asterix.setFont(new Font("Arial", Font.BOLD, 16));
+		asterix.setForeground(Color.RED);
+		bottomPanel.add(asterix);
+		bottomPanel.add(new Cab2bLabel("Select Default View   "));
 
-        /*
-         * Get the class corresponding to the root expression and show that in
-         * the drop down. For this the IClass should be enough as we are
-         * interested only in the name.
-         */
-        Logger.out.debug("queryObject.getQuery().getClass() " + queryObject.getQuery().getClass());
-        // final ICab2bQuery b2bquery = (ICab2bQuery)queryObject.getQuery();
+		/*
+		 * Get the class corresponding to the root expression and show that in
+		 * the drop down. For this the IClass should be enough as we are
+		 * interested only in the name.
+		 */
+		Logger.out.debug("queryObject.getQuery().getClass() " + queryObject.getQuery().getClass());
+		// final ICab2bQuery b2bquery = (ICab2bQuery)queryObject.getQuery();
 
-        final IQuery b2bquery = queryObject.getQuery();
+		final IQuery b2bquery = queryObject.getQuery();
 
-        /* Get the root expression ID. */
-        IExpressionId rootExpressionID = null;
-        try {
-            rootExpressionID = b2bquery.getConstraints().getRootExpressionId();
+		/* Get the root expression ID. */
+		IExpressionId rootExpressionID = null;
+		try {
+			rootExpressionID = b2bquery.getConstraints().getRootExpressionId();
 
-        } catch (MultipleRootsException e) {
-            CheckedException checkedException = new CheckedException(e.getMessage(), e, ErrorCodeConstants.QM_0003);
-            CommonUtils.handleException(checkedException, this, true, true, true, false);
-            isMultipleGraphException = true;
-        }
+		} catch (MultipleRootsException e) {
+			CheckedException checkedException = new CheckedException(e.getMessage(), e,
+					ErrorCodeConstants.QM_0003);
+			CommonUtils.handleException(checkedException, this, true, true, true, false);
+			isMultipleGraphException = true;
+		}
 
-        Vector<String> comboModel = getAllEntityNamesInWhereClause(queryObject);
-        Cab2bComboBox combo = new Cab2bComboBox(comboModel);
-        // Updated since the Cab2bComboBox default preferredSize is to small.
-        combo.setPreferredSize(new Dimension(new Dimension(250, 20)));
-        combo.addItemListener(new ItemListener() {
-            public void itemStateChanged(ItemEvent ie) {
-                if (ie.getStateChange() == ItemEvent.SELECTED) {
-                    Logger.out.debug("item changed : selected object :" + ie.getItem());
-                    /*
-                     * Iterate over the where clause entities to pick the
-                     * selected entity's entity interface
-                     */
-                    String userSelectionOfEntity = ie.getItem().toString();
-                    EntityInterface entInt = findTargetEntityInterface(userSelectionOfEntity);
-                    try {
-                        queryObject.setOutputForQuery(entInt);
-                    } catch (RemoteException e) {
-                        CommonUtils.handleException(e, m_searchCenterPanel, true, true, true, false);
-                    }
+		Vector<String> comboModel = getAllEntityNamesInWhereClause(queryObject);
+		Cab2bComboBox combo = new Cab2bComboBox(comboModel);
+		// Updated since the Cab2bComboBox default preferredSize is to small.
+		combo.setPreferredSize(new Dimension(new Dimension(250, 20)));
+		combo.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent ie) {
+				if (ie.getStateChange() == ItemEvent.SELECTED) {
+					Logger.out.debug("item changed : selected object :" + ie.getItem());
+					/*
+					 * Iterate over the where clause entities to pick the
+					 * selected entity's entity interface
+					 */
+					String userSelectionOfEntity = ie.getItem().toString();
+					EntityInterface entInt = findTargetEntityInterface(userSelectionOfEntity);
+					try {
+						queryObject.setOutputForQuery(entInt);
+					} catch (RemoteException e) {
+						CommonUtils
+								.handleException(e, m_searchCenterPanel, true, true, true, false);
+					}
 
-                }
-            }
-        });
+				}
+			}
+		});
 
-        /*
-         * If user doesn't bother to change the output entity he wants, set the
-         * default selected entity in the combox as the output entity.
-         */
-        String defaultSelectionOfEntity = (String) combo.getSelectedItem();
-        EntityInterface entInt = findTargetEntityInterface(defaultSelectionOfEntity);
-        try {
-            queryObject.setOutputForQuery(entInt);
-        } catch (RemoteException e) {
-            CommonUtils.handleException(e, m_searchCenterPanel, true, true, true, false);
-        }
-        bottomPanel.add(combo);
+		/*
+		 * If user doesn't bother to change the output entity he wants, set the
+		 * default selected entity in the combox as the output entity.
+		 */
+		String defaultSelectionOfEntity = (String) combo.getSelectedItem();
+		EntityInterface entInt = findTargetEntityInterface(defaultSelectionOfEntity);
+		try {
+			queryObject.setOutputForQuery(entInt);
+		} catch (RemoteException e) {
+			CommonUtils.handleException(e, m_searchCenterPanel, true, true, true, false);
+		}
+		bottomPanel.add(combo);
 
-        this.add(BorderLayout.SOUTH, bottomPanel);
+		this.add(BorderLayout.SOUTH, bottomPanel);
 
-    }
+	}
 
-    private EntityInterface findTargetEntityInterface(String targetEntityName) {
-        EntityInterface targetEntityInterface = null;
-        Logger.out.debug("targetEntityName " + targetEntityName);
+	private EntityInterface findTargetEntityInterface(String targetEntityName) {
+		EntityInterface targetEntityInterface = null;
+		Logger.out.debug("targetEntityName " + targetEntityName);
 
-        Iterator<EntityInterface> entityInter = allEntities.iterator();
-        while (entityInter.hasNext()) {
-            EntityInterface entity = entityInter.next();
-            //            String entityName = entity.getName();
-            if (Utility.getDisplayName(entity).equals(targetEntityName)) {
-                targetEntityInterface = entity;
-                Logger.out.debug("found target entity interface to be set set as select entity in Query.");
-                break;
-            }
-        }
+		Iterator<EntityInterface> entityInter = allEntities.iterator();
+		while (entityInter.hasNext()) {
+			EntityInterface entity = entityInter.next();
+			// String entityName = entity.getName();
+			if (Utility.getDisplayName(entity).equals(targetEntityName)) {
+				targetEntityInterface = entity;
+				Logger.out
+						.debug("found target entity interface to be set set as select entity in Query.");
+				break;
+			}
+		}
 
-        return targetEntityInterface;
-    }
+		return targetEntityInterface;
+	}
 
-    Set<EntityInterface> allEntities = new HashSet<EntityInterface>();
+	Set<EntityInterface> allEntities = new HashSet<EntityInterface>();
 
-    private Vector<String> getAllEntityNamesInWhereClause(IClientQueryBuilderInterface queryBuilder) {
-        Vector<String> returner = new Vector<String>();
+	private Vector<String> getAllEntityNamesInWhereClause(IClientQueryBuilderInterface queryBuilder) {
+		Set<IExpressionId> expIdsEnum = queryBuilder.getVisibleExressionIds();
+		Iterator<IExpressionId> iter = expIdsEnum.iterator();
 
-        Set<IExpressionId> expIdsEnum = queryBuilder.getVisibleExressionIds();
-        Iterator<IExpressionId> iter = expIdsEnum.iterator();
+		IQuery query = queryBuilder.getQuery();
 
-        IQuery query = queryBuilder.getQuery();
+		Set<String> returnerSet = new HashSet<String>();
+		while (iter.hasNext()) {
+			IExpressionId expId = iter.next();
+			IExpression expression = query.getConstraints().getExpression(expId);
 
-        while (iter.hasNext()) {
-            IExpressionId expId = iter.next();
-            IExpression expression = query.getConstraints().getExpression(expId);
+			EntityInterface entity = expression.getConstraintEntity().getDynamicExtensionsEntity();
 
-            EntityInterface entity = expression.getConstraintEntity().getDynamicExtensionsEntity();
+			allEntities.add(entity);
 
-            allEntities.add(entity);
+			String entityName = Utility.getDisplayName(entity);
+			/*
+			 * Add an entity name if it doesn't exist, so that the drop down
+			 * items are unique.
+			 */
+			returnerSet.add(entityName);
 
-            //String entityName = entity.getName();      
-            String entityName = Utility.getDisplayName(entity);
-            /*
-             * Add an entity name if it doesn't exist, so that the drop down
-             * items are unique.
-             */
-            if (!returner.contains(entityName))
-                returner.add(entityName);
-            //}
+		}
 
-        }
+		IExpressionId rootExprId = null;
+		try {
+			rootExprId = query.getConstraints().getRootExpressionId();
+		} catch (MultipleRootsException e) {
+			// can't occur
+		}
 
-        return returner;
-    }
+		IExpression rootExpr = query.getConstraints().getExpression(rootExprId);
+		String rootEntityName = Utility.getDisplayName(rootExpr.getConstraintEntity()
+				.getDynamicExtensionsEntity());
+		Vector<String> returner = new Vector<String>();
+		returner.add(rootEntityName);
+		returnerSet.remove(rootEntityName);
+		returner.addAll(returnerSet);
 
-    public EntityInterface getRootEntity() {
-        return this.m_rootEntity;
-    }
+		return returner;
+	}
+
+	public EntityInterface getRootEntity() {
+		return this.m_rootEntity;
+	}
 }
