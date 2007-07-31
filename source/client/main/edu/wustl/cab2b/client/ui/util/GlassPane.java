50c77d337a14433d61fa0ac9aaff02d2a08d097e
chetan_patil <chetan_patil@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-07-31 09:40:58 +0000

    Bug#4937

--- source/client/main/edu/wustl/cab2b/client/ui/util/GlassPane.java
+++ source/client/main/edu/wustl/cab2b/client/ui/util/GlassPane.java
@@ -1,10 +1,8 @@
-
 package edu.wustl.cab2b.client.ui.util;
 
 import java.awt.AWTEvent;
 import java.awt.Component;
 import java.awt.Cursor;
-import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.AWTEventListener;
 import java.awt.event.KeyAdapter;
@@ -19,157 +17,133 @@ import javax.swing.SwingUtilities;
  * This is the glass pane class that intercepts screen interactions during system busy states.
  *
  */
-public class GlassPane extends JComponent implements AWTEventListener
-{
+public class GlassPane extends JComponent implements AWTEventListener {
+    private static final long serialVersionUID = 1L;
 
-	private Window theWindow;
-	private Component activeComponent;
+    private Window theWindow;
 
-	/**
-	 * GlassPane constructor comment.
-	 * @param Container a 
-	 */
-	protected GlassPane(Component activeComponent)
-	{
-		// add adapters that do nothing for keyboard and mouse actions
-		addMouseListener(new MouseAdapter()
-		{
-		});
+    private Component activeComponent;
 
-		addKeyListener(new KeyAdapter()
-		{
-		});
+    /**
+     * GlassPane constructor comment.
+     * @param Container a 
+     */
+    protected GlassPane(Component activeComponent) {
+        // add adapters that do nothing for keyboard and mouse actions
+        addMouseListener(new MouseAdapter() {
+        });
 
-		setActiveComponent(activeComponent);
-	}
+        addKeyListener(new KeyAdapter() {
+        });
 
-	/**
-	 * Receives all key events in the AWT and processes the ones that originated from the
-	 * current window with the glass pane.
-	 *
-	 * @param event the AWTEvent that was fired
-	 */
-	public void eventDispatched(AWTEvent event)
-	{
-		Object source = event.getSource();
+        setActiveComponent(activeComponent);
+    }
 
-		// discard the event if its source is not from the correct type
-		boolean sourceIsComponent = (event.getSource() instanceof Component);
+    /**
+     * Receives all key events in the AWT and processes the ones that originated from the
+     * current window with the glass pane.
+     *
+     * @param event the AWTEvent that was fired
+     */
+    public void eventDispatched(AWTEvent event) {
+        Object source = event.getSource();
 
-		if ((event instanceof KeyEvent) && sourceIsComponent)
-		{
-			// If the event originated from the window w/glass pane, consume the event
-			if ((SwingUtilities.windowForComponent((Component) source) == theWindow))
-			{
-				((KeyEvent) event).consume();
-			}
-		}
-	}
+        // discard the event if its source is not from the correct type
+        boolean sourceIsComponent = (event.getSource() instanceof Component);
 
-	/**
-	 * Finds the glass pane that is related to the specified component.
-	 * 
-	 * @param startComponent the component used to start the search for the glass pane
-	 * @param create a flag whether to create a glass pane if one does not exist
-	 * @return GlassPane
-	 */
-	public synchronized static GlassPane mount(Component startComponent, boolean create)
-	{
-		RootPaneContainer aContainer = null;
-		Component aComponent = startComponent;
+        if ((event instanceof KeyEvent) && sourceIsComponent) {
+            // If the event originated from the window w/glass pane, consume the event
+            if ((SwingUtilities.windowForComponent((Component) source) == theWindow)) {
+                ((KeyEvent) event).consume();
+            }
+        }
+    }
 
-		// Climb the component hierarchy until a RootPaneContainer is found or until the very top
-		while ((aComponent.getParent() != null) && !(aComponent instanceof RootPaneContainer))
-		{
-			aComponent = (Component) aComponent.getParent();
-		}
+    /**
+     * Finds the glass pane that is related to the specified component.
+     * 
+     * @param startComponent the component used to start the search for the glass pane
+     * @param create a flag whether to create a glass pane if one does not exist
+     * @return GlassPane
+     */
+    public synchronized static GlassPane mount(Component startComponent, boolean create) {
+        RootPaneContainer aContainer = null;
+        Component aComponent = startComponent;
 
-		// Guard against error conditions if climb search wasn't successful
-		if (aComponent instanceof RootPaneContainer)
-		{
-			aContainer = (RootPaneContainer) aComponent;
-		}
+        // Climb the component hierarchy until a RootPaneContainer is found or until the very top
+        while ((aComponent.getParent() != null) && !(aComponent instanceof RootPaneContainer)) {
+            aComponent = (Component) aComponent.getParent();
+        }
 
-		if (aContainer != null)
-		{
-			// Retrieve an existing GlassPane if old one already exist or create a new one, otherwise return null
-			if ((aContainer.getGlassPane() != null)
-					&& (aContainer.getGlassPane() instanceof GlassPane))
-			{
-				return (GlassPane) aContainer.getGlassPane();
-			}
-			else if (create)
-			{
-				GlassPane aGlassPane = new GlassPane(startComponent);
-				aContainer.setGlassPane(aGlassPane);
-				
-				return aGlassPane;
-			}
-			else
-			{
-				return null;
-			}
-		}
-		return null;
-	}
+        // Guard against error conditions if climb search wasn't successful
+        if (aComponent instanceof RootPaneContainer) {
+            aContainer = (RootPaneContainer) aComponent;
+        }
 
-	/**
-	 * Set the component that ordered-up the glass pane.
-	 *
-	 * @param aComponent the UI component that asked for the glass pane
-	 */
-	private void setActiveComponent(Component aComponent)
-	{
-		activeComponent = aComponent;
-	}
+        if (aContainer != null) {
+            // Retrieve an existing GlassPane if old one already exist or create a new one, otherwise return null
+            if ((aContainer.getGlassPane() != null) && (aContainer.getGlassPane() instanceof GlassPane)) {
+                return (GlassPane) aContainer.getGlassPane();
+            } else if (create) {
+                GlassPane aGlassPane = new GlassPane(startComponent);
+                aContainer.setGlassPane(aGlassPane);
 
-	/**
-	 * Sets the glass pane as visible or invisible. The mouse cursor will be set accordingly.
-	 */
-	public void setVisible(boolean value)
-	{
-		if (value)
-		{
-			// keep track of the visible window associated w/the component
-			// useful during event filtering
-			if (theWindow == null)
-			{
-				theWindow = SwingUtilities.windowForComponent(activeComponent);
-				if (theWindow == null)
-				{
-					if (activeComponent instanceof Window)
-					{
-						theWindow = (Window) activeComponent;
-					}
-				}
-			}
+                return aGlassPane;
+            } else {
+                return null;
+            }
+        }
+        return null;
+    }
 
-			// Sets the mouse cursor to hourglass mode
-			getTopLevelAncestor().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+    /**
+     * Set the component that ordered-up the glass pane.
+     *
+     * @param aComponent the UI component that asked for the glass pane
+     */
+    private void setActiveComponent(Component aComponent) {
+        activeComponent = aComponent;
+    }
 
-			activeComponent = theWindow.getFocusOwner();
+    /**
+     * Sets the glass pane as visible or invisible. The mouse cursor will be set accordingly.
+     */
+    public void setVisible(boolean value) {
+        if (value) {
+            // keep track of the visible window associated w/the component
+            // useful during event filtering
+            if (theWindow == null) {
+                theWindow = SwingUtilities.windowForComponent(activeComponent);
+                if (theWindow == null) {
+                    if (activeComponent instanceof Window) {
+                        theWindow = (Window) activeComponent;
+                    }
+                }
+            }
 
-			// Start receiving all events and consume them if necessary
-			Toolkit.getDefaultToolkit().addAWTEventListener(this, AWTEvent.KEY_EVENT_MASK);
+            // Sets the mouse cursor to hourglass mode
+            getTopLevelAncestor().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 
-			this.requestFocus();
+            activeComponent = theWindow.getFocusOwner();
 
-			// Activate the glass pane capabilities
-			super.setVisible(value);
-		}
-		else
-		{
-			// Stop receiving all events
-			Toolkit.getDefaultToolkit().removeAWTEventListener(this);
+            // Start receiving all events and consume them if necessary
+            //Toolkit.getDefaultToolkit().addAWTEventListener(this, AWTEvent.KEY_EVENT_MASK);
 
-			// Deactivate the glass pane capabilities
-			super.setVisible(value);
+            this.requestFocus();
 
-			// Sets the mouse cursor back to the regular pointer
-			if (getTopLevelAncestor() != null)
-			{
-				getTopLevelAncestor().setCursor(null);
-			}
-		}
-	}
+            // Activate the glass pane capabilities
+            super.setVisible(value);
+        } else {
+            // Stop receiving all events
+            //Toolkit.getDefaultToolkit().removeAWTEventListener(this);
+
+            // Deactivate the glass pane capabilities
+            super.setVisible(value);
+
+            // Sets the mouse cursor back to the regular pointer
+            if (getTopLevelAncestor() != null) {
+                getTopLevelAncestor().setCursor(null);
+            }
+        }
+    }
 }
