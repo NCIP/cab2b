069c6f343786787d7129f325b50eedb291b57953
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-09-29 06:27:39 +0000

    Added two more constructor with parameters String and Dimension

--- source/client/main/edu/wustl/cab2b/client/ui/controls/Cab2bTextField.java
+++ source/client/main/edu/wustl/cab2b/client/ui/controls/Cab2bTextField.java
@@ -1,18 +1,26 @@
-
 package edu.wustl.cab2b.client.ui.controls;
 
 import java.awt.Dimension;
 
 import javax.swing.JTextField;
 
-public class Cab2bTextField extends JTextField
-{
+public class Cab2bTextField extends JTextField {
 
-	final Dimension dim = new Dimension(100, 20);
+	Dimension dim = new Dimension(100, 20);
 
-	public Cab2bTextField()
-	{
+	public Cab2bTextField() {
 		super();
 		setPreferredSize(dim);
+	}
+
+	public Cab2bTextField(String initString) {
+		super(initString);
+		setPreferredSize(dim);
+	}
+
+	public Cab2bTextField(String initString, Dimension dim) {
+		super(initString);
+		this.dim = dim;
+		setPreferredSize(this.dim);
 	}
 }
