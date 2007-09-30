0a5d88cca3d48274da4dab2d0bbeb5fa953676c8
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-09-30 10:02:09 +0000

    Added new method generateParameterizeUIPanel to generate ParameterizedQuery UI panle

--- source/client/main/edu/wustl/cab2b/client/ui/main/SwingUIManager.java
+++ source/client/main/edu/wustl/cab2b/client/ui/main/SwingUIManager.java
@@ -8,6 +8,7 @@ package edu.wustl.cab2b.client.ui.main;
 
 import java.awt.Dimension;
 import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 
 import edu.common.dynamicextensions.domain.BooleanAttributeTypeInformation;
 import edu.common.dynamicextensions.domain.ByteArrayAttributeTypeInformation;
@@ -25,80 +26,120 @@ import edu.wustl.cab2b.common.errorcodes.ErrorCodeConstants;
 import edu.wustl.cab2b.common.exception.CheckedException;
 
 /**
- * This class is used for creating UI panel that shows attribute name, data-type 
+ * This class is used for creating UI panel that shows attribute name, data-type
  * conditions and components for entering/showing the values.
  * 
  * @author Kaushal Kumar
  * @version 1.0
  */
 public class SwingUIManager {
-	/**
-	 * This method returns a panel object that shows attribute name, data-type 
-	 * conditions and components for entering/showing the values.
-	 * @param parseFile
-	 * @param entity
-	 * @return panel object
-	 */
-	public static Object generateUIPanel(ParseXMLFile parseFile, AttributeInterface attributeEntity, boolean showConditions, Dimension maxLabelDimension ) throws CheckedException {
-		Object[] object = new Object[4];
-		String className = null;
-		AttributeTypeInformationInterface attributeTypeInformation = attributeEntity.getAttributeTypeInformation();
-		String dataTypeString = getDataType(attributeTypeInformation);
-		
-		
-		// Check if attribute has value domain (i.e it is enum)
-		if (true == edu.wustl.cab2b.common.util.Utility.isEnumerated(attributeEntity)) {
-			object[0] = parseFile.getEnumConditionList(dataTypeString);
-			className = parseFile.getEnumClassName(dataTypeString);
-		} else { // If an entity is of non-enumerated type
-			object[0] = parseFile.getNonEnumConditionList(dataTypeString);
-			className = parseFile.getNonEnumClassName(dataTypeString);
-		}
-		object[1] = attributeEntity;
-		object[2] = new Boolean(showConditions);
+    /**
+     * This method returns a panel object that shows attribute name, data-type
+     * conditions and components for entering/showing the values.
+     * 
+     * @param parseFile
+     * @param entity
+     * @return panel object
+     */
+    public static Object generateUIPanel(ParseXMLFile parseFile, AttributeInterface attributeEntity,
+                                         boolean showConditions, Dimension maxLabelDimension)
+            throws CheckedException {
+        Object[] object = new Object[4];
+        String className = null;
+        AttributeTypeInformationInterface attributeTypeInformation = attributeEntity.getAttributeTypeInformation();
+        String dataTypeString = getDataType(attributeTypeInformation);
+
+        // Check if attribute has value domain (i.e it is enum)
+        if (true == edu.wustl.cab2b.common.util.Utility.isEnumerated(attributeEntity)) {
+            object[0] = parseFile.getEnumConditionList(dataTypeString);
+            className = parseFile.getEnumClassName(dataTypeString);
+        } else { // If an entity is of non-enumerated type
+            object[0] = parseFile.getNonEnumConditionList(dataTypeString);
+            className = parseFile.getNonEnumClassName(dataTypeString);
+        }
+        object[1] = attributeEntity;
+        object[2] = new Boolean(showConditions);
         object[3] = maxLabelDimension;
-		
-		Constructor[] cls = null;
-		Object uiObject = null;
-		try {
-			cls = Class.forName(className).getConstructors();
-			uiObject = cls[0].newInstance(object);
-		} catch (Exception e) {
-			throw new CheckedException(e.getMessage(), e, ErrorCodeConstants.RF_0001);
-		}
-		
-		return uiObject;
-	}
-	
-	public static Object generateUIPanel(ParseXMLFile parseFile, AttributeInterface attributeEntity, Dimension maxLabelDimension ) throws CheckedException {
-		return generateUIPanel(parseFile, attributeEntity, true, maxLabelDimension);
-	}
-	
-	/**
-	 * This method returns the datatype of the given AttributeTypeInformation 
-	 * @param attributeTypeInformation
-	 * @return the datatype
-	 */
-	private static String getDataType(AttributeTypeInformationInterface attributeTypeInformation) {
-		String dataTypeString = null;
-		
-		if ((attributeTypeInformation instanceof StringAttributeTypeInformation)
-				|| (attributeTypeInformation instanceof ByteArrayAttributeTypeInformation)) {
-			dataTypeString = "string";
-		} else if ((attributeTypeInformation instanceof IntegerAttributeTypeInformation)
-				|| (attributeTypeInformation instanceof LongAttributeTypeInformation)
-				|| (attributeTypeInformation instanceof FloatAttributeTypeInformation)
-				|| (attributeTypeInformation instanceof DoubleAttributeTypeInformation)
-				|| (attributeTypeInformation instanceof NumericAttributeTypeInformation)
-				|| (attributeTypeInformation instanceof ShortAttributeTypeInformation)) {
-			dataTypeString = "number";
-		} else if (attributeTypeInformation instanceof BooleanAttributeTypeInformation) {
-			dataTypeString = "boolean";
-		} else if (attributeTypeInformation instanceof DateAttributeTypeInformation) {
-			dataTypeString = "date";
-		}
-		
-		return dataTypeString;
-	}
-	
+
+        Object uiObject = null;
+
+        try {
+            Class[] arr = { ArrayList.class, AttributeInterface.class, Boolean.class, Dimension.class };
+            Class<?> clazz = Class.forName(className);
+            Constructor<?> constructor = clazz.getDeclaredConstructor(arr);
+
+            uiObject = constructor.newInstance(object);
+        } catch (Exception e) {
+            throw new CheckedException(e.getMessage(), e, ErrorCodeConstants.RF_0001);
+        }
+
+        return uiObject;
+    }
+
+    public static Object generateParameterizedUIPanel(ParseXMLFile parseFile, AttributeInterface attributeEntity,
+                                                      boolean showConditions, Dimension maxLabelDimension)
+            throws CheckedException {
+        Object[] object = new Object[5];
+        String className = null;
+        AttributeTypeInformationInterface attributeTypeInformation = attributeEntity.getAttributeTypeInformation();
+        String dataTypeString = getDataType(attributeTypeInformation);
+
+        // Check if attribute has value domain (i.e it is enum)
+        if (true == edu.wustl.cab2b.common.util.Utility.isEnumerated(attributeEntity)) {
+            object[0] = parseFile.getEnumConditionList(dataTypeString);
+            className = parseFile.getEnumClassName(dataTypeString);
+        } else { // If an entity is of non-enumerated type
+            object[0] = parseFile.getNonEnumConditionList(dataTypeString);
+            className = parseFile.getNonEnumClassName(dataTypeString);
+        }
+
+        object[1] = attributeEntity;
+        object[2] = new Boolean(showConditions);
+        object[3] = maxLabelDimension;
+        object[4] = true;
+        Object uiObject = null;
+        try {
+            Class[] arr = { ArrayList.class, AttributeInterface.class, Boolean.class, Dimension.class, Boolean.class };
+            Class<?> clazz = Class.forName(className);
+            Constructor<?> constructor = clazz.getDeclaredConstructor(arr);
+            uiObject = constructor.newInstance(object);
+
+        } catch (Exception e) {
+            throw new CheckedException(e.getMessage(), e, ErrorCodeConstants.RF_0001);
+        }
+        return uiObject;
+    }
+
+    public static Object generateUIPanel(ParseXMLFile parseFile, AttributeInterface attributeEntity,
+                                         Dimension maxLabelDimension) throws CheckedException {
+        return generateUIPanel(parseFile, attributeEntity, true, maxLabelDimension);
+    }
+
+    /**
+     * This method returns the datatype of the given AttributeTypeInformation
+     * 
+     * @param attributeTypeInformation
+     * @return the datatype
+     */
+    private static String getDataType(AttributeTypeInformationInterface attributeTypeInformation) {
+        String dataTypeString = null;
+
+        if ((attributeTypeInformation instanceof StringAttributeTypeInformation)
+                || (attributeTypeInformation instanceof ByteArrayAttributeTypeInformation)) {
+            dataTypeString = "string";
+        } else if ((attributeTypeInformation instanceof IntegerAttributeTypeInformation)
+                || (attributeTypeInformation instanceof LongAttributeTypeInformation)
+                || (attributeTypeInformation instanceof FloatAttributeTypeInformation)
+                || (attributeTypeInformation instanceof DoubleAttributeTypeInformation)
+                || (attributeTypeInformation instanceof NumericAttributeTypeInformation)
+                || (attributeTypeInformation instanceof ShortAttributeTypeInformation)) {
+            dataTypeString = "number";
+        } else if (attributeTypeInformation instanceof BooleanAttributeTypeInformation) {
+            dataTypeString = "boolean";
+        } else if (attributeTypeInformation instanceof DateAttributeTypeInformation) {
+            dataTypeString = "date";
+        }
+        return dataTypeString;
+    }
+
 }
