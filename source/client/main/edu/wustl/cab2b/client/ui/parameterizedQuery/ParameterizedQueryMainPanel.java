d2d753c82826aa51bb0eeccf8e880423e49e1f9a
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-10-17 12:50:47 +0000

    Set dialog box resizable false

--- source/client/main/edu/wustl/cab2b/client/ui/parameterizedQuery/ParameterizedQueryMainPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/parameterizedQuery/ParameterizedQueryMainPanel.java
@@ -121,7 +121,7 @@ public class ParameterizedQueryMainPanel extends Cab2bPanel {
     public void showInDialog() {
         Dimension dimension = MainFrame.mainframeScreenDimesion;
         dialog = WindowUtilities.setInDialog(NewWelcomePanel.mainFrame, this, "Unsaved Condition", new Dimension(
-                (int) (dimension.width * 0.80), (int) (dimension.height * 0.75)), true, true);
+                (int) (dimension.width * 0.80), (int) (dimension.height * 0.75)), true, false);
         dialog.setVisible(true);
 
     }
