62e4d72fdd8b6122a338c986f9e0f42683ba532b
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-12-21 11:21:58 +0000

    added  getColumnName() method

--- source/client/main/edu/wustl/cab2b/client/ui/controls/sheet/JSheetViewDataModel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/controls/sheet/JSheetViewDataModel.java
@@ -14,6 +14,7 @@ import javax.swing.table.*;
 public class JSheetViewDataModel extends AbstractTableModel {
 
     JTable tblData;
+
     TableModel compositeDataModel;
 
     JSheetViewDataModel(JTable tblData, TableModel compositeDataModel) {
@@ -43,5 +44,9 @@ public class JSheetViewDataModel extends AbstractTableModel {
 
     public TableModel getJSheetDataModel() {
         return compositeDataModel;
+    }
+
+    public String getColumnName(int column) {
+        return tblData.getColumnName(column);
     }
 }
