1976808a68a54497b2a5b99584b9e09a5094e721
chetan_patil <chetan_patil@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-12-27 06:47:53 +0000

    Formatted

--- source/common/main/edu/wustl/cab2b/common/category/DataCategory.java
+++ source/common/main/edu/wustl/cab2b/common/category/DataCategory.java
@@ -2,30 +2,26 @@ package edu.wustl.cab2b.common.category;
 
 import edu.wustl.common.querysuite.metadata.category.Category;
 
-public class DataCategory extends Category 
-{
-       /**
-        * DataCategory constructor  
-        * @param category
-        */
-    public DataCategory(Category category)
-    {
+public class DataCategory extends Category {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Default Constructor
+     */
+    public DataCategory() {
+
+    }
+
+    /**
+     * DataCategory constructor  
+     * @param category
+     */
+    public DataCategory(Category category) {
         this.setCategoryEntity(category.getCategoryEntity());
         this.setDeEntityId(category.getDeEntityId());
         this.setParentCategory(category.getParentCategory());
         this.setRootClass(category.getRootClass());
         this.setSubCategories(category.getSubCategories());
-        
     }
-    
-   /**
-    * Default Constructor
-    *
-    */
-    public DataCategory()
-    {
-        
-    }
-    
 
 }
