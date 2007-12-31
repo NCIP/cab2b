5d79150a417436fe38d53b52a41e1b26bddce080
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2007-12-31 13:50:13 +0000

    Added test cases

---
+++ source/server/test/edu/wustl/cab2b/server/queryengine/utils/TreeNodeTest.java
@@ -0,0 +1,39 @@
+package edu.wustl.cab2b.server.queryengine.utils;
+
+import junit.framework.TestCase;
+
+/**
+ * @author chandrakant_talele
+ */
+public class TreeNodeTest extends TestCase {
+    public void testSetGetValue() {
+        TreeNode<String> node = new TreeNode<String>("rootNode");
+        assertEquals("rootNode", node.getValue());
+        assertEquals(0, node.getChildren().size());
+    }
+
+    public void testAddChildValue() {
+        TreeNode<String> parent = new TreeNode<String>("rootNode");
+        parent.addChildValue("child");
+        assertEquals(1, parent.getChildren().size());
+        assertEquals("child", parent.getChildren().get(0).getValue());
+    }
+
+    public void testAddChild() {
+        TreeNode<String> parent = new TreeNode<String>("rootNode");
+        TreeNode<String> child = new TreeNode<String>("child");
+        parent.addChild(child);
+        assertEquals(1, parent.getChildren().size());
+        assertEquals("child", parent.getChildren().get(0).getValue());
+        // TODO need to see this assertEquals(parent, child.getParent());
+    }
+
+    public void testIsLeaf() {
+        TreeNode<String> parent = new TreeNode<String>("rootNode");
+        TreeNode<String> child = new TreeNode<String>("child");
+        parent.addChild(child);
+        assertTrue(child.isLeaf());
+        assertFalse(parent.isLeaf());
+        assertTrue(parent.isRoot());
+    }
+}
