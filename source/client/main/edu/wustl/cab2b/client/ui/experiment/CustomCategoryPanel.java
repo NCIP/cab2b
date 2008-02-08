9f12c51fe5036d3382c30fc1b6e83e982d47e89a
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-02-08 13:56:05 +0000

    changes made to fix bug 6887

--- source/client/main/edu/wustl/cab2b/client/ui/experiment/CustomCategoryPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/experiment/CustomCategoryPanel.java
@@ -16,6 +16,7 @@ import javax.swing.BorderFactory;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import org.jdesktop.swingx.JXFrame;
 
@@ -47,228 +48,270 @@ import edu.wustl.cab2b.common.util.Utility;
 
 public class CustomCategoryPanel extends JXFrame {
 
-    private Cab2bComboBox dataListCombo;
+	private Cab2bComboBox dataListCombo;
 
-    private Cab2bComboBox categoryCombo;
+	private Cab2bComboBox categoryCombo;
 
-    private Cab2bPanel finalPanel;
+	private Cab2bPanel finalPanel;
 
-    private Cab2bPanel warningPanel;
+	private Cab2bButton saveButton;
 
-    private Cab2bButton saveButton;
+	private Cab2bButton cancelButton;
 
-    private Cab2bLabel customDataCategoryLabel;
+	private Cab2bLabel customDataCategoryLabel;
 
-    private Cab2bLabel dataListLabel;
+	private Cab2bLabel dataListLabel;
 
-    private JLabel label;
+	private Cab2bLabel categoryLabel;
 
-    private Cab2bLabel categoryLabel;
+	private Cab2bTextField customDataCategoryText;
 
-    private Cab2bTextField customDataCategoryText;
+	CustomDataCategoryModel customDataCategoryModel;
 
-    CustomDataCategoryModel customDataCategoryModel;
+	private JDialog dialog;
 
-    private JDialog dialog;
+	private DataListMetadata dataListMetadata;
 
-    private DataListMetadata dataListMetadata;
+	private AccumulatorPanel accumulatorPanel;
 
-    private AccumulatorPanel accumulatorPanel;
+	private Collection<UserObjectWrapper> availableAttributeCollection = new ArrayList<UserObjectWrapper>();
 
-    private Collection<UserObjectWrapper> availableAttributeCollection = new ArrayList<UserObjectWrapper>();
+	private Cab2bPanel middlePanel;
 
-    private Cab2bPanel middlePanel;
+	private Experiment experiment;
 
-    private Experiment experiment;
+	private DefaultComboBoxModel categoryComboboxModel = new DefaultComboBoxModel();
 
-    private DefaultComboBoxModel categoryComboboxModel = new DefaultComboBoxModel();
+	public CustomCategoryPanel(Experiment exp) {
+		this.experiment = exp;
+		initGUI();
+	}
 
-    public CustomCategoryPanel(Experiment exp) {
-        this.experiment = exp;
+	public void initGUI() {
+		dataListCombo = new Cab2bComboBox();
+		categoryCombo = new Cab2bComboBox();
+		dataListCombo.setPreferredSize(new Dimension(150, 20));
 
-        warningPanel = new Cab2bPanel(new RiverLayout(10, 0));
-        initGUI();
-    }
+		ExperimentBusinessInterface expBus = (ExperimentBusinessInterface) CommonUtils
+				.getBusinessInterface(EjbNamesConstants.EXPERIMENT,
+						ExperimentHome.class);
+		try {
+			customDataCategoryModel = expBus.getDataCategoryModel(experiment);
+		} catch (RemoteException e) {
+			CommonUtils.handleException(e, CustomCategoryPanel.this, true,
+					true, true, false);
+		} catch (CheckedException e) {
+			CommonUtils.handleException(e, CustomCategoryPanel.this, true,
+					true, true, false);
+		}
 
-    public void initGUI() {
-        dataListCombo = new Cab2bComboBox();
-        categoryCombo = new Cab2bComboBox();
-        dataListCombo.setPreferredSize(new Dimension(150, 20));
+		List<IdName> dataListIdName = customDataCategoryModel
+				.getDataListIdName();
 
-        ExperimentBusinessInterface expBus = (ExperimentBusinessInterface) CommonUtils.getBusinessInterface(
-                                                                                                            EjbNamesConstants.EXPERIMENT,
-                                                                                                            ExperimentHome.class);
-        try {
-            customDataCategoryModel = expBus.getDataCategoryModel(experiment);
-        } catch (RemoteException e) {
-            CommonUtils.handleException(e, CustomCategoryPanel.this, true, true, true, false);
-        } catch (CheckedException e) {
-            CommonUtils.handleException(e, CustomCategoryPanel.this, true, true, true, false);
-        }
+		DefaultComboBoxModel dataListModel = new DefaultComboBoxModel();
 
-        List<IdName> dataListIdName = customDataCategoryModel.getDataListIdName();
+		dataListModel.addElement("--Select DataList--");
+		for (IdName idName : dataListIdName) {
+			dataListModel.addElement(idName);
+		}
+		dataListCombo.setModel(dataListModel);
 
-        DefaultComboBoxModel dataListModel = new DefaultComboBoxModel();
+		class DataListComboListener implements ItemListener {
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					categoryComboboxModel.removeAllElements();
+					IdName selectedIdName = (IdName) e.getItem();
+					List<IdName> rooCategoeisList = customDataCategoryModel
+							.getRooCategories(selectedIdName.getId());
+					if (rooCategoeisList != null) {
+						for (IdName idName : rooCategoeisList) {
+							categoryComboboxModel.addElement(idName);
+						}
+					}
+				}
+			}
+		}
+		dataListCombo.addItemListener(new DataListComboListener());
+		dataListCombo.setSelectedIndex(0);
+		categoryCombo.setModel(categoryComboboxModel);
+		categoryCombo.setPreferredSize(new Dimension(250, 20));
+		middlePanel = new Cab2bPanel(new RiverLayout(5, 5));
+		accumulatorPanel = new AccumulatorPanel("Available Attributes",
+				"Selected Attributes");
+		middlePanel.add(accumulatorPanel);
+		categoryCombo.addItemListener(new CategoryComboListener(expBus));
+		categoryCombo.setSelectedItem(null);
+		customDataCategoryLabel = new Cab2bLabel("Custom Data Category Title:");
+		dataListLabel = new Cab2bLabel("Data List:");
+		categoryLabel = new Cab2bLabel("Category:");
+		customDataCategoryText = new Cab2bTextField();
+		customDataCategoryText.setPreferredSize(new Dimension(150, 20));
 
-        dataListModel.addElement("--Select DataList--");
-        for (IdName idName : dataListIdName) {
-            dataListModel.addElement(idName);
-        }
-        dataListCombo.setModel(dataListModel);
+		Cab2bPanel topPanel = new Cab2bPanel(new RiverLayout(5, 10));
+		topPanel.add(customDataCategoryLabel);
+		topPanel.add("tab ", customDataCategoryText);
+		topPanel.add("br", dataListLabel);
+		topPanel.add(dataListCombo);
+		topPanel.add(new JLabel("            "));
+		topPanel.add(categoryLabel);
+		topPanel.add(categoryCombo);
+		topPanel.setBorder(BorderFactory.createLineBorder(new Color(200, 200,
+				220)));
+		topPanel.setPreferredSize(new Dimension(
+				Constants.WIZARD_SIZE2_DIMENSION.width, 80));
 
-        class DataListComboListener implements ItemListener {
-            public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == ItemEvent.SELECTED) {
-                    categoryComboboxModel.removeAllElements();
-                    IdName selectedIdName = (IdName) e.getItem();
-                    List<IdName> rooCategoeisList = customDataCategoryModel.getRooCategories(selectedIdName.getId());
-                    if (rooCategoeisList != null) {
-                        for (IdName idName : rooCategoeisList) {
-                            categoryComboboxModel.addElement(idName);
-                        }
-                    }
-                }
-            }
-        }
+		finalPanel = new Cab2bPanel(new BorderLayout());
+		finalPanel.add(topPanel, BorderLayout.NORTH);
+		finalPanel.add(middlePanel, BorderLayout.CENTER);
 
-        dataListCombo.addItemListener(new DataListComboListener());
-        dataListCombo.setSelectedIndex(0);
-        categoryCombo.setModel(categoryComboboxModel);
-        categoryCombo.setPreferredSize(new Dimension(250, 20));
-        middlePanel = new Cab2bPanel(new RiverLayout(5, 5));
-        accumulatorPanel = new AccumulatorPanel("Available Attributes", "Selected Attributes");
-        middlePanel.add(accumulatorPanel);
-        categoryCombo.addItemListener(new CategoryComboListener(expBus));
-        categoryCombo.setSelectedItem(null);
-        customDataCategoryLabel = new Cab2bLabel("Custom Data Category Title:");
-        dataListLabel = new Cab2bLabel("Data List:");
-        categoryLabel = new Cab2bLabel("Category:");
-        customDataCategoryText = new Cab2bTextField();
-        customDataCategoryText.setPreferredSize(new Dimension(150, 20));
+		saveButton = new Cab2bButton("Save");
+		saveButton.addActionListener(new SaveButtonActionListener());
 
-        Cab2bPanel topPanel = new Cab2bPanel(new RiverLayout(5, 10));
-        topPanel.add(customDataCategoryLabel);
-        topPanel.add("tab ", customDataCategoryText);
-        topPanel.add("br", dataListLabel);
-        topPanel.add(dataListCombo);
-        topPanel.add(new JLabel("            "));
-        topPanel.add(categoryLabel);
-        topPanel.add(categoryCombo);
-        topPanel.setBorder(BorderFactory.createLineBorder(new Color(200, 200, 220)));
-        topPanel.setPreferredSize(new Dimension(Constants.WIZARD_SIZE2_DIMENSION.width, 80));
+		cancelButton = new Cab2bButton("Cancel");
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dialog.dispose();
+			}
+		});
 
-        finalPanel = new Cab2bPanel(new BorderLayout());
-        finalPanel.add(topPanel, BorderLayout.NORTH);
-        finalPanel.add(middlePanel, BorderLayout.CENTER);
+		Cab2bPanel bottomPanel = new Cab2bPanel(new RiverLayout(5, 10));
+		bottomPanel.setPreferredSize(new Dimension(
+				Constants.WIZARD_SIZE2_DIMENSION.width, 40));
 
-        saveButton = new Cab2bButton("Save");
-        saveButton.addActionListener(new SaveButtonActionListener());
+		bottomPanel.add(" right ", saveButton);
+		bottomPanel.add(" right ", cancelButton);
+		finalPanel.add(bottomPanel, BorderLayout.SOUTH);
+		dialog = WindowUtilities.setInDialog(NewWelcomePanel.mainFrame,
+				finalPanel, "Custom Data Category",
+				Constants.WIZARD_SIZE2_DIMENSION, true, false);
+		dialog.setVisible(true);
+	}
 
-        Cab2bPanel bottomPanel = new Cab2bPanel(new BorderLayout());
-        bottomPanel.setPreferredSize(new Dimension(Constants.WIZARD_SIZE2_DIMENSION.width, 40));
-        Cab2bPanel saveButtonPanel = new Cab2bPanel(new RiverLayout(5, 10));
-        saveButtonPanel.add(saveButton);
-        label = new JLabel("");
-        warningPanel.add(label);
-        bottomPanel.add(warningPanel, BorderLayout.WEST);
-        bottomPanel.add(saveButtonPanel, BorderLayout.EAST);
-        finalPanel.add(bottomPanel, BorderLayout.SOUTH);
-        dialog = WindowUtilities.setInDialog(NewWelcomePanel.mainFrame, finalPanel, "Custom category",
-                                             Constants.WIZARD_SIZE2_DIMENSION, true, false);
-        dialog.setVisible(true);
-    }
+	class SaveButtonActionListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
 
-    class SaveButtonActionListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
+			CustomSwingWorker swingWorker = new CustomSwingWorker(
+					CustomCategoryPanel.this.finalPanel) {
+				@Override
+				protected void doNonUILogic() throws Exception {
+					Collection<UserObjectWrapper<AttributeInterface>> objectCollection = accumulatorPanel
+							.getSelectedObjects();
+					if (!customDataCategoryText.getText().equals("")
+							&& objectCollection != null
+							&& objectCollection.size() > 0) {
+						IdName entityName = (IdName) categoryCombo
+								.getSelectedItem();
 
-            CustomSwingWorker swingWorker = new CustomSwingWorker(CustomCategoryPanel.this.finalPanel) {
-                @Override
-                protected void doNonUILogic() throws Exception {
-                    if (!customDataCategoryText.getText().equals("")) {
-                        IdName entityName = (IdName) categoryCombo.getSelectedItem();
+						List<AttributeInterface> attributeList = new ArrayList<AttributeInterface>();
+						for (UserObjectWrapper<AttributeInterface> selectedObjects : objectCollection) {
+							attributeList.add(selectedObjects.getUserObject());
+						}
+						try {
+							DataListBusinessInterface dataListBI = (DataListBusinessInterface) CommonUtils
+									.getBusinessInterface(
+											EjbNamesConstants.DATALIST_BEAN,
+											DataListHomeInterface.class);
+							dataListMetadata = dataListBI
+									.saveCustomDataCategory(entityName,
+											attributeList,
+											customDataCategoryText.getText(),
+											experiment);
+						} catch (RemoteException e1) {
+							CommonUtils.handleException(e1,
+									CustomCategoryPanel.this, true, true, true,
+									false);
+						} catch (CheckedException e1) {
+							CommonUtils.handleException(e1,
+									CustomCategoryPanel.this, true, true, true,
+									false);
+						}
+					}
+				}
 
-                        Collection<UserObjectWrapper<AttributeInterface>> objectCollection = accumulatorPanel.getSelectedObjects();
-                        List<AttributeInterface> attributeList = new ArrayList<AttributeInterface>();
-                        for (UserObjectWrapper<AttributeInterface> selectedObjects : objectCollection) {
-                            attributeList.add(selectedObjects.getUserObject());
-                        }
-                        try {
-                            DataListBusinessInterface dataListBI = (DataListBusinessInterface) CommonUtils.getBusinessInterface(
-                                                                                                                                EjbNamesConstants.DATALIST_BEAN,
-                                                                                                                                DataListHomeInterface.class);
-                            dataListMetadata = dataListBI.saveCustomDataCategory(entityName, attributeList,
-                                                                                 customDataCategoryText.getText(),
-                                                                                 experiment);
-                        } catch (RemoteException e1) {
-                            CommonUtils.handleException(e1, CustomCategoryPanel.this, true, true, true, false);
-                        } catch (CheckedException e1) {
-                            CommonUtils.handleException(e1, CustomCategoryPanel.this, true, true, true, false);
-                        }
-                    }
-                }
+				@Override
+				protected void doUIUpdateLogic() throws Exception {
+					if (customDataCategoryText.getText().equals("")) {
+						JOptionPane.showMessageDialog(CustomCategoryPanel.this,
+								"Please enter valid name before saving.",
+								"Error", JOptionPane.ERROR_MESSAGE);
+					} else {
+						Collection<UserObjectWrapper<AttributeInterface>> objectCollection = accumulatorPanel
+								.getSelectedObjects();
+						if (objectCollection == null
+								|| objectCollection.size() == 0) {
+							JOptionPane
+									.showMessageDialog(
+											CustomCategoryPanel.this,
+											"Please add attributes in 'Selected Attributes' list before saving.",
+											"Error", JOptionPane.ERROR_MESSAGE);
 
-                @Override
-                protected void doUIUpdateLogic() throws Exception {
-                    if (customDataCategoryText.getText().equals("")) {
-                        label.setText("Please enter the name for the data category being saved");
-                        label.setForeground(Color.red);
-                    } else
-                        dialog.dispose();
-                }
+						} else
+							dialog.dispose();
+					}
+				}
 
-            };
-            swingWorker.start();
-        }
+			};
+			swingWorker.start();
+		}
+	}
 
-    }
+	/**
+	 * Select category combobox listener, displays attributes for selected
+	 * category entity into "Available Attribute" list.
+	 * 
+	 * @author deepak_shingan
+	 */
+	class CategoryComboListener implements ItemListener {
+		ExperimentBusinessInterface expBus;
 
-    /**
-     * Select category combobox listener, 
-     * displays attributes for selected category entity into "Available Attribute" list.   
-     * @author deepak_shingan
-     */
-    class CategoryComboListener implements ItemListener {
-        ExperimentBusinessInterface expBus;
+		CategoryComboListener(ExperimentBusinessInterface expBus) {
+			this.expBus = expBus;
+		}
 
-        CategoryComboListener(ExperimentBusinessInterface expBus) {
-            this.expBus = expBus;
-        }
+		public void itemStateChanged(ItemEvent e) {
 
-        public void itemStateChanged(ItemEvent e) {
+			if (e.getStateChange() == ItemEvent.SELECTED) {
+				IdName selectedIdName = (IdName) e.getItem();
+				try {
+					Collection<AttributeInterface> attributes = expBus
+							.getAllAttributes(selectedIdName.getId());
+					availableAttributeCollection.clear();
 
-            if (e.getStateChange() == ItemEvent.SELECTED) {
-                IdName selectedIdName = (IdName) e.getItem();
-                try {
-                    Collection<AttributeInterface> attributes = expBus.getAllAttributes(selectedIdName.getId());
-                    availableAttributeCollection.clear();
+					for (AttributeInterface attributeInterface : attributes) {
+						String name = Utility.getDisplayName(attributeInterface
+								.getEntity())
+								+ ": "
+								+ CommonUtils
+										.getFormattedString(attributeInterface
+												.getName());
+						int left = name.indexOf("(DataList");
+						int right = name.indexOf(")");
+						StringBuffer buffer = new StringBuffer(name);
+						buffer.delete(left, ++right);
+						name = buffer.toString();
+						availableAttributeCollection
+								.add(new UserObjectWrapper<AttributeInterface>(
+										attributeInterface, name));
+					}
+					accumulatorPanel.setModel(availableAttributeCollection,
+							accumulatorPanel.getSelectedObjects());
+					accumulatorPanel.revalidate();
+				} catch (RemoteException e1) {
+					CommonUtils.handleException(e1, CustomCategoryPanel.this,
+							true, true, true, false);
+				} catch (CheckedException e1) {
+					CommonUtils.handleException(e1, CustomCategoryPanel.this,
+							true, true, true, false);
+				}
 
-                    for (AttributeInterface attributeInterface : attributes) {
-                        String name = Utility.getDisplayName(attributeInterface.getEntity()) + ": "
-                                + CommonUtils.getFormattedString(attributeInterface.getName());
-                        int left = name.indexOf("(DataList");
-                        int right = name.indexOf(")");
-                        StringBuffer buffer = new StringBuffer(name);
-                        buffer.delete(left, ++right);
-                        name = buffer.toString();
-                        availableAttributeCollection.add(new UserObjectWrapper<AttributeInterface>(
-                                attributeInterface, name));
-                    }
-                    accumulatorPanel.setModel(availableAttributeCollection, accumulatorPanel.getSelectedObjects());
-                    accumulatorPanel.revalidate();
-                } catch (RemoteException e1) {
-                    CommonUtils.handleException(e1, CustomCategoryPanel.this, true, true, true, false);
-                } catch (CheckedException e1) {
-                    CommonUtils.handleException(e1, CustomCategoryPanel.this, true, true, true, false);
-                }
+			}
+		}
+	}
 
-            }
-        }
-    }
-
-    /**
-     * @return Returns the dataListMetadata.
-     */
-    public DataListMetadata getDataListMetadata() {
-        return dataListMetadata;
-    }
+	/**
+	 * @return Returns the dataListMetadata.
+	 */
+	public DataListMetadata getDataListMetadata() {
+		return dataListMetadata;
+	}
 }
