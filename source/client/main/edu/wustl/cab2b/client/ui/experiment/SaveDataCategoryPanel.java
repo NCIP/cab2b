61f38be60556f06d286ccc57e377d4787b0066f6
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-02-08 05:59:15 +0000

    Changes made to incorporate UI comments

--- source/client/main/edu/wustl/cab2b/client/ui/experiment/SaveDataCategoryPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/experiment/SaveDataCategoryPanel.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 
 import edu.wustl.cab2b.client.ui.RiverLayout;
 import edu.wustl.cab2b.client.ui.WindowUtilities;
@@ -21,81 +22,85 @@ import edu.wustl.cab2b.client.ui.util.CommonUtils;
 
 /**
  * The panel to show dialog for saving a subset of the datalist as a category
+ * 
  * @author Juber Patel
- *
+ * 
  */
 public class SaveDataCategoryPanel extends Cab2bPanel {
-    private ExperimentDataCategoryGridPanel gridPanel;
+	private ExperimentDataCategoryGridPanel gridPanel;
 
-    private Cab2bLabel titleLabel;
+	private Cab2bLabel titleLabel;
 
-    private Cab2bTextField titleField;
+	private Cab2bTextField titleField;
 
-    private Cab2bButton cancelButton;
+	private Cab2bButton cancelButton;
 
-    private Cab2bButton saveButton;
+	private Cab2bButton saveButton;
 
-    private JDialog dialog;
+	private JDialog dialog;
 
-    /**
-     * 
-     * @param gridPanel the grid panel for which this dialog is shown
-     */
-    public SaveDataCategoryPanel(ExperimentDataCategoryGridPanel gridPanel) {
-        this.gridPanel = gridPanel;
-        initGUI();
-        initEventModel();
-        showAsDialog();
-    }
+	/**
+	 * 
+	 * @param gridPanel
+	 *            the grid panel for which this dialog is shown
+	 */
+	public SaveDataCategoryPanel(ExperimentDataCategoryGridPanel gridPanel) {
+		this.gridPanel = gridPanel;
+		initGUI();
+		initEventModel();
+		showAsDialog();
+	}
 
-    private void initGUI() {
-        this.setLayout(new RiverLayout(5, 5));
-        titleLabel = new Cab2bLabel("Title");
-        titleField = new Cab2bTextField();
-        cancelButton = new Cab2bButton("Cancel");
-        saveButton = new Cab2bButton("Save");
+	private void initGUI() {
+		this.setLayout(new RiverLayout(5, 5));
+		titleLabel = new Cab2bLabel("Title");
+		titleField = new Cab2bTextField();
+		cancelButton = new Cab2bButton("Cancel");
+		saveButton = new Cab2bButton("Save");
 
-        add(titleLabel);
-        add("tab hfill", titleField);
-        add("br right", saveButton);
-        add(cancelButton);
+		add(titleLabel);
+		add("tab hfill", titleField);
+		add("br right", saveButton);
+		add(cancelButton);
 
-    }
+	}
 
-    private void initEventModel() {
-        cancelButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                dialog.dispose();
-            }
-        });
+	private void initEventModel() {
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				dialog.dispose();
+			}
+		});
 
-        saveButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                String title = titleField.getText();
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				String title = titleField.getText();
 
-                if (!CommonUtils.isNameValid(title)) {
-                    //TO DO
-                    //invalid title
-                }
+				if (!CommonUtils.isNameValid(title)) {
+					JOptionPane.showMessageDialog(SaveDataCategoryPanel.this,
+							"Please enter valid name before saving.", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					gridPanel.saveDataCategory(title);
+					dialog.dispose();
+				}
+			}
 
-                gridPanel.saveDataCategory(title);
-                dialog.dispose();
-            }
+		});
 
-        });
+	}
 
-    }
+	private JDialog showAsDialog() {
+		Dimension mainDimension = MainFrame.mainframeScreenDimesion;
+		Dimension dialogDimension = new Dimension(
+				(int) (mainDimension.width * 0.35),
+				(int) (mainDimension.height * 0.15));
 
-    private JDialog showAsDialog() {
-        Dimension mainDimension = MainFrame.mainframeScreenDimesion;
-        Dimension dialogDimension = new Dimension((int) (mainDimension.width * 0.35),
-                (int) (mainDimension.height * 0.15));
+		dialog = WindowUtilities.setInDialog(NewWelcomePanel.mainFrame, this,
+				"Save As Data Category", dialogDimension, true, false);
+		dialog.setVisible(true);
 
-        dialog = WindowUtilities.setInDialog(NewWelcomePanel.mainFrame, this, "Save As Data Category",
-                                             dialogDimension, true, false);
-        dialog.setVisible(true);
-
-        return dialog;
-    }
+		return dialog;
+	}
 
 }
