6a51b1ae12fe872d9cd6313b4e9dd5bf3ee66271
srinath_k <srinath_k@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-05-06 12:21:43 +0000

    *** empty log message ***

---
+++ source/server/main/edu/wustl/cab2b/server/path/InterModelConnectionBizLogic.java
@@ -0,0 +1,53 @@
+package edu.wustl.cab2b.server.path;
+
+import java.sql.Connection;
+
+import edu.common.dynamicextensions.domaininterface.AttributeInterface;
+import edu.wustl.cab2b.server.path.PathConstants.AssociationType;
+import edu.wustl.cab2b.server.util.ConnectionUtil;
+import edu.wustl.cab2b.server.util.SQLQueryUtil;
+
+/**
+ * Collection of methods relation to database operations on the table INTER_MODEL_ASSOCIATION.
+ * @author srinath_k
+ *
+ */
+public class InterModelConnectionBizLogic {
+    public void saveInterModelConnection(AttributeInterface a1, AttributeInterface a2) {
+        saveInterModelConnection(new InterModelConnection(a1, a2));
+    }
+
+    public void saveInterModelConnection(InterModelConnection imc) {
+        if (PathFinder.getInstance().doesInterModelConnectionExist(imc)) {
+            return;
+        }
+        Connection conn = ConnectionUtil.getConnection();
+        long nextId = PathBuilder.getNextAssociationId(2, conn);
+        saveInterModelConnection2(imc, nextId, conn);
+        saveInterModelConnection2(imc.mirror(), nextId + 1, conn);
+        ConnectionUtil.close(conn);
+    }
+
+    private void saveInterModelConnection2(InterModelConnection imc, long id, Connection conn) {
+        String sql = "insert into inter_model_association(association_id, left_entity_id, left_attribute_id, right_entity_id, right_attribute_id) values (";
+        sql = sql + id + "," + imc.getLeftEntityId() + "," + imc.getLeftAttributeId() + ","
+                + imc.getRightEntityId() + "," + imc.getRightAttributeId() + ");";
+        SQLQueryUtil.executeUpdate(sql, conn);
+
+        sql = "insert into association(association_id, association_type) values (";
+        sql = sql + id + "," + AssociationType.INTER_MODEL_ASSOCIATION.getValue() + ");";
+        SQLQueryUtil.executeUpdate(sql, conn);
+        // TODO transaction??
+
+        PathFinder.getInstance().addInterModelConnection(imc);
+    }
+
+    //    @Test
+    //    public void foo() throws Exception {
+    //        Logger.configure();
+    //        Class.forName("com.mysql.jdbc.Driver").newInstance();
+    //        String url = "jdbc:mysql://localhost:3306/cab2b";
+    //        Connection conn = DriverManager.getConnection(url, "root", "");
+    //        saveInterModelConnection2(new InterModelConnection(0l, 0l, 0l, 0l), -5l, conn);
+    //    }
+}
