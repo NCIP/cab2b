deeb636eea7976da45bab955767bc7fda5733855
deepak_shingan <deepak_shingan@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-07-28 11:54:28 +0000

    Code review changes

--- source/server/main/edu/wustl/cab2b/server/ejb/experiment/ExperimentSessionBean.java
+++ source/server/main/edu/wustl/cab2b/server/ejb/experiment/ExperimentSessionBean.java
@@ -27,87 +27,84 @@ import edu.wustl.common.util.dbManager.DAOException;
  * @author chetan_bh
  * 
  */
-public class ExperimentSessionBean extends AbstractStatelessSessionBean implements
-		ExperimentBusinessInterface {
+public class ExperimentSessionBean extends AbstractStatelessSessionBean implements ExperimentBusinessInterface {
 
-	private static final long serialVersionUID = 782660710949035029L;
+    private static final long serialVersionUID = 782660710949035029L;
 
-	public void addExperiment(Object exp) throws BizLogicException, UserNotAuthorizedException,
-			RemoteException {
-		(new ExperimentOperations()).addExperiment(exp);
-	}
+    public void addExperiment(Object exp) throws BizLogicException, UserNotAuthorizedException, RemoteException {
+        (new ExperimentOperations()).addExperiment(exp);
+    }
 
-	public void copy(Object exp, Object tarExpGrp) throws BizLogicException,
-			UserNotAuthorizedException, RemoteException {
-		(new ExperimentOperations()).copy(exp, tarExpGrp);
-	}
+    public void copy(Object exp, Object tarExpGrp) throws BizLogicException, UserNotAuthorizedException,
+            RemoteException {
+        (new ExperimentOperations()).copy(exp, tarExpGrp);
+    }
 
-	public Vector getExperimentHierarchy() throws ClassNotFoundException, DAOException,
-			RemoteException {
-		return (new ExperimentOperations()).getExperimentHierarchy();
-	}
+    public Vector getExperimentHierarchy() throws ClassNotFoundException, DAOException, RemoteException {
+        return (new ExperimentOperations()).getExperimentHierarchy();
+    }
 
-	public void move(Object exp, Object srcExpGrp, Object tarExpGrp) throws DAOException,
-			BizLogicException, UserNotAuthorizedException, RemoteException {
-		(new ExperimentOperations()).move(exp, srcExpGrp, tarExpGrp);
-	}
+    public void move(Object exp, Object srcExpGrp, Object tarExpGrp) throws DAOException, BizLogicException,
+            UserNotAuthorizedException, RemoteException {
+        (new ExperimentOperations()).move(exp, srcExpGrp, tarExpGrp);
+    }
 
-	public Experiment getExperiment(Long id) throws RemoteException, DAOException {
+    public Experiment getExperiment(Long id) throws RemoteException, DAOException {
 
-		return (new ExperimentOperations()).getExperiment(id);
-	}
+        return (new ExperimentOperations()).getExperiment(id);
+    }
 
-	public Set<Set<EntityInterface>> getDataListEntitySet(Experiment exp) throws RemoteException {
-		return (new ExperimentOperations()).getDataListEntitySet(exp);
-	}
+    public Set<Set<EntityInterface>> getDataListEntitySet(Experiment exp) throws RemoteException {
+        return (new ExperimentOperations()).getDataListEntitySet(exp);
+    }
 
-	/**
-	 * @see edu.wustl.cab2b.common.experiment.ExperimentBusinessInterface#addExperiment(java.lang.Long,
-	 *      edu.wustl.cab2b.common.domain.Experiment)
-	 */
-	public void addExperiment(Long experimentGroupId, Experiment experiment)
-			throws RemoteException, BizLogicException, UserNotAuthorizedException, DAOException {
-		(new ExperimentOperations()).addExperiment(experimentGroupId, experiment);
-	}
+    /**
+     * @see edu.wustl.cab2b.common.experiment.ExperimentBusinessInterface#addExperiment(java.lang.Long,
+     *      edu.wustl.cab2b.common.domain.Experiment)
+     */
+    public void addExperiment(Long experimentGroupId, Experiment experiment) throws RemoteException,
+            BizLogicException, UserNotAuthorizedException, DAOException {
+        (new ExperimentOperations()).addExperiment(experimentGroupId, experiment);
+    }
 
-	/**
-	 * save the given data as a data category
-	 * 
-	 * @param title
-	 *            the title for the category
-	 * @param attributes
-	 *            list of attributes needed for the new entity
-	 * @param data
-	 *            the data to be saved
-	 * @return the newly created entity
-	 */
-	public EntityInterface saveDataCategory(String title, List<AttributeInterface> attributes,
-			Object[][] data) throws RemoteException {
-		return new ExperimentOperations().saveDataCategory(title, attributes, data);
+    /**
+     * save the given data as a data category
+     * 
+     * @param title
+     *            the title for the category
+     * @param attributes
+     *            list of attributes needed for the new entity
+     * @param data
+     *            the data to be saved
+     * @return the newly created entity
+     */
+    public EntityInterface saveDataCategory(String title, List<AttributeInterface> attributes, Object[][] data)
+            throws RemoteException {
+        return new ExperimentOperations().saveDataCategory(title, attributes, data);
 
-	}
+    }
 
-	public void addDataListToExperiment(Long experimentId, Long dataListMataDataId)
-			throws RemoteException {
-		new ExperimentOperations().addDataListToExperiment(experimentId, dataListMataDataId);
-	}
+    public void addDataListToExperiment(Long experimentId, Long dataListMataDataId) throws RemoteException {
+        new ExperimentOperations().addDataListToExperiment(experimentId, dataListMataDataId);
+    }
 
-	public long getRootId(DataListMetadata dl) throws HibernateException {
-		return new ExperimentOperations().getRootId(dl);
-	}
+    public long getRootId(DataListMetadata dl) throws HibernateException {
+        return new ExperimentOperations().getRootId(dl);
+    }
 
-	public CustomDataCategoryModel getDataCategoryModel(Experiment experiment) throws CheckedException {
-		return new ExperimentOperations().getDataCategoryModel(experiment);
-	}
+    public CustomDataCategoryModel getDataCategoryModel(Experiment experiment) throws CheckedException {
+        return new ExperimentOperations().getDataCategoryModel(experiment);
+    }
 
+    public EntityInterface getEnityById(Long id) throws CheckedException, RemoteException {
+        return new ExperimentOperations().getEnityById(id);
+    }
 
-	public EntityInterface getEnityById(Long id) throws CheckedException, RemoteException {
-		return new ExperimentOperations().getEnityById(id);
-	}
+    public Collection<AttributeInterface> getAllAttributes(Long id) throws RemoteException, CheckedException {
+        return new ExperimentOperations().getAllAttributes(id);
+    }
 
-	public Collection<AttributeInterface> getAllAttributes(Long id) throws RemoteException,
-			CheckedException {
-		return new ExperimentOperations().getAllAttributes(id);
-	}
-
+    public List<Experiment> getExperimentsForUser(String userName) throws RemoteException {
+        return new ExperimentOperations().getLatestExperimentForUser(userName);
+    }
 }
