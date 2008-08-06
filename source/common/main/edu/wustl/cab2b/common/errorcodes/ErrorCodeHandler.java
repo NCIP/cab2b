c2c5c9f56e9e8a35c1f0ec25965fd92295f64491
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-08-06 11:54:53 +0000

    commented unused methods

--- source/common/main/edu/wustl/cab2b/common/errorcodes/ErrorCodeHandler.java
+++ source/common/main/edu/wustl/cab2b/common/errorcodes/ErrorCodeHandler.java
@@ -1,7 +1,5 @@
 package edu.wustl.cab2b.common.errorcodes;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -11,43 +9,51 @@ import java.util.ResourceBundle;
 public class ErrorCodeHandler {
     private static ResourceBundle bundle;
 
+    /**
+     * @param baseName name of bundle
+     */
     public static void initBundle(String baseName) {
         bundle = ResourceBundle.getBundle(baseName);
     }
-
+    /**
+     * @param theKey Key 
+     * @return the value
+     */
     public static String getValue(String theKey) {
         return bundle.getString(theKey);
     }
-
-    /**
-     * This method should be used when you want to customize error message with multiple replacement parameters
-     * 
-     * @param theKey - error key
-     * @param placeHolders - replacement Strings
-     * @return - complete error message
-     */
-    public static String getValue(String theKey, List placeHolders) {
-        String msg = bundle.getString(theKey);
-        StringBuffer message = new StringBuffer(msg);
-
-        for (int i = 0; i < placeHolders.size(); i++) {
-            message.replace(message.indexOf("{"), message.indexOf("}") + 1, (String) placeHolders.get(i));
-        }
-        return message.toString();
-    }
-
-    /**
-     * This method should be used when you want to customize error message with single replacement parameter
-     * 
-     * @param theKey - error key
-     * @param placeHolders - replacement Strings
-     * @return - complete error message
-     */
-    public static String getValue(String theKey, String placeHolder) {
-        List<String> temp = new ArrayList<String>();
-        temp.add(placeHolder);
-        return getValue(theKey, temp);
-    }
+// TODO looks like this code is not used anymore.
+//  Need to check it and remove it
+//
+//    /**
+//     * This method should be used when you want to customize error message with multiple replacement parameters
+//     * 
+//     * @param theKey - error key
+//     * @param placeHolders - replacement Strings
+//     * @return - complete error message
+//     */
+//    public static String getValue(String theKey, List placeHolders) {
+//        String msg = bundle.getString(theKey);
+//        StringBuffer message = new StringBuffer(msg);
+//
+//        for (int i = 0; i < placeHolders.size(); i++) {
+//            message.replace(message.indexOf("{"), message.indexOf("}") + 1, (String) placeHolders.get(i));
+//        }
+//        return message.toString();
+//    }
+//
+//    /**
+//     * This method should be used when you want to customize error message with single replacement parameter
+//     * 
+//     * @param theKey - error key
+//     * @param placeHolders - replacement Strings
+//     * @return - complete error message
+//     */
+//    public static String getValue(String theKey, String placeHolder) {
+//        List<String> temp = new ArrayList<String>();
+//        temp.add(placeHolder);
+//        return getValue(theKey, temp);
+//    }
 
     /**
      * Returns the error message corresponding to the error code passed 
