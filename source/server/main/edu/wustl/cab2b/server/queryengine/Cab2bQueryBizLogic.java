efd7b3721e278f4a665a274361b239254e43ac04
chetan_patil <chetan_patil@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-08-21 05:59:21 +0000

    Removed unnecessory methods

--- source/server/main/edu/wustl/cab2b/server/queryengine/Cab2bQueryBizLogic.java
+++ source/server/main/edu/wustl/cab2b/server/queryengine/Cab2bQueryBizLogic.java
@@ -3,11 +3,8 @@
  */
 package edu.wustl.cab2b.server.queryengine;
 
-import edu.common.dynamicextensions.domaininterface.EntityInterface;
-import edu.wustl.cab2b.common.cache.AbstractEntityCache;
 import edu.wustl.cab2b.common.queryengine.Cab2bQuery;
 import edu.wustl.cab2b.common.queryengine.ICab2bParameterizedQuery;
-import edu.wustl.cab2b.server.cache.EntityCache;
 import edu.wustl.common.querysuite.bizlogic.QueryBizLogic;
 
 /**
@@ -15,25 +12,6 @@ import edu.wustl.common.querysuite.bizlogic.QueryBizLogic;
  * @author chetan_patil
  */
 public class Cab2bQueryBizLogic extends QueryBizLogic<ICab2bParameterizedQuery> {
-
-    public void preProcessQuery(ICab2bParameterizedQuery caB2BQuery) {
-        super.preProcessQuery(caB2BQuery);
-
-        EntityInterface entity = caB2BQuery.getOutputEntity();
-        ((Cab2bQuery) caB2BQuery).setEntityId(entity.getId());
-    }
-
-    /**
-     * @see edu.wustl.cab2b.server.queryengine.QueryProcessor#postProcessQuery(edu.wustl.common.querysuite.queryobject.IQuery)
-     */
-    public void postProcessQuery(ICab2bParameterizedQuery caB2BQuery) {
-        super.postProcessQuery(caB2BQuery);
-
-        Long entityId = ((Cab2bQuery) caB2BQuery).getEntityId();
-        AbstractEntityCache abstractEntityCache = EntityCache.getCache();
-        EntityInterface entity = abstractEntityCache.getEntityById(entityId);
-        caB2BQuery.setOutputEntity(entity);
-    }
 
     /** 
      * @see edu.wustl.common.querysuite.queryobject.util.QueryProcessor#getQueryClassName()
