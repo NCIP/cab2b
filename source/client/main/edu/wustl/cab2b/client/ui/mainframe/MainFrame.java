e0ac0a55554d9e188205564fa880d5f6c9243de4
chetan_patil <chetan_patil@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-08-27 09:57:49 +0000

    Removed unused import

--- source/client/main/edu/wustl/cab2b/client/ui/mainframe/MainFrame.java
+++ source/client/main/edu/wustl/cab2b/client/ui/mainframe/MainFrame.java
@@ -21,7 +21,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
 import javax.swing.WindowConstants;
