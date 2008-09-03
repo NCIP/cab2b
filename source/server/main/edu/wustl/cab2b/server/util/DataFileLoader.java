c2619f6e0804c303ff96ad688fc6edb37e5045f1
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-09-03 06:00:09 +0000

    Addressed JCSC and PMD errors. Modified the logger

--- source/server/main/edu/wustl/cab2b/server/util/DataFileLoader.java
+++ source/server/main/edu/wustl/cab2b/server/util/DataFileLoader.java
@@ -10,74 +10,102 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+
 import edu.wustl.cab2b.common.errorcodes.ErrorCodeConstants;
 import edu.wustl.cab2b.common.exception.RuntimeException;
-import edu.wustl.common.util.logger.Logger;
 
 /**
  * Bulk data loader for any database.
  * @author Chandrakant Talele
  */
 public class DataFileLoader implements DataFileLoaderInterface {
+    private static final Logger logger = edu.wustl.common.util.logger.Logger.getLogger(DataFileLoader.class);
 
     /**
-     * @see edu.wustl.cab2b.server.util.DataFileLoaderInterface#loadDataFromFile(java.sql.Connection, java.lang.String, java.lang.String, java.lang.String, java.lang.Class<?>[], java.lang.String)
+     * Loads data from given file into given table.
+     * <b> NOTE : </b> This method will not create table in database. It assumes that table is already present
+     * @param con Connection to use to fire SQL
+     * @param fileName Full path of data file.
+     * @param columns Data columns in table. They should be in format (column1,column2,...) braces included
+     * @param tableName Name of the table in which data to load.
+     * @param dataTypes Data type of each column. 
+     * @param fieldSeperator String used to separate values of columns in one line of given file.
+     * Order of this array must be same as columns names given in "columns" parameter above.
      */
-    public void loadDataFromFile(Connection connection, String fileName, String columns, String tableName, Class<?>[] dataTypes, String fieldSeperator) {
-        Logger.out.debug("Entering method loadDataFromFile()");
+    public void loadDataFromFile(Connection con, String fileName, String columns, String tableName,
+                                 Class<?>[] dataTypes, String fieldSeperator) {
+        logger.debug("Entering method loadDataFromFile()");
 
-        BufferedReader bufferedReader = null;
-        try {
-            bufferedReader = new BufferedReader(new FileReader(fileName));
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("File not found :" + fileName, e, IO_0001);
-        }
+        BufferedReader reader = readFile(fileName);
         int columnCount = columns.split(",").length;
-        StringBuffer sql = new StringBuffer();
+        StringBuffer sql = new StringBuffer(400);
         sql.append("insert into ").append(tableName).append(" ").append(columns).append(" values(");
         for (int i = 0; i < columnCount; i++) {
             sql.append("?,");
         }
         sql.deleteCharAt(sql.length() - 1);
-        sql.append(")");
+        sql.append(')');
 
         PreparedStatement ps = null;
         try {
-            ps = connection.prepareStatement(sql.toString());
-            String oneRecord = "";
-            while ((oneRecord = bufferedReader.readLine()) != null) {
+            ps = con.prepareStatement(sql.toString());
+            String oneRecord = reader.readLine();
+            while (oneRecord != null) {
                 String[] values = oneRecord.split(fieldSeperator);
-                for (int j = 0; j < columnCount; j++) {
-                    if (dataTypes[j].equals(String.class)) {
-                        ps.setString(j + 1, values[j]);
-                    } else if (dataTypes[j].equals(Long.class)) {
-                        ps.setLong(j + 1, Long.parseLong(values[j]));
-                    } else if (dataTypes[j].equals(Integer.class)) {
-                        ps.setInt(j + 1, Integer.parseInt(values[j]));
-                    }
-                }
+                process(dataTypes, values, columnCount, ps);
                 ps.executeUpdate();
                 ps.clearParameters();
             }
-            bufferedReader.close();
-            if(!connection.getAutoCommit()) {
-                connection.commit();
+            reader.close();
+            if (!con.getAutoCommit()) {
+                con.commit();
             }
+            oneRecord = reader.readLine();
         } catch (SQLException e) {
-            Logger.out.error(e.getMessage());
-            throw new RuntimeException("Error while loading path information into database",e, ErrorCodeConstants.DB_0006);
+            logger.error(e.getMessage());
+            throw new RuntimeException("Error while loading path information into database", e,
+                    ErrorCodeConstants.DB_0006);
         } catch (IOException e) {
-            Logger.out.error(e.getMessage());
-            throw new RuntimeException("Error while loading path information into database",e, ErrorCodeConstants.DB_0006);
+            logger.error(e.getMessage());
+            throw new RuntimeException("Error while loading path information into database", e,
+                    ErrorCodeConstants.DB_0006);
         } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                    Logger.out.error(e);
-                }
+            close(ps);
+        }
+        logger.debug("Leaving method loadDataFromFile()");
+    }
+
+    private void process(Class<?>[] dataTypes, String[] values, int columnCount, PreparedStatement ps)
+            throws SQLException {
+        for (int j = 0; j < columnCount; j++) {
+            if (dataTypes[j].equals(String.class)) {
+                ps.setString(j + 1, values[j]);
+            } else if (dataTypes[j].equals(Long.class)) {
+                ps.setLong(j + 1, Long.parseLong(values[j]));
+            } else if (dataTypes[j].equals(Integer.class)) {
+                ps.setInt(j + 1, Integer.parseInt(values[j]));
             }
         }
-        Logger.out.debug("Leaving method loadDataFromFile()");
+    }
+
+    private BufferedReader readFile(String fileName) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("File not found :" + fileName, e, IO_0001);
+        }
+        return reader;
+    }
+
+    private void close(PreparedStatement ps) {
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (SQLException e) {
+                logger.error(e);
+            }
+        }
     }
 }
