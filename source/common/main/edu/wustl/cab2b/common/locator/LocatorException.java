0257d17172cad070c6188e2543624a530e5afc5e
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-09-15 10:40:06 +0000

    Removed unnecessary constructors

--- source/common/main/edu/wustl/cab2b/common/locator/LocatorException.java
+++ source/common/main/edu/wustl/cab2b/common/locator/LocatorException.java
@@ -1,43 +1,20 @@
 package edu.wustl.cab2b.common.locator;
 
-import edu.wustl.cab2b.common.exception.CheckedException;
 import edu.wustl.cab2b.common.exception.RuntimeException;
 
-
 /**
- * All exception occured in {@link edu.wustl.cab2b.common.locator.Locator} will be wrapped in this exception.
+ * All exception occurred in {@link edu.wustl.cab2b.common.locator.Locator} will be wrapped in this exception.
  * @author Chandrakant Talele
  */
-public class LocatorException extends RuntimeException{
-
-    /**
-     * Default Constructor
-     */
-    public LocatorException() {
-        super();
-    }
+public class LocatorException extends RuntimeException {
+    private static final long serialVersionUID = 8682893414728832937L;
 
     /**
      * @param message Message to set
      * @param cause Cause for exception
      */
     public LocatorException(String message, Throwable cause, String errorCode) {
-        super(message, (Exception)cause, errorCode);
-        cause.printStackTrace();
-    }
-
-    /**
-     * @param message Message to set
-     */
-    public LocatorException(String message) {
-        super(message);
-    }
-
-    /**
-     * @param cause Cause for exception
-     */
-    public LocatorException(Throwable cause) {
-        super((Exception)cause);
+        super(message, (Exception) cause, errorCode);
         cause.printStackTrace();
     }
 }
