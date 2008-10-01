afc3d960ac458383477c0c3437ceabeff3189885
atul_jawale <atul_jawale@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-10-01 08:42:15 +0000

    new class for handling all index service operations.

---
+++ source/server/main/edu/wustl/cab2b/server/serviceurl/IndexServiceOperations.java
@@ -0,0 +1,66 @@
+package edu.wustl.cab2b.server.serviceurl;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.axis.message.addressing.EndpointReferenceType;
+import org.apache.axis.types.URI.MalformedURIException;
+
+import edu.wustl.cab2b.common.util.PropertyLoader;
+import edu.wustl.common.util.logger.Logger;
+import gov.nih.nci.cagrid.discovery.client.DiscoveryClient;
+import gov.nih.nci.cagrid.metadata.exceptions.QueryInvalidException;
+import gov.nih.nci.cagrid.metadata.exceptions.RemoteResourcePropertyRetrievalException;
+import gov.nih.nci.cagrid.metadata.exceptions.ResourcePropertyRetrievalException;
+
+/**
+ * Handles all index service operations
+ * 
+ * @author hrishikesh_rajpathak
+ * 
+ */
+public class IndexServiceOperations {
+
+    /**
+     * @param names of entity group for which service instances are needed
+     * @return array of EPRs for given entity group
+     * @throws MalformedURIException in case of index service url not correct
+     * @throws RemoteException 
+     */
+    public EndpointReferenceType[] getServicesByNames(final String name) throws MalformedURIException,
+            RemoteException {
+        final List<EndpointReferenceType> list = new ArrayList<EndpointReferenceType>();
+        String[] urls = PropertyLoader.getIndexServiceUrls();
+        for (String url : urls) {
+            DiscoveryClient client = new DiscoveryClient(url);
+            EndpointReferenceType[] endpointReferenceType = null;
+            try {
+                endpointReferenceType = client.discoverDataServicesByDomainModel(name);
+            } catch (RemoteResourcePropertyRetrievalException e) {
+                Logger.out.error(e.getMessage(), e);
+                throw new RemoteException(e.getMessage());
+            } catch (QueryInvalidException e) {
+                Logger.out.error(e.getMessage(), e);
+                throw new RemoteException(e.getMessage());
+            } catch (ResourcePropertyRetrievalException e) {
+                Logger.out.error(e.getMessage(), e);
+                throw new RemoteException(e.getMessage());
+            }
+
+            if (endpointReferenceType == null) {
+                return new EndpointReferenceType[0];
+            }
+
+            //There have been times when we got the EPRs null. So this check
+            for (EndpointReferenceType referenceType : endpointReferenceType) {
+                if (referenceType != null) {
+                    list.add(referenceType);
+                }
+            }
+        }
+
+        return list.toArray(new EndpointReferenceType[0]);
+    }
+
+}
