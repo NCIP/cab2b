e279e1fe304f8600533ad637a6438a642f622329
gaurav_mehta <gaurav_mehta@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-10-07 07:21:09 +0000

    Changes for service instance url selection and default selection

--- source/client/main/edu/wustl/cab2b/client/ui/mysettings/ServiceInstancesPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/mysettings/ServiceInstancesPanel.java
@@ -21,6 +21,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Vector;
 
+import javax.swing.JOptionPane;
 import javax.swing.border.EmptyBorder;
 
 import org.jdesktop.swingx.JXTitledPanel;
@@ -41,10 +42,11 @@ import edu.wustl.cab2b.common.user.AdminServiceMetadata;
 import edu.wustl.cab2b.common.user.UserInterface;
 
 /**
- * @author atul_jawale
- * This class is the service instance panel where user is shown all the
- * running service instances of the service name passed as the parameter.
- * 
+ * This class is the service instance panel where user is shown all the running service 
+ * instances of the service name passed as the parameter.
+
+ * @author atul_jawale 
+ * @author gaurav_mehta 
  */
 public class ServiceInstancesPanel extends Cab2bPanel {
 
@@ -57,34 +59,43 @@ public class ServiceInstancesPanel extends Cab2bPanel {
 
     private Collection<AdminServiceMetadata> allServiceInstances;
 
+    private final JXTitledPanel titledSearchResultsPanel = new Cab2bTitledPanel();
+
+    private final Collection<AdminServiceMetadata> filteredServiceInstances = new ArrayList<AdminServiceMetadata>();
+
+    private String serviceName;
+
     /**
      * 
      * @param serviceName
      */
     public ServiceInstancesPanel(String serviceName) {
         super(new BorderLayout());
-        initGUI(serviceName);
+        this.serviceName = serviceName;
+        initGUI();
     }
 
     /**
      * This method initializes the panel
+     * 
      * @param serviceName
      */
-    private void initGUI(final String serviceName) {
-        Cab2bPanel resultPanel = null;
-        SearchPanel searchPanel = new SearchPanel();
-        Cab2bButton saveButton = new Cab2bButton("Save Settings");
-
+    private void initGUI() {
         allServiceInstances = new ArrayList<AdminServiceMetadata>();
-        final Collection<AdminServiceMetadata> filteredServiceInstances = new ArrayList<AdminServiceMetadata>();
-
-        ServiceInstanceBizLogic bizLogic = new ServiceInstanceBizLogic();
         UserInterface user = UserCache.getInstance().getCurrentUser();
-        allServiceInstances.addAll((bizLogic.getServiceMetadataObjects(serviceName, user)));
+        Collection<AdminServiceMetadata> adminServiceMetadata = new ServiceInstanceBizLogic().getServiceMetadataObjects(
+                                                                                                                        serviceName,
+                                                                                                                        user);
+        allServiceInstances.addAll((adminServiceMetadata));
+
         filteredServiceInstances.addAll(allServiceInstances);
 
         String title = serviceName + " Service Instances (" + filteredServiceInstances.size() + ")";
-        final JXTitledPanel titledSearchResultsPanel = new Cab2bTitledPanel(title);
+        titledSearchResultsPanel.setTitle(title);
+
+        Cab2bButton saveButton = initSaveButton();
+        SearchPanel searchPanel = new SearchPanel();
+        Cab2bPanel resultPanel = null;
         if (filteredServiceInstances.size() > 0) {
             resultPanel = generateContentPanel(filteredServiceInstances);
             GradientPaint gp = new GradientPaint(new Point2D.Double(.05d, 0), new Color(185, 211, 238),
@@ -103,36 +114,36 @@ public class ServiceInstancesPanel extends Cab2bPanel {
             saveButton.setEnabled(false);
             searchPanel.setEnable(false);
         }
-
         titledSearchResultsPanel.setContentContainer(resultPanel);
-        searchPanel.addPropertyChangeListener(SEARCH_EVENT, new PropertyChangeListener() {
-            public void propertyChange(PropertyChangeEvent event) {
-                String searchString = (String) event.getNewValue();
-                searchResult(searchString, filteredServiceInstances);
-                Cab2bPanel contentPanel = generateContentPanel(filteredServiceInstances);
-                titledSearchResultsPanel.setContentContainer(contentPanel);
-                String title = serviceName + " Service Instances (" + filteredServiceInstances.size() + ")";
-                titledSearchResultsPanel.setTitle(title);
-                revalidate();
-            }
-        });
+        searchPanel.addPropertyChangeListener(SEARCH_EVENT, new SearchPanelPropertyListener());
 
+        Cab2bButton backButton = initBackButton();
+        Cab2bButton adminSettings = initAdminSettingsButton();
+
+        Cab2bPanel bottomButtonContainer = new Cab2bPanel(new FlowLayout(10, 5, 2));
+        bottomButtonContainer.add(backButton);
+        bottomButtonContainer.add(saveButton);
+        bottomButtonContainer.add(adminSettings);
+
+        add(searchPanel, BorderLayout.NORTH);
+        add(titledSearchResultsPanel, BorderLayout.CENTER);
+        add(bottomButtonContainer, BorderLayout.SOUTH);
+    }
+
+    private Cab2bButton initSaveButton() {
+        Cab2bButton saveButton = new Cab2bButton("Save Settings");
         saveButton.setPreferredSize(new Dimension(120, 22));
         saveButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
                 List<AdminServiceMetadata> selectedInstances = resultsPage.getSelectedPageElementsUserObjects();
-                ServiceInstanceBizLogic bizLogic = new ServiceInstanceBizLogic();
-                try {
-                    bizLogic.saveServiceInstances(selectedInstances, UserCache.getInstance().getCurrentUser());
-                    firePropertyChange(UPDATE_EVENT, serviceName, "( Configured Successfully )");
-
-                } catch (RemoteException exception) {
-                    CommonUtils.handleException(exception, titledSearchResultsPanel, true, true, false, false);
-                    firePropertyChange(UPDATE_EVENT, serviceName, "(Unable to Configure )");
-                }
+                saveButtonFunction(selectedInstances);
             }
         });
 
+        return saveButton;
+    }
+
+    private Cab2bButton initBackButton() {
         Cab2bButton backButton = new Cab2bButton("Back");
         backButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
@@ -140,29 +151,64 @@ public class ServiceInstancesPanel extends Cab2bPanel {
             }
         });
 
-        Cab2bPanel bottomButtonContainer = new Cab2bPanel(new FlowLayout(10, 5, 2));
-        bottomButtonContainer.add(backButton);
-        bottomButtonContainer.add(saveButton);
+        return backButton;
+    }
 
-        add(searchPanel, BorderLayout.NORTH);
-        add(titledSearchResultsPanel, BorderLayout.CENTER);
-        add(bottomButtonContainer, BorderLayout.SOUTH);
+    private Cab2bButton initAdminSettingsButton() {
+        Cab2bButton adminSettings = new Cab2bButton("Admin Settings");
+        adminSettings.setPreferredSize(new Dimension(120, 22));
+        adminSettings.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                adminDefaultSettings();
+            }
+        });
+
+        return adminSettings;
+    }
+
+    private void saveButtonFunction(List<AdminServiceMetadata> selectedInstances) {
+        if (selectedInstances.size() == 0) {
+            JOptionPane.showMessageDialog(this.getParent(), "Please select atleast 1 option");
+        } else {
+            saveServiceInstances(selectedInstances);
+        }
+    }
+
+    private void saveServiceInstances(List<AdminServiceMetadata> selectedInstances) {
+        ServiceInstanceBizLogic bizLogic = new ServiceInstanceBizLogic();
+        try {
+            bizLogic.saveServiceInstances(selectedInstances, UserCache.getInstance().getCurrentUser());
+            firePropertyChange(UPDATE_EVENT, serviceName, "( Configured Successfully )");
+
+        } catch (RemoteException exception) {
+            CommonUtils.handleException(exception, titledSearchResultsPanel, true, true, false, false);
+            firePropertyChange(UPDATE_EVENT, serviceName, "(Unable to Configure )");
+        }
+    }
+
+    private void adminDefaultSettings() {
+        List<AdminServiceMetadata> emptyList = new ArrayList<AdminServiceMetadata>();
+        saveServiceInstances(emptyList);
+        firePropertyChange(UPDATE_EVENT, serviceName, "( Admin Settings restored )");
     }
 
     /**
-     * This method generates the Pagination component using the collection of the 
-     * service metadata.Mainly used to generate the pagination component at first time and 
-     * then every time when user search for the service instance.  
+     * This method generates the Pagination component using the collection of
+     * the service metadata.Mainly used to generate the pagination component at
+     * first time and then every time when user search for the service instance.
+     * 
      * @param resultPanel
      */
     private Cab2bPanel generateContentPanel(final Collection<AdminServiceMetadata> filteredServiceInstances) {
         Cab2bPanel resultPanel = new Cab2bPanel();
         Vector<PageElement> pageElementCollection = new Vector<PageElement>();
         for (AdminServiceMetadata serviceInstance : filteredServiceInstances) {
-            // Create an instance of the PageElement. Initialize with the appropriate data
+            // Create an instance of the PageElement. Initialize with the
+            // appropriate data
             PageElement pageElement = new PageElementImpl();
-            //not able view NCICB completely it was appearing as NCICE so added a space.
-            pageElement.setDisplayName(serviceInstance.getHostingResearchCenter()+" ");
+            // not able view NCICB completely it was appearing as NCICE so added
+            // a space.
+            pageElement.setDisplayName(serviceInstance.getHostingResearchCenter() + " ");
             pageElement.setDescription(serviceInstance.getSeviceDescription());
             pageElement.setUserObject(serviceInstance);
             pageElement.setSelected(serviceInstance.isConfigured());
@@ -181,8 +227,10 @@ public class ServiceInstancesPanel extends Cab2bPanel {
     }
 
     /**
-     * This method is the search method for service instance Panel the user given search string is 
-     * checked in every instance's hosting researchcenter's name and added to the result.   
+     * This method is the search method for service instance Panel the user
+     * given search string is checked in every instance's hosting
+     * researchcenter's name and added to the result.
+     * 
      * @param searchString
      * @return
      */
@@ -199,6 +247,18 @@ public class ServiceInstancesPanel extends Cab2bPanel {
                     filteredServiceInstances.add(metadata);
                 }
             }
+        }
+    }
+
+    class SearchPanelPropertyListener implements PropertyChangeListener {
+        public void propertyChange(PropertyChangeEvent event) {
+            String searchString = (String) event.getNewValue();
+            searchResult(searchString, filteredServiceInstances);
+            Cab2bPanel contentPanel = generateContentPanel(filteredServiceInstances);
+            titledSearchResultsPanel.setContentContainer(contentPanel);
+            String title = serviceName + " Service Instances (" + filteredServiceInstances.size() + ")";
+            titledSearchResultsPanel.setTitle(title);
+            revalidate();
         }
     }
 }
