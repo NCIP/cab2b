a3e8bdfb3236ac07c54d8b9d8e691753fdcbbd39
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-10-21 06:18:33 +0000

    Removed inserts for cab2bUser

--- scripts/caB2B_mysql.sql
+++ scripts/caB2B_mysql.sql
@@ -136,4 +136,4 @@ alter table CAB2B_USER_URL_MAPPING add index FKC64BBF4AB2004842 (SERVICE_URL_ID)
 
 insert into CAB2B_ID_TABLE(NEXT_ASSOCIATION_ID) value(1);
 insert into CAB2B_user (USER_ID, NAME, PASSWORD, IS_ADMIN) values (1,'Admin', 'admin123', 1); 
-insert into CAB2B_user (USER_ID, NAME, PASSWORD, IS_ADMIN) values (2,'cab2bUser', 'cab2b123', 0); 
+ 
