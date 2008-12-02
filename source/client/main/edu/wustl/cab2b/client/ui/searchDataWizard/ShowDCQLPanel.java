7c63170274e882e11149eb9295ddbb17b5ac06ed
gaurav_mehta <gaurav_mehta@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-12-02 10:17:41 +0000

    Fix for Bug # 10703

--- source/client/main/edu/wustl/cab2b/client/ui/searchDataWizard/ShowDCQLPanel.java
+++ source/client/main/edu/wustl/cab2b/client/ui/searchDataWizard/ShowDCQLPanel.java
@@ -15,6 +15,10 @@ import java.io.PrintWriter;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JScrollPane;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.xml.sax.XMLFilter;
 
 import edu.wustl.cab2b.client.ui.controls.Cab2bButton;
 import edu.wustl.cab2b.client.ui.controls.Cab2bLabel;
@@ -25,128 +29,149 @@ import edu.wustl.cab2b.client.ui.util.WindowUtilities;
 import edu.wustl.cab2b.common.domain.DCQL;
 
 /**
- * This class displays the DCQL Panel which shows the XML format of DCQL along with function of saving the XML file 
+ * This class displays the DCQL Panel which shows the XML format of DCQL along
+ * with function of saving the XML file
  * 
  * @author gaurav_mehta
  */
 public class ShowDCQLPanel extends Cab2bPanel {
 
-    /* JDialog in which the Panel is displayed */
-    private JDialog dialog;
+	/* JDialog in which the Panel is displayed */
+	private JDialog dialog;
 
-    // Cab2bLabel in which the entire XML is 
-    final private Cab2bLabel xmlTextPane = new Cab2bLabel();
+	// Cab2bLabel in which the entire XML is
+	final private Cab2bLabel xmlTextPane = new Cab2bLabel();
 
-    // Cab2bPanel for showing Success and failure messages
-    final Cab2bPanel messagePanel = new Cab2bPanel();
-    
-    private String dcqlString;
+	// Cab2bPanel for showing Success and failure messages
+	final Cab2bPanel messagePanel = new Cab2bPanel();
 
-    /**
-     * @param dcql 
-     */
-    public ShowDCQLPanel(DCQL dcql) {
-        this.dcqlString = dcql.getDcqlQuery();
-        initGUI();
-    }
+	private String dcqlString;
 
-    private void initGUI() {
-        String xmlText = new XmlParser().parseXml(dcqlString);
+	/**
+	 * @param dcql
+	 */
+	public ShowDCQLPanel(DCQL dcql) {
+		this.dcqlString = dcql.getDcqlQuery();
+		initGUI();
+	}
 
-        xmlTextPane.setText(xmlText);
-        xmlTextPane.setBackground(Color.WHITE);
+	private void initGUI() {
+		String xmlText = new XmlParser().parseXml(dcqlString);
 
-        Cab2bPanel xmlPanel = new Cab2bPanel();
-        xmlPanel.add(xmlTextPane);
-        xmlPanel.setBackground(Color.WHITE);
+		xmlTextPane.setText(xmlText);
+		xmlTextPane.setBackground(Color.WHITE);
 
-        JScrollPane scrollPane = new JScrollPane();
-        scrollPane.getViewport().add(xmlPanel);
-        scrollPane.getViewport().setBackground(Color.WHITE);
+		Cab2bPanel xmlPanel = new Cab2bPanel();
+		xmlPanel.add(xmlTextPane);
+		xmlPanel.setBackground(Color.WHITE);
 
-        Cab2bPanel xmlNavigationPanel = new Cab2bPanel();
-        Cab2bButton exportButton = new Cab2bButton("Export");
-        Cab2bButton cancelButton = new Cab2bButton("Cancel");
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.getViewport().add(xmlPanel);
+		scrollPane.getViewport().setBackground(Color.WHITE);
 
-        // Action Listener for Export Button
-        exportButton.addActionListener(new ExportButtonListner());
+		Cab2bPanel xmlNavigationPanel = new Cab2bPanel();
+		Cab2bButton exportButton = new Cab2bButton("Export");
+		Cab2bButton cancelButton = new Cab2bButton("Cancel");
 
-        // Action Listener for Cancel Button
-        cancelButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent actionEvent) {
-                dialog.dispose();
-            }
-        });
+		// Action Listener for Export Button
+		exportButton.addActionListener(new ExportButtonListner());
 
-        Cab2bPanel buttonPanel = new Cab2bPanel();
-        FlowLayout flowLayout = new FlowLayout(FlowLayout.RIGHT);
-        buttonPanel.setLayout(flowLayout);
-        buttonPanel.add(exportButton);
-        buttonPanel.add(cancelButton);
-        buttonPanel.setBackground(new Color(240, 240, 240));
+		// Action Listener for Cancel Button
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				dialog.dispose();
+			}
+		});
 
-        xmlNavigationPanel.add("br left", messagePanel);
-        xmlNavigationPanel.add("hfill", buttonPanel);
-        xmlNavigationPanel.setPreferredSize(new Dimension(880, 50));
-        xmlNavigationPanel.setBackground(new Color(240, 240, 240));
+		Cab2bPanel buttonPanel = new Cab2bPanel();
+		FlowLayout flowLayout = new FlowLayout(FlowLayout.RIGHT);
+		buttonPanel.setLayout(flowLayout);
+		buttonPanel.add(exportButton);
+		buttonPanel.add(cancelButton);
+		buttonPanel.setBackground(new Color(240, 240, 240));
 
-        setLayout(new BorderLayout());
-        add(scrollPane, BorderLayout.CENTER);
-        add(xmlNavigationPanel, BorderLayout.SOUTH);
-    }
+		xmlNavigationPanel.add("br left", messagePanel);
+		xmlNavigationPanel.add("hfill", buttonPanel);
+		xmlNavigationPanel.setPreferredSize(new Dimension(880, 50));
+		xmlNavigationPanel.setBackground(new Color(240, 240, 240));
 
-    /**
-     * JDialog for showing DCQL XML Details Panel
-     * 
-     * @return
-     */
-    public JDialog showInDialog() {
-        Dimension dimension = MainFrame.getScreenDimesion();
-        dialog = WindowUtilities.setInDialog(NewWelcomePanel.getMainFrame(), this, "DCQL Xml", new Dimension(
-                (int) (dimension.width * 0.77), (int) (dimension.height * 0.65)), true, false);
-        dialog.setVisible(true);
+		setLayout(new BorderLayout());
+		add(scrollPane, BorderLayout.CENTER);
+		add(xmlNavigationPanel, BorderLayout.SOUTH);
+	}
 
-        return dialog;
-    }
-    
-    private boolean writeFile(File file, String dataString) {
-        try {
-            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file)));
-            out.print(dataString);
-            out.flush();
-            out.close();
-        } catch (IOException e) {
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * JDialog for showing DCQL XML Details Panel
+	 * 
+	 * @return
+	 */
+	public JDialog showInDialog() {
+		Dimension dimension = MainFrame.getScreenDimesion();
+		dialog = WindowUtilities.setInDialog(NewWelcomePanel.getMainFrame(),
+				this, "DCQL Xml", new Dimension((int) (dimension.width * 0.77),
+						(int) (dimension.height * 0.65)), true, false);
+		dialog.setVisible(true);
 
-    /**
-     * Action listener class for Export Button. It saves the XML file to user defined location in user defined format
-     */
-    class ExportButtonListner implements ActionListener {
-    	
-        public void actionPerformed(ActionEvent actionEvent) {
-            JFileChooser fileChooser = new JFileChooser();
+		return dialog;
+	}
 
-            // A call to JFileChooser's ShowSaveDialog PopUp
-            fileChooser.showSaveDialog(NewWelcomePanel.getMainFrame());
+	private boolean writeFile(String file, String fileContent) {
+		try {
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new FileWriter(file)));
+			out.print(fileContent);
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
 
-            File file = fileChooser.getSelectedFile();
-            // Function call for writing the File and saving it
-            boolean saveReturnValue = writeFile(file, dcqlString);
-            if (saveReturnValue == true) {
-                Cab2bLabel successResultLabel = new Cab2bLabel("File Saved Successfully");
-                successResultLabel.setForeground(Color.GREEN);
-                messagePanel.add(successResultLabel);
-                messagePanel.repaint();
-            } else {
-                Cab2bLabel failureResultLabel = new Cab2bLabel("File Could not be Saved");
-                failureResultLabel.setForeground(Color.RED);
-                messagePanel.add(failureResultLabel);
-                messagePanel.repaint();
+	/**
+	 * Action listener class for Export Button. It saves the XML file to user
+	 * defined location in user defined format
+	 */
+	class ExportButtonListner implements ActionListener {
 
-            }
-        }
-    }
+		public void actionPerformed(ActionEvent actionEvent) {
+			JFileChooser fileChooser = new JFileChooser();
+
+			FileNameExtensionFilter filter = new FileNameExtensionFilter(
+					"Xml File Format", "xml");
+			FileNameExtensionFilter filter1 = new FileNameExtensionFilter(
+					"Txt File Format", "txt");
+			fileChooser.setAcceptAllFileFilterUsed(false);
+			fileChooser.setFileFilter(filter);
+			fileChooser.setFileFilter(filter1);
+
+			// A call to JFileChooser's ShowSaveDialog PopUp
+			fileChooser.showSaveDialog(NewWelcomePanel.getMainFrame());
+
+			File file = fileChooser.getSelectedFile();
+			String fileName = file.toString();
+			FileFilter fileFilter = fileChooser.getFileFilter();
+			if (fileFilter.getDescription().equalsIgnoreCase("Txt File Format")) {
+				fileName = fileName + ".txt";
+			} else {
+				fileName = fileName + ".xml";
+			}
+			// Function call for writing the File and saving it
+			boolean saveReturnValue = writeFile(fileName, dcqlString);
+			if (saveReturnValue == true) {
+				Cab2bLabel successResultLabel = new Cab2bLabel(
+						"File Saved Successfully");
+				successResultLabel.setForeground(Color.GREEN);
+				messagePanel.add(successResultLabel);
+				messagePanel.repaint();
+			} else {
+				Cab2bLabel failureResultLabel = new Cab2bLabel(
+						"File Could not be Saved");
+				failureResultLabel.setForeground(Color.RED);
+				messagePanel.add(failureResultLabel);
+				messagePanel.repaint();
+
+			}
+		}
+	}
 }
