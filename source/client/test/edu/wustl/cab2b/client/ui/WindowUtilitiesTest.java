ecde02a1d5b43fc63da1bcecf8e52f1e4395919d
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-12-04 09:08:05 +0000

    added testcases

--- source/client/test/edu/wustl/cab2b/client/ui/WindowUtilitiesTest.java
+++ source/client/test/edu/wustl/cab2b/client/ui/WindowUtilitiesTest.java
@@ -1,8 +1,13 @@
 package edu.wustl.cab2b.client.ui;
+
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Point;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 import junit.framework.TestCase;
@@ -12,39 +17,102 @@ import edu.wustl.cab2b.client.ui.util.WindowUtilities;
  * @author chandrakant_talele
  */
 public class WindowUtilitiesTest extends TestCase {
-    public void testSetNativeLookAndFeel() {
-        WindowUtilities.setNativeLookAndFeel();
-        String name = UIManager.getSystemLookAndFeelClassName();
-        assertEquals(name, UIManager.getLookAndFeel().getClass().getName());
-    }
+	public void testSetNativeLookAndFeel() {
+		WindowUtilities.setNativeLookAndFeel();
+		String name = UIManager.getSystemLookAndFeelClassName();
+		assertEquals(name, UIManager.getLookAndFeel().getClass().getName());
+	}
 
-    public void testSetJavaLookAndFeel() {
-        WindowUtilities.setJavaLookAndFeel();
-        String name = UIManager.getCrossPlatformLookAndFeelClassName();
-        assertEquals(name, UIManager.getLookAndFeel().getClass().getName());
-    }
+	public void testSetJavaLookAndFeel() {
+		WindowUtilities.setJavaLookAndFeel();
+		String name = UIManager.getCrossPlatformLookAndFeelClassName();
+		assertEquals(name, UIManager.getLookAndFeel().getClass().getName());
+	}
 
-    public void testSetMotifLookAndFeel() {
-        WindowUtilities.setMotifLookAndFeel();
-        String name = "com.sun.java.swing.plaf.motif.MotifLookAndFeel";
-        assertEquals(name, UIManager.getLookAndFeel().getClass().getName());
-    }
+	public void testSetMotifLookAndFeel() {
+		WindowUtilities.setMotifLookAndFeel();
+		String name = "com.sun.java.swing.plaf.motif.MotifLookAndFeel";
+		assertEquals(name, UIManager.getLookAndFeel().getClass().getName());
+	}
 
-    public void testOpenInJFrame() {
-        Container content = new Container();
-        JFrame frame = WindowUtilities.openInJFrame(content, 100, 100);
-        assertEquals(Color.white, frame.getBackground());
-        assertEquals(Color.white, content.getBackground());
-    }
+	public void testOpenInJFrame() {
+		Container content = new Container();
+		JFrame frame = WindowUtilities.openInJFrame(content, 100, 100);
+		assertEquals(Color.white, frame.getBackground());
+		assertEquals(Color.white, content.getBackground());
+	}
 
-    public void testOpenInJFrameWithTitle() {
-        String title = "title";
-        Container content = new Container();
-        int width = 100;
-        int height = 100;
-        JFrame frame = WindowUtilities.openInJFrame(content, width, height, title);
-        assertEquals(Color.white, frame.getBackground());
-        assertEquals(Color.white, content.getBackground());
-        assertEquals(title, frame.getTitle());
-    }
+	public void testOpenInJFrameWithTitle() {
+		String title = "title";
+		Container content = new Container();
+		int width = 100;
+		int height = 100;
+		JFrame frame = WindowUtilities.openInJFrame(content, width, height, title);
+		assertEquals(Color.white, frame.getBackground());
+		assertEquals(Color.white, content.getBackground());
+		assertEquals(title, frame.getTitle());
+	}
+
+	public void testCenterWindow() {
+		JFrame w = new JFrame("Test Frame");
+		WindowUtilities.centerWindow(w);
+		Dimension size = w.getToolkit().getScreenSize();
+		assertEquals(size.width / 2, w.getLocation().x);
+		assertEquals(size.height / 2, w.getLocation().y);
+	}
+
+	public void testCenterWindowPassingParent() {
+		JFrame parent = new JFrame("Parent Frame");
+		parent.setSize(640, 480);
+		parent.setLocation(100, 80);
+		parent.setVisible(true);
+		JFrame child = new JFrame("Child Frame");
+		child.setSize(200, 150);
+		WindowUtilities.centerWindow(parent, child);
+
+		assertEquals(320, child.getLocation().x);
+		assertEquals(245, child.getLocation().y);
+	}
+
+	public void testCenterWindowPassingParentChildOffScreen() {
+
+		JFrame parent = new JFrame("Parent Frame");
+		Dimension size = parent.getToolkit().getScreenSize();
+		parent.setSize(100, 100);
+		parent.setLocation(1, 1);
+		parent.setVisible(true);
+
+		JFrame child = new JFrame("Child Frame");
+		Dimension d = new Dimension(1000, 800);
+		child.setSize(d);
+		WindowUtilities.centerWindow(parent, child);
+
+		assertEquals((size.width - d.width) / 2, child.getLocation().x);
+		assertEquals((size.height - d.height) / 2, child.getLocation().y);
+	}
+
+	public void testCenterWindowParentOrphanComponent() {
+		Component component = new JFrame("Parent Frame");
+		JFrame window = new JFrame("Child Frame");
+		WindowUtilities.centerWindow(component, window);
+		Dimension size = component.getToolkit().getScreenSize();
+		assertEquals(size.width / 2, window.getLocation().x);
+		assertEquals(size.height / 2, window.getLocation().y);
+	}
+
+	public void testCenterWindowParentComponentWithParent() {
+		JFrame componentParent = new JFrame("Parent Frame");
+
+		JPanel componentChild = new JPanel();
+		componentParent.add(componentChild);
+		componentParent.setVisible(true);
+		JFrame window = new JFrame("Child Frame");
+		WindowUtilities.centerWindow(componentChild, window);
+
+		Dimension size = componentParent.getSize();
+		Point location = componentParent.getLocation();
+
+		assertEquals(size.width / 2 + location.x, window.getLocation().x);
+		assertEquals(size.height / 2 + location.y, window.getLocation().y);
+	}
 }
--- source/common/test/edu/wustl/cab2b/common/util/UtilityTest.java
+++ source/common/test/edu/wustl/cab2b/common/util/UtilityTest.java
@@ -1,5 +1,8 @@
 package edu.wustl.cab2b.common.util;
+
 import static edu.wustl.cab2b.common.util.Constants.CAB2B_ENTITY_GROUP;
+import static edu.wustl.cab2b.common.util.Constants.TYPE_CATEGORY;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,93 +17,123 @@ import edu.wustl.common.querysuite.metadata.associations.impl.IntraModelAssociat
 import edu.wustl.common.querysuite.metadata.path.Path;
 
 /**
- * @author deepak_shingan
+ * @author Chandrakant Talele 
  */
 public class UtilityTest extends TestCase {
 
-    /**
-     * Test method for {@link edu.wustl.cab2b.common.util.Utility#replaceAllWords(java.lang.String, java.lang.String, java.lang.String)}.
-     */
-    public final void testReplaceAllWords() {
-        //char replacement test 
-        assertEquals("Test", Utility.replaceAllWords("Teasta", "a", ""));
+	/**
+	 * Test method for {@link edu.wustl.cab2b.common.util.Utility#replaceAllWords(java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	public final void testReplaceAllWords() {
+		//char replacement test 
+		assertEquals("Test", Utility.replaceAllWords("Teasta", "a", ""));
 
-        //String replacement test 
-        assertEquals("What is this? this is this and is this Correct?",
-                     Utility.replaceAllWords("What is that? that is that and is that Correct?", "that", "this"));
-        //Null argument passing 
-        assertEquals(null, Utility.replaceAllWords(null, "that", "this"));
+		//String replacement test 
+		assertEquals("What is this? this is this and is this Correct?",
+					 Utility.replaceAllWords("What is that? that is that and is that Correct?", "that", "this"));
+		//Null argument passing 
+		assertEquals(null, Utility.replaceAllWords(null, "that", "this"));
 
-        //No replacement found test  
-        assertEquals("No replacement test", Utility.replaceAllWords("No replacement test", "Deepak", "Shingan"));
-    }
+		//No replacement found test  
+		assertEquals("No replacement test", Utility.replaceAllWords("No replacement test", "Deepak", "Shingan"));
+	}
 
-    public void testCapitalizeString() {
-        assertEquals("Gene", Utility.capitalizeFirstCharacter("gene"));
-    }
+	public void testCapitalizeString() {
+		assertEquals("Gene", Utility.capitalizeFirstCharacter("gene"));
+	}
 
-    public void testCapitalizeStringAlreadyCapital() {
-        assertEquals("Gene", Utility.capitalizeFirstCharacter("Gene"));
-    }
-    public void testHasAnySecureServiceforHttp() {
-        List<String> list = new ArrayList<String>(2);
-        list.add(" http://foo.bar/grid/service");
-        Cab2bQuery query = new Cab2bQuery();
-        query.setOutputUrls(list);
-        assertFalse(Utility.hasAnySecureService(query));
-    }
-    public void testHasAnySecureServiceforHttps() {
-        List<String> list = new ArrayList<String>(2);
-        list.add(" https://foo.bar/grid/service");
-        Cab2bQuery query = new Cab2bQuery();
-        query.setOutputUrls(list);
-        assertTrue(Utility.hasAnySecureService(query));
-    }
-    public void testGetPathDisplayStringJustAbove100() {
+	public void testCapitalizeStringAlreadyCapital() {
+		assertEquals("Gene", Utility.capitalizeFirstCharacter("Gene"));
+	}
 
-        EntityGroupInterface eg = TestUtil.getEntityGroup("caNanoLab v1.4", 120L);
-        DynamicExtensionUtility.addTaggedValue(eg, CAB2B_ENTITY_GROUP, CAB2B_ENTITY_GROUP);
-        EntityInterface nanoparticleSample = TestUtil.getEntity("NanoparticleSample", 134L);
-        nanoparticleSample.addEntityGroupInterface(eg);
-        EntityInterface sampleComposition = TestUtil.getEntity("SampleComposition", 133L);
-        sampleComposition.addEntityGroupInterface(eg);
-        AssociationInterface a = TestUtil.getAssociation(nanoparticleSample, sampleComposition);
+	public void testHasAnySecureServiceforHttp() {
+		List<String> list = new ArrayList<String>(2);
+		list.add(" http://foo.bar/grid/service");
+		Cab2bQuery query = new Cab2bQuery();
+		query.setOutputUrls(list);
+		assertFalse(Utility.hasAnySecureService(query));
+	}
 
-        Path path = TestUtil.getPath(new IntraModelAssociation(a));
-        String str = Utility.getPathDisplayString(path);
-        String expected = "<HTML><B>Path</B>:NanoparticleSample (caNanoLab v1.4)<B>----></B>SampleComposition (caNanoLab v1.4)</HTML>";
-        assertEquals(expected, str);
-    }
+	public void testHasAnySecureServiceforHttps() {
+		List<String> list = new ArrayList<String>(2);
+		list.add(" https://foo.bar/grid/service");
+		Cab2bQuery query = new Cab2bQuery();
+		query.setOutputUrls(list);
+		assertTrue(Utility.hasAnySecureService(query));
+	}
 
-    public void testGetPathDisplayStringLessThan100() {
+	public void testGetPathDisplayStringJustAbove100() {
 
-        EntityGroupInterface eg = TestUtil.getEntityGroup("GeneConnect", 120L);
-        DynamicExtensionUtility.addTaggedValue(eg, CAB2B_ENTITY_GROUP, CAB2B_ENTITY_GROUP);
-        EntityInterface nanoparticleSample = TestUtil.getEntity("Gene", 134L);
-        nanoparticleSample.addEntityGroupInterface(eg);
-        EntityInterface sampleComposition = TestUtil.getEntity("Protein", 133L);
-        sampleComposition.addEntityGroupInterface(eg);
-        AssociationInterface a = TestUtil.getAssociation(nanoparticleSample, sampleComposition);
+		EntityGroupInterface eg = TestUtil.getEntityGroup("caNanoLab v1.4", 120L);
+		DynamicExtensionUtility.addTaggedValue(eg, CAB2B_ENTITY_GROUP, CAB2B_ENTITY_GROUP);
+		EntityInterface nanoparticleSample = TestUtil.getEntity("NanoparticleSample", 134L);
+		nanoparticleSample.addEntityGroupInterface(eg);
+		EntityInterface sampleComposition = TestUtil.getEntity("SampleComposition", 133L);
+		sampleComposition.addEntityGroupInterface(eg);
+		AssociationInterface a = TestUtil.getAssociation(nanoparticleSample, sampleComposition);
 
-        Path path = TestUtil.getPath(new IntraModelAssociation(a));
-        String str = Utility.getPathDisplayString(path);
-        String expected = "<HTML><B>Path</B>:Gene (GeneConnect)<B>----></B>Protein (GeneConnect)</HTML>";
-        assertEquals(expected, str);
-    }
+		Path path = TestUtil.getPath(new IntraModelAssociation(a));
+		String str = Utility.getPathDisplayString(path);
+		String expected = "<HTML><B>Path</B>:NanoparticleSample (caNanoLab v1.4)<B>----></B>SampleComposition (caNanoLab v1.4)</HTML>";
+		assertEquals(expected, str);
+	}
 
-    public void testGetPathDisplayStringGreather100() {
+	public void testGetPathDisplayStringLessThan100() {
 
-        EntityGroupInterface eg = TestUtil.getEntityGroup("caTissue_Core_1_2 v1.2", 120L);
-        DynamicExtensionUtility.addTaggedValue(eg, CAB2B_ENTITY_GROUP, CAB2B_ENTITY_GROUP);
-        EntityInterface nanoparticleSample = TestUtil.getEntity("Participant", 134L);
-        nanoparticleSample.addEntityGroupInterface(eg);
-        EntityInterface sampleComposition = TestUtil.getEntity("ParticipantMedicalIdentifier", 133L);
-        sampleComposition.addEntityGroupInterface(eg);
-        AssociationInterface a = TestUtil.getAssociation(nanoparticleSample, sampleComposition);
+		EntityGroupInterface eg = TestUtil.getEntityGroup("GeneConnect", 120L);
+		DynamicExtensionUtility.addTaggedValue(eg, CAB2B_ENTITY_GROUP, CAB2B_ENTITY_GROUP);
+		EntityInterface nanoparticleSample = TestUtil.getEntity("Gene", 134L);
+		nanoparticleSample.addEntityGroupInterface(eg);
+		EntityInterface sampleComposition = TestUtil.getEntity("Protein", 133L);
+		sampleComposition.addEntityGroupInterface(eg);
+		AssociationInterface a = TestUtil.getAssociation(nanoparticleSample, sampleComposition);
 
-        Path path = TestUtil.getPath(new IntraModelAssociation(a));
-        String str = Utility.getPathDisplayString(path);
-        String expected = "<HTML><B>Path</B>:Participant (caTissue_Core_1_2 v1.2)<B>----></B>ParticipantMedicalIdentifier (caTissue_Core_1_2 v1.2)<P></HTML>";
-        assertEquals(expected, str);
-    }
+		Path path = TestUtil.getPath(new IntraModelAssociation(a));
+		String str = Utility.getPathDisplayString(path);
+		String expected = "<HTML><B>Path</B>:Gene (GeneConnect)<B>----></B>Protein (GeneConnect)</HTML>";
+		assertEquals(expected, str);
+	}
+
+	public void testGetPathDisplayStringGreather100() {
+
+		EntityGroupInterface eg = TestUtil.getEntityGroup("caTissue_Core_1_2 v1.2", 120L);
+		DynamicExtensionUtility.addTaggedValue(eg, CAB2B_ENTITY_GROUP, CAB2B_ENTITY_GROUP);
+		EntityInterface nanoparticleSample = TestUtil.getEntity("Participant", 134L);
+		nanoparticleSample.addEntityGroupInterface(eg);
+		EntityInterface sampleComposition = TestUtil.getEntity("ParticipantMedicalIdentifier", 133L);
+		sampleComposition.addEntityGroupInterface(eg);
+		AssociationInterface a = TestUtil.getAssociation(nanoparticleSample, sampleComposition);
+
+		Path path = TestUtil.getPath(new IntraModelAssociation(a));
+		String str = Utility.getPathDisplayString(path);
+		String expected = "<HTML><B>Path</B>:Participant (caTissue_Core_1_2 v1.2)<B>----></B>ParticipantMedicalIdentifier (caTissue_Core_1_2 v1.2)<P></HTML>";
+		assertEquals(expected, str);
+	}
+
+	public void testGetDisplayName() {
+		String name = "entityName";
+		EntityInterface entity = TestUtil.getEntityWithCaB2BGrp("groupName", name, "attrName");
+		DynamicExtensionUtility.addTaggedValue(entity, TYPE_CATEGORY, TYPE_CATEGORY);
+		String res = Utility.getDisplayName(entity);
+		assertEquals(name, res);
+	}
+
+	public void testGetDisplayNameFE() {
+		String name = "Gene";
+		EntityInterface entity = TestUtil.getEntityWithCaB2BGrp("caFE Server 1.1", name, "attrName");
+
+		entity.getEntityGroupCollection().iterator().next().setLongName("caFE Server 1.1");
+		entity.getEntityGroupCollection().iterator().next().setVersion("1.1");
+		String res = Utility.getDisplayName(entity);
+
+		assertEquals("Gene (caFE Server v1.1)", res);
+	}
+
+	public void testGetOnlyEntityName() {
+		String name = "edu.wustl.geneconnect.domain.Gene";
+		EntityInterface entity = TestUtil.getEntityWithCaB2BGrp("groupName", name, "attrName");
+		DynamicExtensionUtility.addTaggedValue(entity, TYPE_CATEGORY, TYPE_CATEGORY);
+		String res = Utility.getOnlyEntityName(entity);
+		assertEquals("Gene", res);
+	}
 }
