b10e8396be47276849945c94e6222990bc6e5708
chandrakant_talele <chandrakant_talele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2008-12-29 12:42:37 +0000

    Added javadoc. Commented unused constants

--- source/common/main/edu/wustl/cab2b/common/util/Constants.java
+++ source/common/main/edu/wustl/cab2b/common/util/Constants.java
@@ -1,44 +1,48 @@
-/**
- * <p>Title: Constants class>
- * <p>Description:	Constants class for the server.</p>
- * Copyright:    Copyright (c) year
- * Company: Washington University, School of Medicine, St. Louis.
- * @author Gautam Shetty
- * @version 1.00
- */
 package edu.wustl.cab2b.common.util;
 
 import java.awt.Dimension;
 
 /**
- * Constants class for the server.
+ * Constants class for the common
  * @author gautam_shetty
  */
 public interface Constants {
+    
+    /** Metadata search configuration parameter : Used when a class name is to be included in search*/
     public static final int CLASS = 1;
-
+    
+    /** Metadata search configuration parameter : Used when a attribute name is to be included in search*/
     public static final int ATTRIBUTE = 2;
-
+    
+    /** Metadata search configuration parameter : Used when a class's description is to be included in search*/
     public static final int CLASS_WITH_DESCRIPTION = 4;
-
+    
+    /** Metadata search configuration parameter : Used when a attribute's description is to be included in search*/
     public static final int ATTRIBUTE_WITH_DESCRIPTION = 5;
-
+    
+    /** Metadata search configuration parameter : Used when a permissible value is to be included in search*/
     public static final int PV = 3;
-
+    
+    /** Metadata search configuration parameter : Used when a text based search is desired*/
     public static final int BASED_ON_TEXT = 0;
-
+    
+    /** Metadata search configuration parameter : Used when a code based search is desired*/
     public static final int BASED_ON_CONCEPT_CODE = 1;
 
-    public static final int INLUDE_CLASSES_CLASS = 6;
+    //public static final int INLUDE_CLASSES_CLASS = 6;
 
-    public static final int INLUDE_CLASSES_ATTRIBUTE = 7;
+    //public static final int INLUDE_CLASSES_ATTRIBUTE = 7;
 
     public static final String OLD_ENTITY_ID_TAG_NAME = "original_entity_id";
-
+    
+    /** Tagged value key to store the name of original entity*/
     public static final String ENTITY_DISPLAY_NAME = "original_entity_display_name";
-
+    
+    /**Tagged value to indicate a data category is a filtered data category*/
     public static final String FILTERED = "filtered";
 
+    /** Tagged value for non-search-able entities or attributes*/
+    public static final String TAGGED_VALUE_NOT_SEARCHABLE = "NOT_SEARCHABLE";
     /**
      * Constant which represents String that will be used as URL KEY in tagged value
      */
@@ -81,7 +85,7 @@ public interface Constants {
 
     public static final String SELECT = "-- Select --";
     
-    public static final String SELECT_COLUMN = "-- Select Column --";
+    //public static final String SELECT_COLUMN = "-- Select Column --";
 
     public static final String SELECTED_PATH = "Selected Path";
 
