1867508667c603167dc26e28580c7f9ecf1fab8b
chetan_patil <chetan_patil@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-01-14 10:15:02 +0000

    Used commons-io-1.4.jar for code cleaning. Used copyFile method.

--- source/server/main/edu/wustl/cab2b/server/util/TimerUtill.java
+++ source/server/main/edu/wustl/cab2b/server/util/TimerUtill.java
@@ -1,13 +1,11 @@
 package edu.wustl.cab2b.server.util;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import org.apache.commons.io.FileUtils;
 
 import edu.wustl.cab2b.common.errorcodes.ErrorCodeConstants;
 import edu.wustl.cab2b.common.exception.RuntimeException;
@@ -51,70 +49,40 @@ public class TimerUtill {
      * This method sync the globus credential of server
      */
     private static void syncGlobusCredential() {
-        File commonDir = new File(System.getProperty("user.home") + File.separator + "commonDirCert");
-        commonDir.mkdir();
-
         try {
             Utility.generateGlobusCertificate("Production");
-        } catch (RuntimeException re) {
-            logger.error("Cannot Create Production Grid Certificate", re);
+        } catch (RuntimeException e) {
+            logger.error("Cannot Create Production Grid Certificate", e);
         }
-        copyToCommonPlace(commonDir);
 
+        File certDir = gov.nih.nci.cagrid.common.Utils.getTrustedCerificatesDirectory();
+        File tempDir = new File(System.getProperty("user.home") + File.separator + "temp_dir");
         try {
-            Utility.generateGlobusCertificate("Training");
-        } catch (RuntimeException re) {
-            logger.error("Cannot Create Training Grid Certificate", re);
-        }
-
-        File dir = gov.nih.nci.cagrid.common.Utils.getTrustedCerificatesDirectory();
-        if (commonDir != null && commonDir.isDirectory()) {
-            for (File files : commonDir.listFiles()) {
-                copyFiles(files, dir);
-            }
-        }
-    }
-
-    private static void copyToCommonPlace(File commonDir) {
-        File dir = gov.nih.nci.cagrid.common.Utils.getTrustedCerificatesDirectory();
-        if (dir != null && dir.isDirectory()) {
-            for (File file : dir.listFiles()) {
-                copyFiles(file, commonDir);
-            }
-        }
-    }
-
-    private static void copyFiles(File file, File commonDir) {
-        InputStream in = null;
-        try {
-            in = new FileInputStream(file);
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-            throw new RuntimeException(file.getPath() + " file not found", ErrorCodeConstants.CDS_016);
-        }
-
-        File dest = new File(commonDir + File.separator + file.getName());
-        OutputStream out = null;
-        try {
-            out = new FileOutputStream(dest);
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-            throw new RuntimeException(dest.getPath() + " file not found", ErrorCodeConstants.CDS_016);
-        }
-
-        byte[] buf = new byte[1024];
-        int len = 0;
-        try {
-            while ((len = in.read(buf)) > 0) {
-                out.write(buf, 0, len);
-            }
-
-            in.close();
-            out.close();
+            FileUtils.copyDirectory(certDir, tempDir);
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
             throw new RuntimeException("Unable to copy CA certificates to [user.home]/.globus",
                     ErrorCodeConstants.CDS_003);
+        }
+
+        try {
+            Utility.generateGlobusCertificate("Training");
+        } catch (RuntimeException e) {
+            logger.error("Cannot Create Training Grid Certificate", e);
+        }
+
+        try {
+            FileUtils.copyDirectory(tempDir, certDir);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+            throw new RuntimeException("Unable to copy CA certificates to [user.home]/.globus",
+                    ErrorCodeConstants.CDS_003);
+        }
+        
+        try {
+            FileUtils.deleteDirectory(tempDir);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
         }
     }
 
