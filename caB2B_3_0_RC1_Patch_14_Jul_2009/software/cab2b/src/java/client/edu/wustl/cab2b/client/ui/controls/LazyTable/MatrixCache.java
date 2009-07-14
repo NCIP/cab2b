6595a093353c33370e8a39176c139b5bbad076ee
chetan_patil <chetan_patil@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-07-14 10:15:01 +0000

    Changes in fixing few app scan report and null value insertion in data field.

---
+++ caB2B_3_0_RC1_Patch_14_Jul_2009/software/cab2b/src/java/client/edu/wustl/cab2b/client/ui/controls/LazyTable/MatrixCache.java
@@ -0,0 +1,88 @@
+package edu.wustl.cab2b.client.ui.controls.LazyTable;
+
+import org.apache.log4j.Logger;
+
+import edu.wustl.cab2b.client.cache.UserCache;
+
+/**
+ * This cache implementation is extension to BucketCache with 2 dimensional bucket.
+ * 
+ * @author rahul_ner
+ * @param <D>
+ */
+public class MatrixCache<D> implements CacheInterface<D> {
+
+    private static final Logger logger = edu.wustl.common.util.logger.Logger.getLogger(UserCache.class);
+    
+    private Page<D>[][] pageCache;
+
+    private int cacheSize;
+
+    /**
+     * @param cacheSize
+     */
+    public MatrixCache(int cacheSize) {
+        this.cacheSize = cacheSize;
+        pageCache = new Page[cacheSize][cacheSize];
+    }
+
+    /**
+     * 
+     */
+    public MatrixCache() {
+        this(3);
+    }
+
+    /**
+     * Checks whether a page is cached or not
+     * @param pageInfo
+     * @return boolean value
+     * @see edu.wustl.cab2b.client.CacheInterface#isCached(edu.wustl.cab2b.client.PageInfo)
+     */
+    public boolean isCached(PageInfo pageInfo) {
+        if (get(pageInfo) == null) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Puts a page in cache
+     * @param page
+     * @see edu.wustl.cab2b.client.CacheInterface#put(edu.wustl.cab2b.client.Page)
+     */
+    public void put(Page page) {
+        int cacheXLocation = getCacheXLocation(page.getPageInfo());
+        int cacheYLocation = getCacheYLocation(page.getPageInfo());
+        pageCache[cacheXLocation][cacheYLocation] = page;
+        logger.debug("Page Cached: " + page.getPageInfo());
+
+    }
+
+    /**
+     * @param pageInfo
+     * @return Page information whether cached or not
+     * @see edu.wustl.cab2b.client.CacheInterface#get(edu.wustl.cab2b.client.PageInfo)
+     */
+    public Page get(PageInfo pageInfo) {
+        int cacheXLocation = getCacheXLocation(pageInfo);
+        int cacheYLocation = getCacheYLocation(pageInfo);
+        Page cachedPage = pageCache[cacheXLocation][cacheYLocation];
+        if (cachedPage != null && pageInfo.equals(cachedPage.getPageInfo())) {
+            logger.debug("Cache Hit: " + pageInfo);
+            return cachedPage;
+        } else {
+            logger.debug("**Cache Miss: " + pageInfo);
+            return null;
+        }
+    }
+
+    private int getCacheXLocation(PageInfo pageInfo) {
+        return pageInfo.getStartX() % cacheSize;
+    }
+
+    private int getCacheYLocation(PageInfo pageInfo) {
+        return pageInfo.getStartY() % cacheSize;
+    }
+
+}
