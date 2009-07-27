83f7616bd8cbdc94cc5a8919731bf8e80243ff88
gaurav_mehta <gaurav_mehta@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-07-27 14:58:54 +0000

    AppScan fixes

--- dev_11Jun2009/software/cab2bAdmin/src/java/edu/wustl/cab2b/admin/preactionlogic/AuthenticationProcessor.java
+++ dev_11Jun2009/software/cab2bAdmin/src/java/edu/wustl/cab2b/admin/preactionlogic/AuthenticationProcessor.java
@@ -1,6 +1,7 @@
 package edu.wustl.cab2b.admin.preactionlogic;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,34 +11,33 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import edu.wustl.cab2b.admin.util.AdminConstants;
 import edu.wustl.cab2b.common.user.User;
 import edu.wustl.cab2b.common.user.UserInterface;
 import edu.wustl.cab2b.server.user.UserOperations;
+import edu.wustl.cab2b.server.util.XSSVulnerableDetector;
 
 /**
- * This is the AuthenticationProcessor
+ * This filter class is used to refine all requests before actual processing.
  * @author atul_jawale
  */
 public class AuthenticationProcessor implements Filter {
     private static final long serialVersionUID = -5992264946335132057L;
+
+    FilterConfig fc = null;
 
     /**
      * This method is called only once at startup and we are setting the property file name
      * @param filterConfig
      * @throws ServletException
      */
-    FilterConfig fc = null;
-
     public void init(FilterConfig filterConfig) throws ServletException {
         fc = filterConfig;
     }
 
     /**
-     * This method is called for every request to the server and used to check for a 
-     * valid session also saves user URL in request which is used by the pagination logic
+     * This method is called for every request to the server and is used to refine the requests before actual processing.
      * @param req
      * @param res
      * @param filterChain
@@ -46,40 +46,62 @@ public class AuthenticationProcessor implements Filter {
      */
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException,
             ServletException {
+
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
         response.addHeader("Cache-Control", fc.getInitParameter("Cache-Control"));
         request.setAttribute("URL", request.getRequestURL().toString());
-        HttpSession session = request.getSession(true);
 
-        if (session.getAttribute(AdminConstants.USER_OBJECT) == null
-                || (request.getMethod().indexOf("POST") != -1 && request.getParameter("userName") != null)) {
-            String userName = request.getParameter("userName");
-            String password = request.getParameter("password");
-            if (userName == null) {
-                request.setAttribute("error", "Session expired please login again.");
-                request.getRequestDispatcher("/jsp/default.jsp").forward(request, response);
-                return;
-            }
-            String result = AdminConstants.FAILURE;
-            UserInterface user = new UserOperations().getUserByName(userName);
-            if (user != null && ((User) user).getPassword().compareTo(password) == 0) {
-                session.setAttribute(AdminConstants.USER_OBJECT, user);
-                result = AdminConstants.SUCCESS;
-
-            }
-            if (AdminConstants.FAILURE.compareTo(result) == 0) {
-                session.invalidate();
-                request.setAttribute("error", "Invalid username or password.");
-                request.getRequestDispatcher("/jsp/default.jsp").forward(request, response);
-                return;
+        if (!(request.getMethod().trim().equalsIgnoreCase("GET") || request.getMethod().trim()
+            .equalsIgnoreCase("POST"))) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            //To make check on cross site scripting (XSS).
+            //Check each parameter in the request for XssVulnerability using RegEx=[<>] (useful for POST requests).
+            //If any parameter is found to be XssVulnerable, then redirect to home page.
+            for (Enumeration e = request.getParameterNames(); e.hasMoreElements();) {
+                String parameterVal = request.getParameter((String) e.nextElement());
+                if (XSSVulnerableDetector.isXssVulnerable(parameterVal)) {
+                    request.setAttribute(AdminConstants.INVALID_REQUEST, AdminConstants.INVALID_REQUEST);
+                    request.getRequestDispatcher("/pages/default.jsp").forward(req, res);
+                    return;
+                }
             }
         }
-        filterChain.doFilter(request, response);
+
+        if (request.getSession().getAttribute(AdminConstants.USER_OBJECT) == null
+                || request.getParameter("userName") != null) {
+
+            String userName = request.getParameter("userName");
+            String password = request.getParameter("password");
+
+            if (request.getParameter("userName") == null) {
+                request.setAttribute("error", "Session expired! Please login again.");
+                request.getRequestDispatcher("/jsp/default.jsp").forward(request, response);
+            } else {
+                String forward = AdminConstants.FAILURE;
+                if (!(XSSVulnerableDetector.isXssSQLVulnerable(userName) || XSSVulnerableDetector
+                    .isXssSQLVulnerable(password))) {
+                    UserInterface user = new UserOperations().getUserByName(userName);
+                    if (user != null && ((User) user).getPassword().compareTo(password) == 0) {
+                        request.getSession().setAttribute(AdminConstants.USER_OBJECT, user);
+                        forward = AdminConstants.SUCCESS;
+                        filterChain.doFilter(request, response);
+                    }
+                }
+                if (AdminConstants.FAILURE.equals(forward)) {
+                    request.getSession().invalidate();
+                    request.setAttribute("error", "Invalid username or password.");
+                    request.getRequestDispatcher("/jsp/default.jsp").forward(request, response);
+                }
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
     }
 
     /**
-     * Destroys session
+     * This method is called only once when the filter class is unloaded.
      */
     public void destroy() {
     }
