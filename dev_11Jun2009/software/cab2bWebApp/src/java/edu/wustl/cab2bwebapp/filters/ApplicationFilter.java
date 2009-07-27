403aaa3736b8add831be2a8b9f9fee9a4cfef4bb
chetan_pundhir <chetan_pundhir@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-07-27 14:41:38 +0000

    AppScan Fixes.

--- dev_11Jun2009/software/cab2bWebApp/src/java/edu/wustl/cab2bwebapp/filters/ApplicationFilter.java
+++ dev_11Jun2009/software/cab2bWebApp/src/java/edu/wustl/cab2bwebapp/filters/ApplicationFilter.java
@@ -13,25 +13,21 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import edu.wustl.cab2bwebapp.constants.Constants;
+
 import edu.wustl.cab2b.server.util.XSSVulnerableDetector;
+import edu.wustl.cab2bwebapp.constants.Constants;
 
 /**
- * This filter class is used for disabling page caching and checking proper authentications.
+ * This filter class is used to refine all requests before actual processing.
  * @author chetan_pundhir
  */
 
 public class ApplicationFilter implements Filter {
 
-    /**
-     * This method is called only once at startup.
-     * @param filterConfig
-     * @throws ServletException
-     */
-
     FilterConfig fc = null;
 
     /**
+     * This method is called only once at startup.
      * @param FilterConfig
      * @throws ServletException
      */
@@ -40,7 +36,7 @@ public class ApplicationFilter implements Filter {
     }
 
     /**
-     * This method is called for every request to the server and is used to disable caching.
+     * This method is called for every request to the server and is used to refine the requests before actual processing.
      * @param req
      * @param res
      * @param filterChain
@@ -56,32 +52,37 @@ public class ApplicationFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.addHeader("Cache-Control", fc.getInitParameter("Cache-Control"));
 
-        //To prevent cross site scripting (XSS)
-        boolean isXssVulnerable = false;
-        
-        //1)Check each parameter in the request for XssVulnerability using RegEx= [<>] (useful for POST requests).
-        //As soon as a parameter is found to be XssVulnerable, break the loop and redirect to homepage 
-        for (Enumeration e = request.getParameterNames(); e.hasMoreElements();) {
-            String parameterVal = request.getParameter((String) e.nextElement());
-            if (XSSVulnerableDetector.isXssVulnerable(parameterVal)) {
-                isXssVulnerable = true;
-                break;
+        if (!(request.getMethod().trim().equalsIgnoreCase("GET") || request.getMethod().trim()
+            .equalsIgnoreCase("POST"))) {
+            //Disable all HTTP methods except GET and POST.           
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            //To make check on cross site scripting (XSS).
+            boolean isXssVulnerable = false;
+
+            //There should not be any parameter appended in the URL (for Non AJAX GET requests).
+            //If some parameters are appended in the URL and the call is not AJAX based, then redirect to home page.
+            isXssVulnerable = request.getQueryString() != null && request.getHeader(Constants.AJAX_CALL) == null;
+
+            //Check each parameter in the request for XssVulnerability using RegEx=[<>] (useful for POST requests).
+            //If any parameter is found to be XssVulnerable, then redirect to home page.
+            if (!isXssVulnerable) {
+                for (Enumeration e = request.getParameterNames(); e.hasMoreElements();) {
+                    String parameterVal = request.getParameter((String) e.nextElement());
+                    if (XSSVulnerableDetector.isXssVulnerable(parameterVal)) {
+                        isXssVulnerable = true;
+                        break;
+                    }
+                }
+            }
+            if (isXssVulnerable) { //If request is XssVulnerable, redirect to home page.
+                request.setAttribute(Constants.INVALID_REQUEST, Constants.INVALID_REQUEST);
+                request.getRequestDispatcher("/pages/home.jsp").forward(req, res);
+                return;
             }
         }
-        //2)Even if the parameters are non-XssVulnerable, there should not be any parameter appended in the URL (useful for GET requests)
-        //If no parameter is found XssVulnerable & its not a ajax call & it has some parameters in the URL, then redirect to homepage 
-        if ((isXssVulnerable == false) && (request.getHeader(Constants.AJAX_CALL) == null)
-                && (request.getQueryString() != null)) {
-            isXssVulnerable = true;
-        }
-        
-        //If request is XssVulnerable, redirect to homepage (with an alert message in the home.jsp)
-        if (isXssVulnerable) {
-            request.setAttribute(Constants.INVALID_REQUEST, Constants.INVALID_REQUEST);
-            request.getRequestDispatcher("/pages/home.jsp").forward(req, res);
-        }
 
-        else if (request.getSession().isNew()) {
+        if (request.getSession().isNew()) {
             if (request.getHeader(Constants.AJAX_CALL) != null) {
                 response.setStatus(HttpServletResponse.SC_FORBIDDEN);
                 PrintWriter pw = response.getWriter();
@@ -109,7 +110,7 @@ public class ApplicationFilter implements Filter {
     }
 
     /**
-     * Destroys session
+     * This method is called only once when the filter class is unloaded.
      */
     public void destroy() {
     }
