5bcb40679844cb6081117d8899cb4dce8c4614ba
PundhirC <PundhirC@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-11 11:55:16 +0000

    Added Methods For XSS Vulnerable Keywords Check.

--- dev_11Jun2009/software/cab2b/src/java/server/edu/wustl/cab2b/server/util/XSSVulnerableDetector.java
+++ dev_11Jun2009/software/cab2b/src/java/server/edu/wustl/cab2b/server/util/XSSVulnerableDetector.java
@@ -22,9 +22,9 @@ public class XSSVulnerableDetector {
     private static final String REGEX_SQLINJECTION_VULNERABLE = "[()<>'=\\*&;]";
 
     /**
-     * This method check for Xss vulnerable characters like <, >, [, ] etc.
-     * @param String for which Xss vulnerable character [ ,< or > ] to be checked.
-     * @return true if the string contains any Xss vulnerable character else false.
+     * This method check for XSS vulnerable characters, like, <, >, [, ], etc.
+     * @param String for which XSS vulnerable character [ ,< or > ] to be checked.
+     * @return true if the string contains any XSS vulnerable character else false.
      */
     public static boolean isXssVulnerable(String value) {
         boolean isXssVulnerable = false;
@@ -35,13 +35,28 @@ public class XSSVulnerableDetector {
         }
         return isXssVulnerable;
     }
+ 
+    /**
+     * This method check for XSS vulnerable keywords, like, alert, etc.
+     * @param String for which XSS vulnerable keyword to be checked.
+     * @return true if the string contains any XSS vulnerable keyword else false.
+     */    
+    public static boolean isXssKeywordVulnerable(String value) {
+        boolean isXssKeywordVulnerable = false;
+        if (value != null) {
+            Pattern p = Pattern.compile(REGEX_XSS_VULNERABLE_KEYWORDS, Pattern.CASE_INSENSITIVE);
+            Matcher m = p.matcher(value);
+            isXssKeywordVulnerable = m.find();
+        }
+        return isXssKeywordVulnerable;
+    }    
 
     /**
-     * This method check for Xss vulnerable characters like <, >, (, ) etc.
+     * This method check for XSS vulnerable characters like <, >, (, ) etc.
      * This method also checks to see if the input contains any SQL injection
      * or Cross scripting vulnerable keywords. 
-     * @param String for which Xss vulnerable character [ (,),< or > ] to be checked.
-     * @return true if the string contains any Xss vulnerable character else false.
+     * @param String for which XSS vulnerable character [ (,),< or > ] to be checked.
+     * @return true if the string contains any XSS vulnerable character else false.
      */
     public static boolean isXssSQLVulnerable(String value) {
         boolean isXssVulnerable = false;
