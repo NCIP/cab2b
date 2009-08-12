91ec41ebc5b2208b6ad4119a0a26d867cb388c86
PallaviM <PallaviM@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-12 09:07:35 +0000

    Code changes for adding keyword/MMC wrapper by chetan patil

---
+++ dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/queryengine/CompoundQueryImpl.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package edu.wustl.cab2b.common.queryengine;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+
+/**
+ * @author chetan_patil
+ *
+ */
+public abstract class CompoundQueryImpl extends Cab2bQuery implements CompoundQuery {
+    private static final long serialVersionUID = 8692891177510236676L;
+
+    private Collection<ICab2bQuery> subQueries = new HashSet<ICab2bQuery>();
+
+    /**
+     * 
+     */
+    public CompoundQueryImpl() {
+        super();
+    }
+
+    /**
+     * @param query
+     */
+    public CompoundQueryImpl(ICab2bQuery query) {
+        super(query);
+    }
+
+    /**
+     * @param id
+     * @param name
+     * @param description
+     * @param createdDate
+     */
+    public CompoundQueryImpl(Long id, String name, String description, Date createdDate) {
+        super(id, name, description, createdDate);
+    }
+
+    /* (non-Javadoc)
+     * @see edu.wustl.cab2b.common.queryengine.MultiModelCategoryQuery#getSubQueries()
+     */
+    public Collection<ICab2bQuery> getSubQueries() {
+        return subQueries;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see edu.wustl.cab2b.common.queryengine.MultiModelCategoryQuery#addSubQueries(edu.wustl.cab2b.common.queryengine.ICab2bQuery)
+     */
+    public void addSubQuery(ICab2bQuery query) {
+        subQueries.add(query);
+    }
+
+    @Override
+    public void setSubQueries(Collection<ICab2bQuery> queries) {
+        subQueries.addAll(queries);
+    }
+
+}
