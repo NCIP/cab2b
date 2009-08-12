a81aaec12bdada17a6ccd5eb5289992652d448a9
ShinganD <ShinganD@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-12 13:18:41 +0000

    Changes related to Query execution in background by Chetan Pundhir

---
+++ dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/querystatus/QueryURLStatusOperations.java
@@ -0,0 +1,40 @@
+package edu.wustl.cab2b.server.queryengine.querystatus;
+
+import java.util.Collection;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import edu.wustl.cab2b.common.ejb.queryengine.querystatus.QueryStatus;
+import edu.wustl.cab2b.common.user.UserInterface;
+import edu.wustl.common.hibernate.HibernateDatabaseOperations;
+import edu.wustl.common.hibernate.HibernateUtil;
+import edu.wustl.common.util.dbManager.HibernateUtility;
+
+public class QueryURLStatusOperations {
+
+    public void insertQueryStatus(QueryStatus qs) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            HibernateDatabaseOperations<QueryStatus> handler =
+                    new HibernateDatabaseOperations<QueryStatus>(session);
+            handler.insertOrUpdate(qs);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            session.flush();
+            session.close();
+        }
+    }
+
+    public Collection<QueryStatus> getQueryStatus(UserInterface user) {
+        Collection<QueryStatus> queryStatus = null;
+        try {
+            queryStatus = HibernateUtility.executeHQL("getQueryStatus");
+        } catch (HibernateException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+        return queryStatus;
+    }
+}
