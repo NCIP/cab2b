4197c0415cac18c0f66ecfee8a53f51c6fee3411
PallaviM <PallaviM@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-13 14:07:28 +0000

    Refactored for new query design

--- dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/queryengine/MultiModelCategoryQuery.java
+++ dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/queryengine/MultiModelCategoryQuery.java
@@ -1,9 +1,4 @@
-/**
- * 
- */
 package edu.wustl.cab2b.common.queryengine;
-
-import java.util.Collection;
 
 /**
  * MultiModelCategoryQuery class represents query object for MultiModelCategory.
@@ -11,24 +6,6 @@ import java.util.Collection;
  * @author chetan_patil
  *
  */
-public interface MultiModelCategoryQuery extends ICab2bQuery {
-    
-    /**
-     * This method adds a single query to the collection of sub queries
-     * @param query
-     */
-    void addSubQuery(ICab2bQuery query);
-    
-    /**
-     * This method sets the given collections of queries 
-     * @param queries
-     */
-    void setSubQueries(Collection<ICab2bQuery> queries);
-        
-    /**
-     * This method returns respective the sub queries for all the child categories.
-     * @return
-     */
-    Collection<ICab2bQuery> getSubQueries();
+public interface MultiModelCategoryQuery extends CompoundQuery {
 
 }
--- dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/queryengine/MultiModelCategoryQueryImpl.java
+++ dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/queryengine/MultiModelCategoryQueryImpl.java
@@ -1,20 +1,13 @@
-/**
- * 
- */
 package edu.wustl.cab2b.common.queryengine;
 
-import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
 
 /**
  * @author chetan_patil
  *
  */
-public class MultiModelCategoryQueryImpl extends Cab2bQuery implements MultiModelCategoryQuery {
+public class MultiModelCategoryQueryImpl extends CompoundQueryImpl implements MultiModelCategoryQuery {
     private static final long serialVersionUID = -7969272734716913783L;
-
-    private Collection<ICab2bQuery> subQueries = new HashSet<ICab2bQuery>();
 
     /**
      * Default constructor
@@ -39,27 +32,6 @@ public class MultiModelCategoryQueryImpl extends Cab2bQuery implements MultiMode
      */
     public MultiModelCategoryQueryImpl(Long id, String name, String description, Date createdDate) {
         super(id, name, description, createdDate);
-    }
-
-    /* (non-Javadoc)
-     * @see edu.wustl.cab2b.common.queryengine.MultiModelCategoryQuery#getSubQueries()
-     */
-
-    public Collection<ICab2bQuery> getSubQueries() {
-        return subQueries;
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see edu.wustl.cab2b.common.queryengine.MultiModelCategoryQuery#addSubQueries(edu.wustl.cab2b.common.queryengine.ICab2bQuery)
-     */
-    public void addSubQuery(ICab2bQuery query) {
-        subQueries.add(query);
-    }
-
-    @Override
-    public void setSubQueries(Collection<ICab2bQuery> queries) {
-        subQueries.addAll(queries);
     }
 
 }
