2c2b35de705cfb7135e927467eebeff1f298281a
MehtaG <MehtaG@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-14 07:09:44 +0000

    Removed the relation between URLStatus and ServiceURL

--- dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/queryengine/querystatus/URLStatus.java
+++ dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/queryengine/querystatus/URLStatus.java
@@ -3,17 +3,15 @@
  */
 package edu.wustl.cab2b.common.queryengine.querystatus;
 
-import edu.wustl.cab2b.common.user.ServiceURLInterface;
-
 /**
  * This is the Interface class for URLStatusImpl concrete class
  * @author gaurav_mehta
  */
-public interface URLStatus extends AbstarctStatus{
-    
+public interface URLStatus extends AbstarctStatus {
+
     /** Gets the ServiceURLInterface object for that url */
-    public ServiceURLInterface getUrl();
-    
+    public String getUrl();
+
     /** sets the ServiceURL object for a url in a query */
-    public void setUrl(ServiceURLInterface url);
+    public void setUrl(String url);
 }
--- dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/queryengine/querystatus/URLStatusImpl.java
+++ dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/queryengine/querystatus/URLStatusImpl.java
@@ -1,24 +1,22 @@
 package edu.wustl.cab2b.common.queryengine.querystatus;
 
-import edu.wustl.cab2b.common.user.ServiceURLInterface;
-
 /**
  * This class is the Hibernate class for URL_STATUS 
  * It extends AbstractStatusImpl for properties like : ID, Status, Message, ResultCount, Description
  * @author gaurav_mehta
  */
-public class URLStatusImpl extends AbstractStatusImpl implements URLStatus{
+public class URLStatusImpl extends AbstractStatusImpl implements URLStatus {
 
     private static final long serialVersionUID = 1L;
 
     /** This is the reference of serviceURL object for urls in a query */
-    private ServiceURLInterface url;
+    private String url;
 
     /**
      * @return the serviceURL object of a url in a query
      * @hibernate.property name="url" column="URL" type="edu.wustl.cab2b.common.user.ServiceURLInterface" not-null="true"
      */
-    public ServiceURLInterface getUrl() {
+    public String getUrl() {
         return url;
     }
 
@@ -26,7 +24,7 @@ public class URLStatusImpl extends AbstractStatusImpl implements URLStatus{
      * sets the ServiceURL object for a url in a query
      * @param url the url to set
      */
-    public void setUrl(ServiceURLInterface url) {
+    public void setUrl(String url) {
         this.url = url;
     }
 }
