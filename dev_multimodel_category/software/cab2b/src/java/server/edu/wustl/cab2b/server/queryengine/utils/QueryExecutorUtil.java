bb2b102b2df6638cbdd1b5b09fc88531bbdf60c9
PallaviM <PallaviM@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-14 12:40:48 +0000

    moved utility functions from webapp utility to cab2b server bcz of introduction of execution handlers.

--- dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/utils/QueryExecutorUtil.java
+++ dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/utils/QueryExecutorUtil.java
@@ -1,13 +1,37 @@
 package edu.wustl.cab2b.server.queryengine.utils;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
+import org.globus.gsi.GlobusCredential;
+
+import edu.common.dynamicextensions.domaininterface.EntityGroupInterface;
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.wustl.cab2b.common.errorcodes.ErrorCodeConstants;
+import edu.wustl.cab2b.common.exception.RuntimeException;
+import edu.wustl.cab2b.common.queryengine.ICab2bQuery;
+import edu.wustl.cab2b.common.queryengine.MultiModelCategoryQuery;
 import edu.wustl.cab2b.common.queryengine.result.ICategorialClassRecord;
+import edu.wustl.cab2b.common.user.ServiceURLInterface;
+import edu.wustl.cab2b.common.user.UserInterface;
+import edu.wustl.cab2b.server.cache.EntityCache;
+import edu.wustl.cab2b.server.category.CategoryOperations;
+import edu.wustl.cab2b.server.user.UserOperations;
+import edu.wustl.cab2bwebapp.constants.Constants;
 import edu.wustl.common.querysuite.metadata.category.CategorialClass;
+import edu.wustl.common.querysuite.metadata.category.Category;
+import edu.wustl.common.querysuite.queryobject.ICondition;
+import edu.wustl.common.querysuite.queryobject.IExpression;
+import edu.wustl.common.querysuite.queryobject.IExpressionOperand;
+import edu.wustl.common.querysuite.queryobject.IRule;
 
 /**
  * @author Deepak
+ * @author pallavi_mistry
  */
 public class QueryExecutorUtil {
     /**
@@ -78,4 +102,155 @@ public class QueryExecutorUtil {
         }
         return leavesCount; //if its not a leaf, while returning in the called stack, return 0 for the interleaved nodes.
     }
+
+    /**
+     * This method returns the EntityGroupInterface for given query
+     * @param query
+     * @return EntityGroupInterfcae object of query
+     */
+    public static Collection<EntityGroupInterface> getEntityGroups(ICab2bQuery query) {
+        Collection<EntityGroupInterface> entityGroups = new HashSet<EntityGroupInterface>();
+
+        EntityInterface outputEntity = query.getOutputEntity();
+        if (edu.wustl.cab2b.common.util.Utility.isCategory(outputEntity)) {
+            entityGroups.add(getEntityGroupForCategory(outputEntity));
+        } else if (edu.wustl.cab2b.common.util.Utility.isMultiModelCategory(outputEntity)) {
+            MultiModelCategoryQuery mmcQuery = (MultiModelCategoryQuery) query;
+            Collection<ICab2bQuery> subQueries = mmcQuery.getSubQueries();
+            for (ICab2bQuery subQuery : subQueries) {
+                outputEntity = subQuery.getOutputEntity();
+                entityGroups.add(getEntityGroupForCategory(outputEntity));
+            }
+        } else {
+            entityGroups.add(edu.wustl.cab2b.common.util.Utility.getEntityGroup(outputEntity));
+        }
+
+        return entityGroups;
+    }
+
+    private static EntityGroupInterface getEntityGroupForCategory(EntityInterface outputEntity) {
+        CategoryOperations categoryOperations = new CategoryOperations();
+        Category category = categoryOperations.getCategoryByEntityId(outputEntity.getId());
+        outputEntity = EntityCache.getInstance().getEntityById(category.getRootClass().getDeEntityId());
+
+        return edu.wustl.cab2b.common.util.Utility.getEntityGroup(outputEntity);
+    }
+
+    /**
+     * @param mmcQuery
+     * @return
+     */
+    public static Map<String, List<String>> getAttributeNameValuesMap(MultiModelCategoryQuery mmcQuery) {
+        Map<String, List<String>> attributeNameValuesMap = new HashMap<String, List<String>>();
+        IRule rule = getOperand(mmcQuery);
+        for (ICondition condition : rule) {
+            attributeNameValuesMap.put(condition.getAttribute().getName(), condition.getValues());
+        }
+        return attributeNameValuesMap;
+    }
+
+    /**
+     * @param query
+     * @return
+     */
+    public static IRule getOperand(ICab2bQuery query) {
+        IExpression expression = query.getConstraints().getExpression(1);
+        IExpressionOperand operand = expression.getOperand(0);
+
+        IRule rule = null;
+        if (operand instanceof IRule) {
+            rule = (IRule) operand;
+        }
+
+        return rule;
+    }
+
+    /**
+     * @param queries
+     * @param proxy
+     * @param user
+     * @param modelGroupNames
+     * @throws RuntimeException
+     */
+    public static void insertURLConditions(Collection<? extends ICab2bQuery> queries, GlobusCredential proxy,
+                                     UserInterface user, String[] modelGroupNames) throws RuntimeException {
+        Map<EntityGroupInterface, List<String>> entityGroupURLsMap = getUserConfiguredUrls(user, modelGroupNames);
+        for (ICab2bQuery query : queries) {
+            Collection<EntityGroupInterface> queryEntityGroups = QueryExecutorUtil.getEntityGroups(query);
+            for (EntityGroupInterface queryEntityGroup : queryEntityGroups) {
+                List<String> urls = entityGroupURLsMap.get(queryEntityGroup);
+                if (urls != null && !urls.isEmpty()) {
+                    query.setOutputUrls(urls);
+                } else if (query.getIsSystemGenerated()) {
+                    query.setOutputUrls(new ArrayList<String>(0));
+                } else {
+                    StringBuffer errorMessage =
+                            new StringBuffer("Incorrect service instance configured for query ");
+                    errorMessage.append("having model as ").append(queryEntityGroup.getName());
+                    throw new RuntimeException(errorMessage.toString(), ErrorCodeConstants.MG_008);
+                }
+            }
+        }
+    }
+
+    /**
+     * This method returns the List of url's which are configured by user for that model group/entity group.
+     * This method is required to set the url's in query
+     * @param user
+     * @param entityGroups
+     * @return List<String>
+     * @throws Exception
+     */
+    public static Map<EntityGroupInterface, List<String>> getUserConfiguredUrls(UserInterface user,
+                                                                                String[] modelGroupNames) {
+        Map<EntityGroupInterface, List<String>> entityGroupVsSelectedUrls =
+                new HashMap<EntityGroupInterface, List<String>>();
+
+        if (modelGroupNames != null && modelGroupNames.length != 0) {
+            Collection<ServiceURLInterface> userConfiguredUrls = user.getServiceURLCollection();
+
+            if (userConfiguredUrls == null || userConfiguredUrls.size() == 0) {
+                if (!user.isAdmin()) {
+                    entityGroupVsSelectedUrls =
+                            getUserConfiguredUrls(new UserOperations().getAdmin(), modelGroupNames);
+                } else {
+                    throw new RuntimeException(Constants.SERVICE_INSTANCES_NOT_CONFIGURED);
+                }
+            } else {
+                for (ServiceURLInterface serviceUrl : userConfiguredUrls) {
+                    String entityGroupName = serviceUrl.getEntityGroupName();
+
+                    EntityGroupInterface entityGroup =
+                            EntityCache.getInstance().getEntityGroupByName(entityGroupName);
+                    List<String> urls = entityGroupVsSelectedUrls.get(entityGroup);
+                    if (urls == null) {
+                        urls = new ArrayList<String>();
+                        entityGroupVsSelectedUrls.put(entityGroup, urls);
+                    }
+                    urls.add(serviceUrl.getUrlLocation());
+                }
+            }
+        }
+        return entityGroupVsSelectedUrls;
+    }
+    
+    /**
+     * This method replaces the values of the conditions of given query with the provided keyword.
+     * @param query
+     */
+    public static void insertKeyword(ICab2bQuery query, String keyword) {
+        for (IExpression expression : query.getConstraints()) {
+            for (IExpressionOperand operand : expression) {
+                if (operand instanceof IRule) {
+                    IRule rule = (IRule) operand;
+                    for (ICondition condition : rule) {
+                        List<String> values = new ArrayList<String>();
+                        values.add(keyword);
+                        condition.setValues(values);
+                    }
+                }
+            }
+        }
+    }
+
 }
