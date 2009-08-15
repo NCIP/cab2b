5a3b4ec9adf3c2fce0831cc0c58e9d5cf4266b88
ctalele <ctalele@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-15 09:36:28 +0000

    Simplified the model loading by using single property file and hibernate connection. Reviewed by Gaurav Mehta

--- dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/path/LoadModelUtil.java
+++ dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/path/LoadModelUtil.java
@@ -1,76 +1,74 @@
 package edu.wustl.cab2b.server.path;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
+import edu.wustl.cab2b.common.util.Utility;
+import edu.wustl.common.util.dbManager.DBUtil;
+import gov.nih.nci.cagrid.common.Utils;
+import gov.nih.nci.cagrid.metadata.dataservice.DomainModel;
+
 /**
- * 
+ * Loads all domain model XMLs present in given folder as specified in model.dir property.
+ * This is used to load model from back end. It is not invoked during normal application execution. 
+ * To use this, create a new file named "server.properties" having following properties
+ * ---------------------------------------------- 
+            model.dir=C:/caDSR_Models/2009_aug_14
+            database.loader=edu.wustl.cab2b.server.util.DataFileLoader
+            max.path.length=6
+ * ----------------------------------------------
+ *  Update hibernate.cfg.xml with database information
+ *  Ensure hibernate.cfg.xml and above "server.properties" are present in classpath
+ *      
  * @author chandrakant_talele
- *
  */
 public class LoadModelUtil {
-
-    /* Format of properties file
-    db.driver=com.mysql.jdbc.Driver
-    db.server=10.99.88.100
-    db.port=8080
-    db.name=test_database
-    db.userName=root
-    db.password=password
-    max.path.length=6
-    #   If you want to load metadata for applications say "app1" and "app2" then 
-    #   entity.group.names=app1,app2
-    #   app1.file=<Model path for app1>
-    #   app2.file=<Model path for app2>
-    #
-    #   For example, to load metadata for caArray and caTissue, the cab2b.properties will look like
-    #   entity.group.names=caArray,caTissue
-    #   caArray.file=c:/caArrayModel.xml
-    #   caTissue.file=c:/caTissueModel.xml
-    entity.group.names=model1,model2
-    model1.file=file1.xml
-    model2.file=file2.xml 
+    /**
+     * Main method to load load model
+     * @param args command line args
+     * @throws Exception
      */
-	/**
-	 * Main method to load load model
-	 * @param args command line args
-	 * @throws Exception
-	 */
     public static void main(String[] args) throws Exception {
-        //Logger.configure();
-        if (args.length != 1 || args[0].trim().equals("")) {
-            System.out.println("Please provide a property file");
-            return;
-        }
-        Properties p = new Properties();
-        p.load(new FileInputStream(new File(args[0])));
-
-        String driver = p.getProperty("db.driver");
-        String server = p.getProperty("db.server");
-        String port = p.getProperty("db.port");
-        String dbName = p.getProperty("db.name");
-        String userName = p.getProperty("db.userName");
-        String password = p.getProperty("db.password");
+        System.out.println("Started at : " + new Date());
+        Properties p = Utility.getPropertiesFromFile("server.properties");
         int maxPathlength = Integer.parseInt(p.getProperty("max.path.length"));
-        String[] entityGroupNames = p.getProperty("entity.group.names").split(",");
-
-        String url = "jdbc:mysql://" + server + ":" + port + "/" + dbName;
+        File dir = new File(p.getProperty("model.dir"));
+        List<String> list = new ArrayList<String>();
+        for (File f : dir.listFiles()) {
+            if (isXML(f)) {
+                list.add(f.getAbsolutePath());
+            }
+        }
         Connection con = null;
         try {
-            Class.forName(driver).newInstance();
-            con = DriverManager.getConnection(url, userName, password);
-            for (String entityGroupName : entityGroupNames) {
-                String xmlFilePath = p.getProperty(entityGroupName + ".file").trim();
-                System.out.println("Loading model : " + xmlFilePath);
-                PathBuilder.loadSingleModel(con, xmlFilePath, entityGroupName, maxPathlength);
+            con = DBUtil.getConnection();
+            for (String path : list) {
+                DomainModel domainModel = (DomainModel) Utils.deserializeDocument(path, DomainModel.class);
+                System.out.println("Loading model : " + path);
+                PathBuilder.loadSingleModel(con, path, domainModel.getProjectLongName(), maxPathlength);
+                System.out.println("Finished at : " + new Date());
             }
         } finally {
+            System.out.println("Finished at : " + new Date());
             if (con != null) {
                 con.close();
             }
         }
     }
+
+    private static boolean isXML(File f) {
+        String path = f.getAbsolutePath();
+        String onlyName = path.substring(path.lastIndexOf("\\") + 1, path.length());
+        int idx = onlyName.lastIndexOf(".");
+        String ext = "";
+        if (idx > 0) {
+            ext = onlyName.substring(idx + 1, onlyName.length());
+        }
+        return ext.equalsIgnoreCase("xml");
+    }
+
 }
