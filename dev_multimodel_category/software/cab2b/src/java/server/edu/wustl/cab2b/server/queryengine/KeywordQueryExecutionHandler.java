9122a57cb94e514ec7c464b4b5748fdfc88a47d3
PallaviM <PallaviM@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-16 07:15:22 +0000

    added getKeyWrdvsResultsmap() method and made an attribute of the same at the class level.

--- dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/KeywordQueryExecutionHandler.java
+++ dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/KeywordQueryExecutionHandler.java
@@ -31,6 +31,7 @@ import edu.wustl.common.querysuite.queryobject.IRule;
 public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQuery> {
 
     private String keyword;
+    private Map<KeywordQuery, IQueryResult<? extends IRecord>> queryVsResultMap;
 
     /**
      * @param queries
@@ -47,6 +48,7 @@ public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQ
         super(query, proxy, user, modelGroupNames);
         this.keyword = keyword;
         this.queryExecutorsList = new ArrayList<QueryExecutor>(query.getSubQueries().size());
+        this.queryVsResultMap = new HashMap<KeywordQuery, IQueryResult<? extends IRecord>>(query.getSubQueries().size());
     }
 
     /* (non-Javadoc)
@@ -54,8 +56,8 @@ public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQ
      */
     @Override
     protected void executeQuery() {
-        Collection<ICab2bQuery> subQueries = new ArrayList<ICab2bQuery>();
-        subQueries.addAll(this.query.getSubQueries());
+        // TODO Auto-generated method stub
+        Collection<ICab2bQuery> subQueries = this.query.getSubQueries();
         for (ICab2bQuery query : subQueries) {
             QueryExecutor queryExecutor = new QueryExecutor(query, proxy);
             queryExecutorsList.add(queryExecutor);
@@ -76,19 +78,17 @@ public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQ
     public IQueryResult<? extends IRecord> getResult() {
         return null;
     }
-
+    
     /**
      * @return
      */
-    public Map<ICab2bQuery, IQueryResult<? extends IRecord>> getResults() {
-        Map<ICab2bQuery, IQueryResult<? extends IRecord>> results =
-                new HashMap<ICab2bQuery, IQueryResult<? extends IRecord>>(query.getSubQueries().size());
+    public Map<KeywordQuery, IQueryResult<? extends IRecord>> getQueryVsResultMap() {
         for (QueryExecutor executor : queryExecutorsList) {
             IQueryResult<? extends IRecord> result = executor.getPartialResult();
-            ICab2bQuery query = executor.getQuery();
-            results.put(query, result);
+            KeywordQuery query = (KeywordQuery)executor.getQuery();
+            queryVsResultMap.put(query, result);
         }
-        return results;
+        return queryVsResultMap;
     }
 
     /* (non-Javadoc)
@@ -96,6 +96,7 @@ public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQ
      */
     @Override
     public QueryStatusImpl getStatus() {
+        // TODO Auto-generated method stub
         return null;
     }
 
@@ -111,12 +112,17 @@ public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQ
      * @see edu.wustl.cab2b.server.queryengine.QueryExecutionHandler#preProcessQuery()
      */
     @Override
-    protected void preProcessQuery() throws RuntimeException {
+    protected void preProcessQuery() throws RuntimeException{
+        // TODO Auto-generated method stub
         Collection<ICab2bQuery> queries = this.query.getSubQueries();
         for (ICab2bQuery query : queries) {
             insertKeyword(query, this.keyword);
         }
         QueryExecutorUtil.insertURLConditions(queries, proxy, user, modelGroupNames);
+    }
+    
+    public KeywordQuery getQuery() {
+        return query;
     }
 
     /**
