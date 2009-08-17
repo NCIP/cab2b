330d325ec722c472e020f0951a2f7211e6c7d63d
PallaviM <PallaviM@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-17 08:26:39 +0000

    modified the code for ajax based execution of all type of queries.

--- dev_multimodel_category/software/cab2bWebApp/src/java/edu/wustl/cab2bwebapp/action/ExecuteQueryAction.java
+++ dev_multimodel_category/software/cab2bWebApp/src/java/edu/wustl/cab2bwebapp/action/ExecuteQueryAction.java
@@ -6,8 +6,6 @@ package edu.wustl.cab2bwebapp.action;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.ArrayList;
-import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -24,25 +22,27 @@ import org.apache.struts.action.ActionMapping;
 import org.globus.gsi.GlobusCredential;
 
 import edu.wustl.cab2b.common.queryengine.ICab2bQuery;
+import edu.wustl.cab2b.common.queryengine.KeywordQuery;
+import edu.wustl.cab2b.common.queryengine.MultiModelCategoryQuery;
 import edu.wustl.cab2b.common.user.UserInterface;
+import edu.wustl.cab2b.server.queryengine.CaB2QueryExecutionHandler;
+import edu.wustl.cab2b.server.queryengine.KeywordQueryExecutionHandler;
+import edu.wustl.cab2b.server.queryengine.MMCQueryExecutionHandler;
 import edu.wustl.cab2bwebapp.bizlogic.SavedQueryBizLogic;
-import edu.wustl.cab2bwebapp.bizlogic.executequery.ExecuteQueryBizLogic;
-import edu.wustl.cab2bwebapp.bizlogic.executequery.QueryUpdateBizLogic;
+import edu.wustl.cab2bwebapp.bizlogic.executequery.QueryBizLogic;
 import edu.wustl.cab2bwebapp.constants.Constants;
 
 /**
  * Action for executing query related operations.
- * @author deepak_shingan
- * @author chetan_patil
- * @author chetan_pundhir
+ * @author pallavi_mistry
+ * 
  */
 public class ExecuteQueryAction extends Action {
 
     private static final Logger logger = edu.wustl.common.util.logger.Logger.getLogger(ExecuteQueryAction.class);
 
     /**
-     * The execute method is called by the struts action flow and it calls the code to retrieve the 
-     * query results.
+     * The execute method is called via ajax by searchresults.jsp, it calls the code to execute the query
      * @param mapping
      * @param form
      * @param request
@@ -51,6 +51,9 @@ public class ExecuteQueryAction extends Action {
      * @throws IOException
      * @throws ServletException
      */
+    /* (non-Javadoc)
+     * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
     public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
                                  HttpServletResponse response) throws IOException, ServletException {
         try {
@@ -58,31 +61,17 @@ public class ExecuteQueryAction extends Action {
             String[] modelGroupNames = (String[]) session.getAttribute(Constants.MODEL_GROUPS);
             SavedQueryBizLogic savedQueryBizLogic =
                     (SavedQueryBizLogic) session.getAttribute(Constants.SAVED_QUERY_BIZ_LOGIC);
-            ICab2bQuery query =
-                    savedQueryBizLogic.getQueryById(Long.parseLong(request.getParameter(Constants.QUERY_ID)));
+            GlobusCredential proxy = (GlobusCredential) session.getAttribute(Constants.GLOBUS_CREDENTIAL);
             UserInterface user = (UserInterface) session.getAttribute(Constants.USER);
-
+            Long queryId = (Long) session.getAttribute(Constants.QUERY_ID);
+            String keyword = (String) session.getAttribute(Constants.KEYWORD);
             String conditionstr = (String) session.getAttribute(Constants.CONDITION_LIST);
-            if (conditionstr == null) {
-                conditionstr = "";
-            }
+            QueryBizLogic queryBizLogic =
+                    new QueryBizLogic(queryId, conditionstr, keyword, savedQueryBizLogic, user, proxy,
+                            modelGroupNames);
+            session.setAttribute(Constants.QUERY_BIZ_LOGIC_OBJECT, queryBizLogic);
 
-            //Update the IQuery according to parameterized conditions.
-            String errorMessage =
-                    new QueryUpdateBizLogic().setInputDataToQuery(conditionstr, query.getConstraints(), null,
-                                                                  query);
-            if (!errorMessage.equals("")) {
-                throw new Exception(errorMessage);
-            } else {
-                Collection<ICab2bQuery> queries = new ArrayList<ICab2bQuery>();
-                queries.add(query);
-
-                GlobusCredential proxy = (GlobusCredential) session.getAttribute(Constants.GLOBUS_CREDENTIAL);
-                ExecuteQueryBizLogic executeQueryBizLogic =
-                        new ExecuteQueryBizLogic(queries, proxy, user, modelGroupNames);
-                session.setAttribute(Constants.EXECUTE_QUERY_BIZ_LOGIC_OBJECT, executeQueryBizLogic);
-            }
-        } catch (Exception e) {
+        } catch (RuntimeException e) {
             logger.error(e.getMessage(), e);
             ActionErrors errors = new ActionErrors();
             ActionError error = new ActionError("fatal.home.failure", e.getMessage());
