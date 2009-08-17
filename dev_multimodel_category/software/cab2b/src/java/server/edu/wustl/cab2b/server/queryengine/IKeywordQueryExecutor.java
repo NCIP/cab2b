a731194400ba79871d4a17ea438dc7c9148a6a00
PallaviM <PallaviM@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-17 14:15:54 +0000

    Removed unwanted classes- By Deepak

--- dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/IKeywordQueryExecutor.java
+++
@@ -1,5 +0,0 @@
-package edu.wustl.cab2b.server.queryengine;
-
-public interface IKeywordQueryExecutor extends ICab2bQueryExecutor {
-    //Add your own methods
-}
--- dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/IMmcQueryExector.java
+++
@@ -1,5 +0,0 @@
-package edu.wustl.cab2b.server.queryengine;
-
-public interface IMmcQueryExector extends ICab2bQueryExecutor {
-    //Add mmc specific methods
-}
