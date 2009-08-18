381bdaea55695fb430c106837d01b20d149ed804
MehtaG <MehtaG@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-18 08:57:53 +0000

    Changes made in Test Case for getModelGroup method in UtilityOperation.java

--- dev_multimodel_category/software/cab2b/test/unit/java/server/edu/wustl/cab2b/server/util/UtilityOperationsTest.java
+++ dev_multimodel_category/software/cab2b/test/unit/java/server/edu/wustl/cab2b/server/util/UtilityOperationsTest.java
@@ -15,16 +15,17 @@ import edu.wustl.cab2b.server.cache.EntityCache;
  * @author gaurav_mehta
  *
  */
-public class UtilityOperationsTest extends TestCase{
-    
-    public void testgetModelGroups(){
+public class UtilityOperationsTest extends TestCase {
+
+    public void testgetModelGroups() {
         EntityCache cache = EntityCache.getInstance();
         EntityGroupInterface entityGroup = cache.getEntityGroupByName("caTissue_Core_1_2_v1.2");
-        Collection<ModelGroupInterface> modelGroups = UtilityOperations.getModelGroups(entityGroup);
+        Collection<ModelGroupInterface> modelGroups =
+                UtilityOperations.getModelGroups(entityGroup.getEntityCollection().iterator().next());
         assertTrue(modelGroups.isEmpty());
-        for(ModelGroupInterface mg : modelGroups){
-            if(!mg.getEntityGroupList().contains(entityGroup)){
-                assertFalse("Given Entity Group is not a part of Model group",Boolean.FALSE);
+        for (ModelGroupInterface mg : modelGroups) {
+            if (!mg.getEntityGroupList().contains(entityGroup)) {
+                assertFalse("Given Entity Group is not a part of Model group", Boolean.FALSE);
             }
         }
     }
