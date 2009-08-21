6e163bd6c19ec5811d96d7f76b3085cc037069a4
PallaviM <PallaviM@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-21 15:17:55 +0000

    Added consolidated status of MMC query. Added updateStatus(), initializeStatus(), setStatusChild() and called updateStatus() from getStatus() and postProcessResults(). -Needed as part of MMC query Execution. - By Deepak & Pallavi

--- dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/MMCQueryExecutionHandler.java
+++ dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/MMCQueryExecutionHandler.java
@@ -5,12 +5,18 @@ package edu.wustl.cab2b.server.queryengine;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.globus.gsi.GlobusCredential;
 
 import edu.wustl.cab2b.common.queryengine.ICab2bQuery;
 import edu.wustl.cab2b.common.queryengine.MultiModelCategoryQuery;
+import edu.wustl.cab2b.common.queryengine.querystatus.AbstractStatus;
 import edu.wustl.cab2b.common.queryengine.querystatus.QueryStatus;
+import edu.wustl.cab2b.common.queryengine.querystatus.QueryStatusImpl;
+import edu.wustl.cab2b.common.queryengine.querystatus.URLStatus;
 import edu.wustl.cab2b.common.queryengine.result.IQueryResult;
 import edu.wustl.cab2b.common.queryengine.result.IRecord;
 import edu.wustl.cab2b.common.user.UserInterface;
@@ -24,6 +30,8 @@ public class MMCQueryExecutionHandler extends QueryExecutionHandler<MultiModelCa
     private MMCQueryResultConflator resultConflator;
 
     private IQueryResult<? extends IRecord> result;
+
+    private QueryStatus status = null;
 
     /**
      * @param queries
@@ -39,7 +47,61 @@ public class MMCQueryExecutionHandler extends QueryExecutionHandler<MultiModelCa
         super(query, proxy, user, modelGroupNames);
         resultConflator = new MMCQueryResultConflator(query);
         this.queryExecutorsList = new ArrayList<QueryExecutor>(query.getSubQueries().size());
+        status = new QueryStatusImpl();
     }
+
+    /**
+     * 
+     */
+    private void updateStatus() {
+      //  if (status.getStatus() == null || !status.getStatus().equals(AbstractStatus.Processing)) {
+            String desc = "";
+            boolean isProcessing = false;
+            boolean isCompleteWithError = false;
+            boolean isNull = true;
+            Integer recCount = null;
+            for (QueryExecutor queryExecutor : queryExecutorsList) {
+                QueryStatus qs = queryExecutor.getStatus();
+                desc = desc + "  " + qs.getDescription();
+                Integer resCount = qs.getResultCount();
+                if (resCount != null) {
+                    recCount = +resCount;
+                }
+                String statusStr = qs.getStatus();
+                if (statusStr != null) {
+                    isNull = false;
+                    if (statusStr.equals(AbstractStatus.Processing)) {
+                        isProcessing = true;
+                    } else if (statusStr.equals(AbstractStatus.Complete_With_Error)) {
+                        isCompleteWithError = true;
+                    }
+                }
+            }
+            if (status.getQueryStartTime() == null) {
+                status.setQueryStartTime(new Date());
+            }
+            status.setResultCount(recCount);
+            if (isProcessing) {
+                status.setStatus(AbstractStatus.Processing);
+            } else {
+                boolean isQueryExecutorListFinished = true;
+                for (QueryExecutor queryExecutor : queryExecutorsList) {
+                    if (!queryExecutor.isProcessingFinished()) {
+                        isQueryExecutorListFinished = false;
+                        break;
+                    }
+                }
+                if (isQueryExecutorListFinished) {
+                    if (isCompleteWithError) {
+                        status.setStatus(AbstractStatus.Complete_With_Error);
+                    } else if (!isNull) {
+                        status.setStatus(AbstractStatus.Complete);
+                        status.setQueryEndTime(new Date());
+                    }
+                }
+            }
+        }
+   // }
 
     /* (non-Javadoc)
      * @see edu.wustl.cab2b.server.queryengine.QueryExecutionHandler#executeQuery()
@@ -50,16 +112,43 @@ public class MMCQueryExecutionHandler extends QueryExecutionHandler<MultiModelCa
         for (ICab2bQuery query : subQueries) {
             QueryExecutor queryExecutor = new QueryExecutor(query, proxy);
             this.queryExecutorsList.add(queryExecutor);
+
         }
-        //making it serial temporarily
+        new Thread() {
+            public void run() {
+                for (QueryExecutor queryExecutor : queryExecutorsList) {
+                    queryExecutor.executeQuery();
+                }
+            }
+        }.start();
+
+        initializeQueryStatus();
+        updateStatus();
+        setStatusChild();
+    }
+
+    /**
+     * Initializes main query status with Child query URL-status.
+     * 
+     */
+    private void initializeQueryStatus() {
+        Set<URLStatus> queryURLStatusSet = new HashSet<URLStatus>();
         for (QueryExecutor queryExecutor : queryExecutorsList) {
-            queryExecutor.executeQuery();
+            Set<URLStatus> urlStatusSet = queryExecutor.getStatus().getUrlStatus();
+            queryURLStatusSet.addAll(urlStatusSet);
         }
-        /* new Thread() {
-             public void run() {
-                 
-             }
-         }.start();*/
+        status.setUrlStatus(queryURLStatusSet);
+    }
+
+    /**
+     * Set status for all child/sub queries.
+     */
+    private void setStatusChild() {
+        Set<QueryStatus> childrenQueryStatus = new HashSet<QueryStatus>(query.getSubQueries().size());
+        for (QueryExecutor queryExecutor : queryExecutorsList) {
+            childrenQueryStatus.add(queryExecutor.getStatus());
+        }
+        status.setChildrenQueryStatus(childrenQueryStatus);
     }
 
     /**
@@ -77,10 +166,8 @@ public class MMCQueryExecutionHandler extends QueryExecutionHandler<MultiModelCa
      */
     @Override
     public QueryStatus getStatus() {
-        for (QueryExecutor queryExecutor : queryExecutorsList) {
-
-        }
-        return null;
+        updateStatus();
+        return status;
     }
 
     /* (non-Javadoc)
@@ -104,6 +191,7 @@ public class MMCQueryExecutionHandler extends QueryExecutionHandler<MultiModelCa
             }
         }
         result = resultConflator.conflate(queryResults);
+        updateStatus();
     }
 
     /* (non-Javadoc)
