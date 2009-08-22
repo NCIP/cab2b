b30fcb36bfd714843c195e03a3622a1962f6da36
PallaviM <PallaviM@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-22 11:07:43 +0000

    Added consolidated status of Keyword query. Added updateStatus(), initializeStatus(), setStatusChild() and called updateStatus() from getStatus() and postProcessResults(). -Needed as part of Keyword query Execution. - By Deepak & Pallavi

--- dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/KeywordQueryExecutionHandler.java
+++ dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/KeywordQueryExecutionHandler.java
@@ -5,16 +5,23 @@ package edu.wustl.cab2b.server.queryengine;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.apache.log4j.Logger;
 import org.globus.gsi.GlobusCredential;
 
 import edu.wustl.cab2b.common.exception.RuntimeException;
 import edu.wustl.cab2b.common.queryengine.ICab2bQuery;
 import edu.wustl.cab2b.common.queryengine.KeywordQuery;
+import edu.wustl.cab2b.common.queryengine.querystatus.AbstractStatus;
+import edu.wustl.cab2b.common.queryengine.querystatus.QueryStatus;
 import edu.wustl.cab2b.common.queryengine.querystatus.QueryStatusImpl;
+import edu.wustl.cab2b.common.queryengine.querystatus.URLStatus;
 import edu.wustl.cab2b.common.queryengine.result.IQueryResult;
 import edu.wustl.cab2b.common.queryengine.result.IRecord;
 import edu.wustl.cab2b.common.user.UserInterface;
@@ -29,10 +36,14 @@ import edu.wustl.common.querysuite.queryobject.IRule;
  *
  */
 public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQuery> {
+    private static final Logger logger =
+            edu.wustl.common.util.logger.Logger.getLogger(KeywordQueryExecutionHandler.class);
 
     private String keyword;
 
     private Map<ICab2bQuery, IQueryResult<? extends IRecord>> queryVsResultMap;
+
+    private QueryStatus status = null;
 
     /**
      * @param queries
@@ -51,6 +62,84 @@ public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQ
         this.queryExecutorsList = new ArrayList<QueryExecutor>(query.getSubQueries().size());
         this.queryVsResultMap =
                 new HashMap<ICab2bQuery, IQueryResult<? extends IRecord>>(query.getSubQueries().size());
+        status = new QueryStatusImpl();
+    }
+
+    /**
+     * Set status for all child/sub queries.
+     */
+    private void setStatusChild() {
+        Set<QueryStatus> childrenQueryStatus = new HashSet<QueryStatus>(query.getSubQueries().size());
+        for (QueryExecutor queryExecutor : queryExecutorsList) {
+            childrenQueryStatus.add(queryExecutor.getStatus());
+        }
+        status.setChildrenQueryStatus(childrenQueryStatus);
+    }
+
+    /**
+     * Initializes main query status with Child query URL-status.
+     * 
+     */
+    private void initializeQueryStatus() {
+        Set<URLStatus> queryURLStatusSet = new HashSet<URLStatus>();
+        for (QueryExecutor queryExecutor : queryExecutorsList) {
+            Set<URLStatus> urlStatusSet = queryExecutor.getStatus().getUrlStatus();
+            queryURLStatusSet.addAll(urlStatusSet);
+        }
+        status.setUrlStatus(queryURLStatusSet);
+    }
+    /**
+     * Method to update url status properties. It will update only the in-memory query status.
+     */
+    private void updateStatus() {
+    	logger.info("--------Updating Keyword query status.");
+        if (status.getStatus() == null || !status.getStatus().equals(AbstractStatus.Processing)) {
+            String desc = "";
+            boolean isProcessing = false;
+            boolean isCompleteWithError = false;
+            boolean isNull = true;
+            Integer recCount = null;
+            for (QueryExecutor queryExecutor : queryExecutorsList) {
+                QueryStatus qs = queryExecutor.getStatus();
+                desc = desc + "  " + qs.getDescription();
+                Integer resCount = qs.getResultCount();
+                if (resCount != null) {
+                    recCount = +resCount;
+                }
+                String statusStr = qs.getStatus();
+                if (statusStr != null) {
+                    isNull = false;
+                    if (statusStr.equals(AbstractStatus.Processing)) {
+                        isProcessing = true;
+                    } else if (statusStr.equals(AbstractStatus.Complete_With_Error)) {
+                        isCompleteWithError = true;
+                    }
+                }
+            }
+            if (status.getQueryStartTime() == null) {
+                status.setQueryStartTime(new Date());
+            }
+            status.setResultCount(recCount);
+            if (isProcessing) {
+                status.setStatus(AbstractStatus.Processing);
+            } else {
+                boolean isQueryExecutorListFinished = true;
+                for (QueryExecutor queryExecutor : queryExecutorsList) {
+                    if (!queryExecutor.isProcessingFinished()) {
+                        isQueryExecutorListFinished = false;
+                        break;
+                    }
+                }
+                if (isQueryExecutorListFinished) {
+                    if (isCompleteWithError) {
+                        status.setStatus(AbstractStatus.Complete_With_Error);
+                    } else if (!isNull) {
+                        status.setStatus(AbstractStatus.Complete);
+                        status.setQueryEndTime(new Date());
+                    }
+                }
+            }
+        }
     }
 
     /* (non-Javadoc)
@@ -70,7 +159,12 @@ public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQ
                 }
             }
         }.start();
+
+        initializeQueryStatus();
+        updateStatus();
+        setStatusChild();
     }
+
 
     /**
      * For keyword query, one needs to call getQueryVsResultMap() instead of getResult()
@@ -100,6 +194,7 @@ public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQ
             ICab2bQuery query = (ICab2bQuery) executor.getQuery();
             queryVsResultMap.put(query, result);
         }
+        updateStatus();
         return queryVsResultMap;
     }
 
@@ -107,9 +202,9 @@ public class KeywordQueryExecutionHandler extends QueryExecutionHandler<KeywordQ
      * @see edu.wustl.cab2b.server.queryengine.QueryExecutionHandler#getStatus()
      */
     @Override
-    public QueryStatusImpl getStatus() {
-        // TODO Auto-generated method stub
-        return null;
+    public QueryStatus getStatus() {
+        updateStatus();
+        return status;
     }
 
     /* (non-Javadoc)
