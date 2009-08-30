93b2cadaab8f4bc99cc437116c85709330e49dd3
VyasR <VyasR@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-08-30 14:58:29 +0000

    Moved getUserByName() inside IF. User is required only for SAVED queries. It was causing problem in Apply Data List

--- dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/QueryExecutor.java
+++ dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/QueryExecutor.java
@@ -180,11 +180,8 @@ public class QueryExecutor {
         if (credential != null) {
             userName = credential.getIdentity();
         }
-        UserInterface user = new UserOperations().getUserByName(userName);
-
         qStatus = new QueryStatusImpl();
         qStatus.setQuery(query);
-        qStatus.setUser(user);
         qStatus.setVisible(Boolean.FALSE);
         qStatus.setQueryConditions(UtilityOperations.getStringRepresentationofConstraints(query.getConstraints()));
         qStatus.setStatus(AbstractStatus.Processing);
@@ -199,6 +196,8 @@ public class QueryExecutor {
         }
         qStatus.setUrlStatus(urlStatusCollection);
         if (recordStatus) {
+            UserInterface user = new UserOperations().getUserByName(userName);
+            qStatus.setUser(user);
             QueryURLStatusOperations qso = new QueryURLStatusOperations();
             qso.insertQueryStatus(qStatus);
         }
