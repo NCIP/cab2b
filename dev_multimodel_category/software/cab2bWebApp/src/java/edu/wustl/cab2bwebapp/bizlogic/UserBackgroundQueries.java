fd89a8f260963ee50a81d47b30c6ca9382295b63
ShinganD <ShinganD@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-09-02 17:51:57 +0000

    Changed BACKGROUND_QUERY_REFRESH_TIME_INTERVAL = 15. It was set to 10 during local code testing and mistakenly updated in SVN.

--- dev_multimodel_category/software/cab2bWebApp/src/java/edu/wustl/cab2bwebapp/bizlogic/UserBackgroundQueries.java
+++ dev_multimodel_category/software/cab2bWebApp/src/java/edu/wustl/cab2bwebapp/bizlogic/UserBackgroundQueries.java
@@ -43,7 +43,7 @@ public class UserBackgroundQueries {
     /**
      * Time interval for updating database for queries executing in background. 
      */
-    public static final int BACKGROUND_QUERY_REFRESH_TIME_INTERVAL = 10;
+    public static final int BACKGROUND_QUERY_REFRESH_TIME_INTERVAL = 15;
 
     static {
         //Creating directory for saving CSV files on server.
