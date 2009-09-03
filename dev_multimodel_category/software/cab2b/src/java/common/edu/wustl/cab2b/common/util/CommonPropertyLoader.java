2d6897aa0cfeebfb56e3b1540b32b6d2e60c6a87
VyasR <VyasR@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-09-03 10:21:38 +0000

    Truststore property file and ignore host name verification  properties are added for non-trusted certificates.

--- dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/util/CommonPropertyLoader.java
+++ dev_multimodel_category/software/cab2b/src/java/common/edu/wustl/cab2b/common/util/CommonPropertyLoader.java
@@ -13,7 +13,10 @@ import org.apache.log4j.Logger;
 public class CommonPropertyLoader {
     private static final Logger logger = edu.wustl.common.util.logger.Logger.getLogger(CommonPropertyLoader.class);
 
-    private static Properties properties = Utility.getPropertiesFromFile("cab2b.properties");
+    public static final String propertyFileName = "cab2b.properties";
+    
+    private static Properties properties = Utility.getPropertiesFromFile(propertyFileName);
+    
 
     /**
      * @return The URL of JNDI service running on caB2B server
@@ -25,6 +28,15 @@ public class CommonPropertyLoader {
         String invokerURLSuffix = properties.getProperty("caB2B.server.jndi.invoker.url.suffix");
         return jndiProtocol+"://" + serverIP + ":" + jndiPort+invokerURLSuffix;
     }
+    
+    public static String getSSLTruststoreFileName(){
+        return properties.getProperty("caB2B.ssl.truststore.filename");
+    }
+    
+    public static String isIgnoreHttpHost(){
+        return properties.getProperty("org.jboss.security.ignoreHttpsHost");
+    }
+    
     
     //https://localhost:8443/invoker/JNDIFactorySSL
     //"jnp://" + serverIP + ":" + jndiPort;
