aa720c263bbf8b90ea9c6a656e36ff00e67248bf
VyasR <VyasR@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-09-04 12:35:36 +0000

    New method for Apply datalist functionality.

--- dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/ejb/queryengine/QueryEngineBean.java
+++ dev_multimodel_category/software/cab2b/src/java/server/edu/wustl/cab2b/server/ejb/queryengine/QueryEngineBean.java
@@ -41,6 +41,25 @@ public class QueryEngineBean extends AbstractStatelessSessionBean implements Que
             throws RemoteException {
         return new QueryOperations().executeQuery(query, serializedDCR);
     }
+    
+    /**
+     * This method executes the given query if the given credential is authentic.
+     *
+     * @see edu.wustl.cab2b.common.ejb.queryengine.QueryEngineBusinessInterface#executeQuery(edu.wustl.cab2b.common.queryengine.ICab2bQuery,
+     *      org.globus.gsi.GlobusCredential)
+     *
+     * @param query ICab2bQuery to be executed
+     * @param serializedDCR
+     * @param idP
+     * @return query result
+     * @throws Exception if authentication fails or query execution fails.
+     * @throws GeneralSecurityException
+     * @throws IOException
+     */
+    public IQueryResult<? extends IRecord> executeQueryForApplyDatalist(ICab2bQuery query, String serializedDCR)
+            throws RemoteException {
+        return new QueryOperations().executeQueryForApplyDatalist(query, serializedDCR);
+    }
 
     /**
      * This method saves the given ICab2bQuery object.
