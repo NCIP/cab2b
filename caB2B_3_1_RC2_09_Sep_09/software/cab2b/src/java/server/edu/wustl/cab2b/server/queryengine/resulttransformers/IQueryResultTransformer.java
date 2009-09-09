5dae0a093ed45b2c616de32b00a04a7a2c2683db
VyasR <VyasR@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-09-09 15:55:18 +0000

    Tag for RC2 release.

---
+++ caB2B_3_1_RC2_09_Sep_09/software/cab2b/src/java/server/edu/wustl/cab2b/server/queryengine/resulttransformers/IQueryResultTransformer.java
@@ -0,0 +1,47 @@
+package edu.wustl.cab2b.server.queryengine.resulttransformers;
+
+import org.globus.gsi.GlobusCredential;
+
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.wustl.cab2b.common.queryengine.result.ICategorialClassRecord;
+import edu.wustl.cab2b.common.queryengine.result.IQueryResult;
+import edu.wustl.cab2b.common.queryengine.result.IRecord;
+import edu.wustl.common.querysuite.metadata.category.CategorialClass;
+import gov.nih.nci.cagrid.dcql.DCQLQuery;
+
+/**
+ * Specifies the operations to be supported by a results transformer. A query
+ * result transformer has to be registered in the file
+ * <code>ResultConfiguration.xml</xml>.
+ * @author srinath_k
+ * @see AbstractQueryResultTransformer
+ * @param <R>
+ *            the type of records created when executing a query for a class.
+ * @param <C>
+ *            the type of records created when executing a query for a category.
+ */
+public interface IQueryResultTransformer<R extends IRecord, C extends ICategorialClassRecord> {
+    /**
+     * Executes the DCQL and transforms the results obtained to an appropriate
+     * {@link IQueryResult}.
+     * 
+     * @param query the DCQL.
+     * @param targetEntity the target entity (corresponds to the target object
+     *            of the dcql).
+     * @param cred security credentials
+     * @return the results.
+     */
+    IQueryResult<R> getResults(DCQLQuery query, EntityInterface targetEntity, GlobusCredential cred);
+
+    /**
+     * Executes the DCQL and transforms the results obtained to an appropriate
+     * {@link IQueryResult}.
+     * 
+     * @param query the DCQL whose target object corresponds to the actual UML
+     *            class represented by the categorial class.
+     * @param categorialClass the categorial class.
+     * @param cred security credentials
+     * @return the results.
+     */
+    IQueryResult<C> getCategoryResults(DCQLQuery query, CategorialClass categorialClass, GlobusCredential cred);
+}
---
+++ caB2B_3_1_RC2_09_Sep_09/software/cab2b/test/unit/java/server/edu/wustl/cab2b/server/queryengine/querybuilders/dcql/OneOutputOneExpressionTest_Any.res
@@ -0,0 +1,3 @@
+<DCQLQuery xmlns="http://caGrid.caBIG/1.0/gov.nih.nci.cagrid.dcql">
+    <TargetObject name="srinath.mocks.query.class_1" serviceURL="http://targetUrl"/>
+</DCQLQuery>
