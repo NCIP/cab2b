e60aa1d998ec96c18e076df6b245ea7a9c0daf97
MehtaG <MehtaG@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-09-14 13:23:11 +0000

    This tag is final tag from development side. Hence forth only changes if required will be done for App Scan fixes. This tag is the Final tag. After this only GA tag will be done

---
+++ caB2B_3_1_RC3_14_sep_09/software/cab2b/src/java/client/edu/wustl/cab2b/client/ui/searchDataWizard/Signal.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.wustl.cab2b.client.ui.searchDataWizard;
+
+/**
+ * @author chetan_patil
+ *
+ */
+public interface Signal {
+
+}
---
+++ caB2B_3_1_RC3_14_sep_09/software/cab2b/src/java/server/edu/wustl/cab2b/server/datalist/DataCategorySaver.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package edu.wustl.cab2b.server.datalist;
+
+import java.util.List;
+
+import edu.common.dynamicextensions.domaininterface.AttributeInterface;
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+
+/**
+ * @author deepak_shingan
+ *
+ */
+public class DataCategorySaver extends AbstractDataListSaver {
+
+    List<AttributeInterface> newAttributes;
+
+    List<AttributeInterface> oldAttributes;
+
+    /**
+     * Cosnstructor for DataCategorySaver, initializes new attributes and old attributes
+     * @param newOnes
+     * @param oldOnes
+     */
+    public DataCategorySaver(List<AttributeInterface> newOnes, List<AttributeInterface> oldOnes) {
+        newAttributes = newOnes;
+        oldAttributes = oldOnes;
+    }
+
+    /* (non-Javadoc)
+     * @see edu.wustl.cab2b.server.datalist.AbstractDataListSaver#populateNewEntity(edu.common.dynamicextensions.domaininterface.EntityInterface)
+     */
+    @Override
+    protected void populateNewEntity(EntityInterface oldEntity) {
+        DataListUtil.copyNonVirtualAttributes(newEntity, oldEntity);
+        if (oldAttributes != null) {
+            for (AttributeInterface oldAttribute : oldAttributes) {
+                newEntity.removeAttribute(oldAttribute);
+            }
+        }
+
+        if (newAttributes != null) {
+            for (AttributeInterface newAttribute : newAttributes) {
+                newEntity.addAttribute(newAttribute);
+                newAttribute.setEntity(newEntity);
+            }
+        }
+    }
+}
