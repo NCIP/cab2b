43a311dfaa91fe8eb3b99259ddea5264f51810bd
piepenbringc <piepenbringc@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-10-14 03:44:30 +0000

    Removed cab2b subprojects

---
+++ caB2B_3.1.1/software/cab2b/src/java/common/edu/wustl/cab2b/common/ejb/utility/UtilityRemoteInterface.java
@@ -0,0 +1,10 @@
+package edu.wustl.cab2b.common.ejb.utility;
+
+import javax.ejb.EJBObject;
+
+/**
+ * @author Chandrakant Talele
+ */
+public interface UtilityRemoteInterface extends UtilityBusinessInterface,EJBObject {
+
+}
