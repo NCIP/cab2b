1f61edc5671198036c013ecb405ea43e2782b2fd
rokickik <rokickik@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-11-02 18:38:41 +0000

    branched at the wrong level

--- iphone_branch/caarray-extentions/src/java/common/cab2b/common/caarray/ICaArrayRecord.java
+++
@@ -1,7 +0,0 @@
-package cab2b.common.caarray;
-
-import edu.wustl.cab2b.common.queryengine.result.IRecordWithAssociatedIds;
-
-public interface ICaArrayRecord extends IRecordWithAssociatedIds {
-
-}
