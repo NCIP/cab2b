5141ca43cadaaca6f59e0b0dab70a661ec623745
piepenbringc <piepenbringc@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2009-12-15 03:35:54 +0000

    Tagging trunk merge of 3.0 release

---
+++ caB2B_3.0_GA_trunk/software/caB2BAdmin/src/web/pages/Layout.jsp
@@ -0,0 +1,24 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>caBench-To-Bedside</title>
+<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+<link href="css/cab2b.css" rel="stylesheet" type="text/css" />
+<link href="css/cab2b_1024.css" rel="stylesheet" type="text/css">
+<script language="JavaScript" src="javascript/menu_create.js"></script>
+<script src="javascript/menu_popup.js"></script>
+</head>
+
+<body
+	onLoad="MM_preloadImages('images/home1.gif','images/sd2.gif','images/home2.gif','images/sd1.gif','images/exp1.gif')">
+<table width="100%" border="0" align="center" cellpadding="0"
+	cellspacing="0" class="content_table">
+	<tr>
+		<td align="center" valign="top"><tiles:insertAttribute
+			name="header" /> <tiles:insertAttribute name="body" /></td>
+	</tr>
+</table>
+</body>
+</html>
---
+++ caB2B_3.0_GA_trunk/software/cab2b/src/java/client/edu/wustl/cab2b/client/ui/controls/LazyTable/BucketCache.java
@@ -0,0 +1,81 @@
+package edu.wustl.cab2b.client.ui.controls.LazyTable;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This cache implementation based on buckets.
+ * A page can be cached into a fixed bucket. If it is not present in that bucket, the existing page at that location is replaced
+ * by this page.
+ * 
+ * @author Rahul Ner
+ * @param <D>
+ */
+public class BucketCache<D> implements CacheInterface<D> {
+
+    private static final Logger logger = edu.wustl.common.util.logger.Logger.getLogger(BucketCache.class);
+
+    private Page<D>[] pageCache;
+
+    private int cacheSize;
+
+    /**
+     * @param cacheSize
+     */
+    public BucketCache(int cacheSize) {
+        this.cacheSize = cacheSize;
+        pageCache = new Page[cacheSize];
+    }
+
+    /**
+     * 
+     */
+    public BucketCache() {
+        this(3);
+    }
+
+    /**
+     * Checks whether a certain page is cached or not
+     * @param pageInfo
+     * @return boolean value
+     * @see edu.wustl.cab2b.client.CacheInterface#isCached(edu.wustl.cab2b.client.PageInfo)
+     */
+    public boolean isCached(PageInfo pageInfo) {
+        if (get(pageInfo) == null) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * puts a certain page in cache
+     * @param page
+     * @see edu.wustl.cab2b.client.CacheInterface#put(edu.wustl.cab2b.client.Page)
+     */
+    public void put(Page page) {
+        int cacheLocation = getCacheLocation(page.getPageInfo());
+        pageCache[cacheLocation] = page;
+        logger.debug("Page Cached: " + page.getPageInfo());
+    }
+
+    /**
+     * @param pageInfo
+     * @return Checks a page information
+     * @see edu.wustl.cab2b.client.CacheInterface#get(edu.wustl.cab2b.client.PageInfo)
+     */
+    public Page get(PageInfo pageInfo) {
+        int cacheLocation = getCacheLocation(pageInfo);
+        Page cachedPage = pageCache[cacheLocation];
+        if (cachedPage != null && pageInfo.equals(cachedPage.getPageInfo())) {
+            logger.debug("Cache Hit: " + pageInfo);
+            return cachedPage;
+        } else {
+            logger.debug("**Cache Miss: " + pageInfo);
+            return null;
+        }
+    }
+
+    private int getCacheLocation(PageInfo pageInfo) {
+        return pageInfo.getStartX() % cacheSize;
+    }
+
+}
