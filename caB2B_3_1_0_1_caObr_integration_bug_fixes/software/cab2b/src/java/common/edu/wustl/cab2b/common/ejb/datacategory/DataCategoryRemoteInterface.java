63906357fdc6d4b39623ac96c2bea77ebee069cb
gpatel <gpatel@2aec5c7b-6d6d-b947-a8ac-14bdfe0f8343>
2010-05-20 08:11:23 +0000

    for configuration change . We will provide new build for same bug

--- caB2B_3_1_0_1_caObr_integration_bug_fixes/software/cab2b/src/java/common/edu/wustl/cab2b/common/ejb/datacategory/DataCategoryRemoteInterface.java
+++
@@ -1,8 +0,0 @@
-package edu.wustl.cab2b.common.ejb.datacategory;
-
-import javax.ejb.EJBObject;
-
-public interface DataCategoryRemoteInterface extends DataCategoryBusinessInterface, EJBObject {
-    
-
-}
